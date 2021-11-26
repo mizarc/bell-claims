5c27eb128432c6ba6488e882b87aad44f0d53330
Mizarc <mizarc@protonmail.com>
2021-11-27 02:41:24 +1000

    🌠 This is where the fun begins

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.5.10"
+    id("com.github.johnrengelman.shadow") version "7.1.0"
+}
+
+group = "xyz.mizarc"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    }
+    maven {
+        url = uri("https://oss.sonatype.org/content/repositories/snapshots")
+    }
+    maven {
+        url = uri("https://oss.sonatype.org/content/repositories/central")
+    }
+    mavenLocal()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    compileOnly("org.spigotmc:spigot-api:1.18-pre8-R0.1-SNAPSHOT")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "11"
+}
