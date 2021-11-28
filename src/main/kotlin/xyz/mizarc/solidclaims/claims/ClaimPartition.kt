c29af256beca151998d017064d3d6e0f1a77ca18
Mizarc <mizarc@protonmail.com>
2021-11-28 20:16:28 +1000

    Add logic to fetch claim at a specified location
    By having the claim partition check whether a location is within the x
    and y bounds of the partition, the claim container can check each claim
    that exists within the narrowed down list of claims inside of a chunk to
    check whether or not the event has been executed within the partition.

--- src/main/kotlin/xyz/mizarc/solidclaims/claims/ClaimPartition.kt
+++ src/main/kotlin/xyz/mizarc/solidclaims/claims/ClaimPartition.kt
@@ -3,5 +3,18 @@ package xyz.mizarc.solidclaims.claims
 import org.bukkit.Location
 
 class ClaimPartition(var claim: Claim, var firstPosition: Location, var secondPosition: Location) {
+    fun isLocationInClaim(location: Location) : Boolean {
+        if (location.world?.name != claim.world) {
+            return false
+        }
 
+        if (location.x >= firstPosition.x
+            && location.x <= secondPosition.x
+            && location.z >= firstPosition.z
+            && location.z <= secondPosition.z) {
+            return true
+        }
+
+        return false
+    }
 }
