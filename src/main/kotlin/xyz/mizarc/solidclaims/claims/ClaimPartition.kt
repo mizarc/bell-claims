13f86b2d078d26e22b95ceb42608dba06330bc68
Mizarc <mizarc@protonmail.com>
2021-11-30 19:43:00 +1000

    Change Location object to integer pairs
    To save on memory usage, rather than storing the full location data of
    the block corners on a claim partition, it instead stores just two
    integers. If the location data is required, dedicated functions now
    exist to fetch location data. This is not required for most cases.

--- src/main/kotlin/xyz/mizarc/solidclaims/claims/ClaimPartition.kt
+++ src/main/kotlin/xyz/mizarc/solidclaims/claims/ClaimPartition.kt
@@ -2,16 +2,16 @@ package xyz.mizarc.solidclaims.claims
 
 import org.bukkit.Location
 
-class ClaimPartition(var claim: Claim, var firstPosition: Location, var secondPosition: Location) {
+class ClaimPartition(var claim: Claim, var firstPosition: Pair<Int, Int>, var secondPosition: Pair<Int, Int>) {
     fun isLocationInClaim(location: Location) : Boolean {
         if (location.world?.uid != claim.worldId) {
             return false
         }
 
-        if (location.x >= firstPosition.x
-            && location.x <= secondPosition.x
-            && location.z >= firstPosition.z
-            && location.z <= secondPosition.z) {
+        if (location.x >= firstPosition.first
+            && location.x <= secondPosition.first
+            && location.z >= firstPosition.second
+            && location.z <= secondPosition.second) {
             return true
         }
 
@@ -20,18 +20,26 @@ class ClaimPartition(var claim: Claim, var firstPosition: Location, var secondPo
 
     fun getEdgeBlockPositions() : MutableSet<Pair<Int, Int>> {
         var blocks : MutableSet<Pair<Int, Int>> = mutableSetOf()
-        for (block in firstPosition.x.toInt()..secondPosition.x.toInt()) {
-            blocks.add(Pair(block, firstPosition.z.toInt()))
+        for (block in firstPosition.first..secondPosition.first) {
+            blocks.add(Pair(block, firstPosition.second.toInt()))
         }
-        for (block in firstPosition.x.toInt()..secondPosition.x.toInt()) {
-            blocks.add(Pair(block, secondPosition.z.toInt()))
+        for (block in firstPosition.first..secondPosition.first) {
+            blocks.add(Pair(block, secondPosition.second))
         }
-        for (block in firstPosition.z.toInt()..secondPosition.z.toInt()) {
-            blocks.add(Pair(block, firstPosition.x.toInt()))
+        for (block in firstPosition.second..secondPosition.second) {
+            blocks.add(Pair(block, firstPosition.first))
         }
-        for (block in firstPosition.z.toInt()..secondPosition.z.toInt()) {
-            blocks.add(Pair(block, secondPosition.x.toInt()))
+        for (block in firstPosition.second..secondPosition.second) {
+            blocks.add(Pair(block, secondPosition.first))
         }
         return blocks
+    }
+
+    fun getFirstLocation() : Location {
+        return Location(claim.getWorld(), firstPosition.first.toDouble(), 0.0, firstPosition.second.toDouble())
+    }
+
+    fun getSecondLocation() : Location {
+        return Location(claim.getWorld(), secondPosition.first.toDouble(), 0.0, secondPosition.second.toDouble())
     }
 }
