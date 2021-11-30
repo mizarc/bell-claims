9036c28a76f310035dfcdc12d22b866724a39ebc
Mizarc <mizarc@protonmail.com>
2021-11-30 17:24:35 +1000

    Add function for getting claim edges
    This will allow detecting of claim partition edges, returning a set of
    x and z positions that make up the edges.

--- src/main/kotlin/xyz/mizarc/solidclaims/claims/ClaimPartition.kt
+++ src/main/kotlin/xyz/mizarc/solidclaims/claims/ClaimPartition.kt
@@ -17,4 +17,21 @@ class ClaimPartition(var claim: Claim, var firstPosition: Location, var secondPo
 
         return false
     }
+
+    fun getEdgeBlockPositions() : MutableSet<Pair<Int, Int>> {
+        var blocks : MutableSet<Pair<Int, Int>> = mutableSetOf()
+        for (block in firstPosition.x.toInt()..secondPosition.x.toInt()) {
+            blocks.add(Pair(block, firstPosition.z.toInt()))
+        }
+        for (block in firstPosition.x.toInt()..secondPosition.x.toInt()) {
+            blocks.add(Pair(block, secondPosition.z.toInt()))
+        }
+        for (block in firstPosition.z.toInt()..secondPosition.z.toInt()) {
+            blocks.add(Pair(block, firstPosition.x.toInt()))
+        }
+        for (block in firstPosition.z.toInt()..secondPosition.z.toInt()) {
+            blocks.add(Pair(block, secondPosition.x.toInt()))
+        }
+        return blocks
+    }
 }
