365a8e9c4b806f9645bf8f268668d275b21805eb
Mizarc <mizarc@protonmail.com>
2021-11-30 13:12:12 +1000

    Add function to add a claim to a list
    A secondary constructor has been added to the claim class in order to
    build new claims, allowing it to omit the id and player trust list. A
    new list has been added to the ClaimContainer to hold claims, meaning
    the claim partition lists have been renamed appropriately.

--- src/main/kotlin/xyz/mizarc/solidclaims/claims/Claim.kt
+++ src/main/kotlin/xyz/mizarc/solidclaims/claims/Claim.kt
@@ -1,8 +1,10 @@
 package xyz.mizarc.solidclaims.claims
 
+import org.bukkit.OfflinePlayer
+import org.bukkit.World
 import java.util.*
 import kotlin.collections.ArrayList
 
-class Claim(var id: Int, var world: UUID, var owner: UUID, var players: ArrayList<Player>) {
-
+class Claim(var id: UUID, var world: World, var owner: OfflinePlayer, var players: ArrayList<Player>) {
+    constructor(world: World, owner: OfflinePlayer) : this(UUID.randomUUID(), world, owner, ArrayList())
 }
