c8c57f1a1f18a6e98be3c0767b315e23c12bf358
Mizarc <mizarc@protonmail.com>
2021-12-01 16:18:31 +1000

    Add default permissions field to claim
    This can be used to define the permissions for all players for a
    specified claim.

--- src/main/kotlin/xyz/mizarc/solidclaims/claims/Claim.kt
+++ src/main/kotlin/xyz/mizarc/solidclaims/claims/Claim.kt
@@ -3,6 +3,7 @@ package xyz.mizarc.solidclaims.claims
 import org.bukkit.Bukkit
 import org.bukkit.OfflinePlayer
 import org.bukkit.World
+import xyz.mizarc.solidclaims.events.ClaimPermission
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -15,13 +16,14 @@ import kotlin.collections.ArrayList
  * @property owner A reference to the owning player.
  * @property claimPlayers A list of trusted players.
  */
-class Claim(var id: UUID, var worldId: UUID, var owner: OfflinePlayer, var claimPlayers: ArrayList<ClaimPlayer>) {
+class Claim(var id: UUID, var worldId: UUID, var owner: OfflinePlayer,
+            var defaultPermissions: ArrayList<ClaimPermission>, var claimPlayers: ArrayList<ClaimPlayer>) {
     /**
      * Compiles a new claim based on the world and owning player.
      * @param worldId The unique identifier of the world the claim is to be made in.
      * @param owner A reference to the owning player.
      */
-    constructor(worldId: UUID, owner: OfflinePlayer) : this(UUID.randomUUID(), worldId, owner, ArrayList())
+    constructor(worldId: UUID, owner: OfflinePlayer) : this(UUID.randomUUID(), worldId, owner, ArrayList(), ArrayList())
 
     /**
      * Gets a reference to the world if available.
