2aad340b5b9ac339a9bc729e662ecd09103c3778
Mizarc <mizarc@protonmail.com>
2021-12-02 18:56:36 +1000

    Add function to check if claims is not overlapping
    Currently incomplete, but the useClaimTool function will utilise this
    function to check whether or not the claim is overlapping any other
    claim.

--- src/main/kotlin/xyz/mizarc/solidclaims/events/ClaimToolListener.kt
+++ src/main/kotlin/xyz/mizarc/solidclaims/events/ClaimToolListener.kt
@@ -4,15 +4,55 @@ import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.block.Action
 import org.bukkit.event.player.PlayerInteractEvent
+import xyz.mizarc.solidclaims.claims.ClaimContainer
+import xyz.mizarc.solidclaims.claims.ClaimPartition
 import xyz.mizarc.solidclaims.getClaimTool
 
-class ClaimToolListener : Listener {
+class ClaimToolListener(val claimContainer: ClaimContainer) : Listener {
+    var playerClaimBuilders: ArrayList<PlayerClaimBuilder> = ArrayList()
+
     @EventHandler
     fun onUseClaimTool(event: PlayerInteractEvent) {
         if (event.action != Action.RIGHT_CLICK_BLOCK) return
         if (event.item == null) return
         if (event.item!!.itemMeta != getClaimTool().itemMeta) return
 
-        event.player.sendMessage("test")
+        // Check if player is already making a claim
+        var isMakingClaim = false
+        for (player in playerClaimBuilders) {
+            if (player.playerId == event.player.uniqueId) {
+                isMakingClaim = true
+                break
+            }
+        }
+
+        // Set first location
+        val playerClaimBuilder = PlayerClaimBuilder(event.player.uniqueId)
+        if (!isMakingClaim) {
+            playerClaimBuilder.firstLocation = event.clickedBlock?.location!!
+            event.player.sendMessage("New claim building started. First position has been selected.")
+            return
+        }
+
+        // Set second location
+        playerClaimBuilder.secondLocation = event.clickedBlock?.location!!
+        checkValidClaim(playerClaimBuilder)
+    }
+
+    fun checkValidClaim(playerClaimBuilder: PlayerClaimBuilder) {
+        val chunks = claimContainer.getClaimChunks(
+            claimContainer.getPositionFromLocation(playerClaimBuilder.firstLocation!!),
+            claimContainer.getPositionFromLocation(playerClaimBuilder.secondLocation!!))
+
+        val partitionsInClaim: MutableSet<ClaimPartition> = mutableSetOf()
+        for(chunk in chunks) {
+            partitionsInClaim.addAll(claimContainer.getClaimPartitionsAtChunk(chunk)!!)
+        }
+
+
+        /*val newClaim: ClaimPartition = ClaimPartition(
+            claimContainer.getPositionFromLocation(playerClaimBuilder.firstLocation),
+            claimContainer.getPositionFromLocation(playerClaimBuilder.secondLocation))
+        claimContainer.getClaimPartitionsAtChunk()*/
     }
 }
