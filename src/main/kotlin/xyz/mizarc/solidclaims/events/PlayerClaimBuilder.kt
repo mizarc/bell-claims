fe5e8da430f7e44c0545ab72f98116b69954ee47
Mizarc <mizarc@protonmail.com>
2021-12-02 15:51:53 +1000

    Create object to hold temporary claim builder
    To store the data of the claim the player is trying to create, a new
    object has been created that holds the player id as well as the two
    locations of the blocks they have selected.

---
+++ src/main/kotlin/xyz/mizarc/solidclaims/events/PlayerClaimBuilder.kt
@@ -0,0 +1,18 @@
+package xyz.mizarc.solidclaims.events
+
+import org.bukkit.Location
+import java.util.*
+
+/**
+ * Associates a player with two different locations to get the locations intended to make a claim out of
+ * @property playerId The unique identifier of the player.
+ * @property firstLocation The location of the first claim corner.
+ * @property secondLocation The location of the second claim corner.
+ */
+class PlayerClaimBuilder(var playerId: UUID, var firstLocation: Location?, var secondLocation: Location?) {
+    /**
+     * Creates a PlayerClaimBuilder object with empty location values.
+     * @param playerId The unique identifier of the player.
+     */
+    constructor(playerId: UUID) : this(playerId, null, null)
+}
