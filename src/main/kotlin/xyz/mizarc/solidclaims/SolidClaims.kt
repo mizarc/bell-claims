7303e5ffce18a526b0958e32ec6177d259ce9f5d
Mizarc <mizarc@protonmail.com>
2021-12-02 12:51:46 +1000

    Add claim and owner container vars to main class
    This allows for a singular reference to these two essential collection
    types.

--- src/main/kotlin/xyz/mizarc/solidclaims/SolidClaims.kt
+++ src/main/kotlin/xyz/mizarc/solidclaims/SolidClaims.kt
@@ -3,6 +3,7 @@ package xyz.mizarc.solidclaims
 import co.aikar.commands.PaperCommandManager
 import org.bukkit.plugin.java.JavaPlugin
 import xyz.mizarc.solidclaims.claims.ClaimContainer
+import xyz.mizarc.solidclaims.claims.OwnerContainer
 import xyz.mizarc.solidclaims.commands.ClaimCommand
 import xyz.mizarc.solidclaims.commands.HandleEventsCommand
 import xyz.mizarc.solidclaims.events.ClaimEventHandler
@@ -10,11 +11,15 @@ import xyz.mizarc.solidclaims.events.ClaimEventHandler
 class SolidClaims : JavaPlugin() {
     internal lateinit var commandManager: PaperCommandManager
     internal var configIO: ConfigIO = ConfigIO(this)
+    lateinit var claimContainer: ClaimContainer
+    lateinit var ownerContainer: OwnerContainer
+
 
     override fun onEnable() {
-        val claimContainer = ClaimContainer()
-        server.pluginManager.registerEvents(ClaimEventHandler(this, claimContainer), this)
         logger.info("SolidClaims has been Enabled")
+        claimContainer = ClaimContainer()
+        ownerContainer = OwnerContainer()
+        server.pluginManager.registerEvents(ClaimEventHandler(this, claimContainer), this)
         commandManager = PaperCommandManager(this)
         commandManager.registerCommand(ClaimCommand())
         commandManager.registerCommand(HandleEventsCommand())
