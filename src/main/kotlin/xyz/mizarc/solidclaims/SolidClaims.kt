ae0a812345b049612b08d4b84de791637d4440e5
Mizarc <mizarc@protonmail.com>
2021-12-04 03:00:12 +1000

    Add function to move data from database to memory
    To finally be able to read existing data from the database, all of the
    data that is stored in the database to do with claims is stored into
    memory on initialisation.

--- src/main/kotlin/xyz/mizarc/solidclaims/SolidClaims.kt
+++ src/main/kotlin/xyz/mizarc/solidclaims/SolidClaims.kt
@@ -12,22 +12,36 @@ import xyz.mizarc.solidclaims.events.ClaimToolListener
 class SolidClaims : JavaPlugin() {
     internal lateinit var commandManager: PaperCommandManager
     internal var configIO: ConfigIO = ConfigIO(this)
+    lateinit var database: DatabaseStorage
     lateinit var claimContainer: ClaimContainer
     lateinit var ownerContainer: OwnerContainer
 
 
     override fun onEnable() {
         logger.info("SolidClaims has been Enabled")
-        claimContainer = ClaimContainer()
+        claimContainer = ClaimContainer(database)
         ownerContainer = OwnerContainer()
         server.pluginManager.registerEvents(ClaimEventHandler(this, claimContainer), this)
         server.pluginManager.registerEvents(ClaimToolListener(this.claimContainer), this)
         commandManager = PaperCommandManager(this)
         commandManager.registerCommand(ClaimCommand())
         commandManager.registerCommand(HandleEventsCommand())
+        loadDataFromDatabase()
     }
 
     override fun onDisable() {
         logger.info("SolidClaims has been Disabled")
+    }
+
+    private fun loadDataFromDatabase() {
+        val claims = database.getAllClaims()
+        for (claim in claims!!) {
+            claimContainer.addClaim(claim)
+
+            val claimPartitions = database.getClaimPartitionsByClaim(claim) ?: continue
+            for (partition in claimPartitions) {
+                claimContainer.addClaimPartition(partition)
+            }
+        }
     }
 }
