3b11b3cabd061c47ab149bea70386ea1bac0faeb
Raspberry-Jam <raspberryjam.chivers@gmail.com>
2021-12-04 17:31:42 +1000

    Implement event handler to visualise claim borders
    ClaimVisualiser will listen to PlayerItemHeldEvents and display claim
    borders to players holding the claiming tool by replacing blocks in
    that player's instance to a distinct block to represent the position
    of the borders of the claims that are within a set distance of them

---
+++ src/main/kotlin/xyz/mizarc/solidclaims/events/ClaimVisualiser.kt
@@ -0,0 +1,68 @@
+package xyz.mizarc.solidclaims.events
+
+import org.bukkit.Location
+import org.bukkit.Material
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerItemHeldEvent
+import xyz.mizarc.solidclaims.SolidClaims
+import xyz.mizarc.solidclaims.claims.ClaimContainer
+import xyz.mizarc.solidclaims.claims.ClaimPartition
+import xyz.mizarc.solidclaims.getClaimTool
+
+class ClaimVisualiser(val plugin: SolidClaims) : Listener {
+    var playerVisualisingState: MutableMap<Player, Boolean> = HashMap()
+
+    @EventHandler
+    fun onHoldClaimTool(event: PlayerItemHeldEvent) {
+        val holdingClaimTool = event.player.inventory.itemInMainHand == getClaimTool() || event.player.inventory.itemInOffHand == getClaimTool()
+        val state: Boolean = playerVisualisingState[event.player] ?: !holdingClaimTool // If map does not contain player, set value to opposite of holdingClaimTool to force it to update
+        if (state == holdingClaimTool) return
+
+        playerVisualisingState[event.player] = holdingClaimTool // Register state to the cache
+
+        val chunks = getSurroundingChunks(ClaimContainer.getPositionFromLocation(event.player.location), 2)
+        val partitions = getClaimPartitionsInChunks(chunks)
+        if (partitions.isEmpty()) return
+
+        val borders: ArrayList<Pair<Int, Int>> = ArrayList()
+        for (part in partitions) {
+            borders.addAll(part.getEdgeBlockPositions())
+        }
+
+        // TODO: Make this not flood packets please
+        for (block in borders) {
+            for (y in -64..255) {
+                event.player.sendBlockChange(Location(event.player.location.world, block.first.toDouble(), y.toDouble(), block.second.toDouble()), Material.CYAN_GLAZED_TERRACOTTA.createBlockData())
+            }
+        }
+    }
+
+    private fun getClaimPartitionsInChunks(chunks: Array<Pair<Int, Int>>): Array<ClaimPartition> {
+        val claims: ArrayList<ClaimPartition> = ArrayList()
+
+        for (chunk in chunks) {
+            val parts = plugin.claimContainer.getClaimPartitionsAtChunk(chunk) ?: continue
+            for (part in parts) {
+                if (claims.contains(part)) continue
+                claims.add(part)
+            }
+        }
+        return claims.toTypedArray()
+    }
+
+    @Suppress("SameParameterValue")
+    private fun getSurroundingChunks(loc: Pair<Int, Int>, radius: Int): Array<Pair<Int, Int>> {
+        val sideLength = (radius * 2) + 1 // Make it always odd (eg. radius of 2 results in 5x5 square
+        val chunks: Array<Pair<Int, Int>> = Array(sideLength) {Pair(0, 0)}
+
+        for (x in 0..sideLength) {
+            for (z in 0..sideLength) {
+                chunks[(x * sideLength) + z] = Pair(loc.first + x - radius, loc.second + z - radius)
+            }
+        }
+
+        return chunks
+    }
+}
