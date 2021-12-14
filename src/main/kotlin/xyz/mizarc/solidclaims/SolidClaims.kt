9543419305ae825bbd754e3e9bda4d7055bd3dbf
Mizarc <mizarc@protonmail.com>
2021-12-14 23:55:46 +1000

    Add partitionlist subcommand for claim
    This utilises the same chat info builder setup as the trust list, but
    fetches a list of partitions rather than trusted players.

--- src/main/kotlin/xyz/mizarc/solidclaims/SolidClaims.kt
+++ src/main/kotlin/xyz/mizarc/solidclaims/SolidClaims.kt
@@ -30,6 +30,7 @@ class SolidClaims : JavaPlugin() {
         commandManager.registerCommand(UntrustCommand())
         commandManager.registerCommand(ClaiminfoCommand())
         commandManager.registerCommand(TrustlistCommand())
+        commandManager.registerCommand(PartitionlistCommand())
         commandManager.registerCommand(HandleEventsCommand())
         commandManager.commandCompletions.registerCompletion("permissions") {
             val perms: ArrayList<String> = ArrayList()
