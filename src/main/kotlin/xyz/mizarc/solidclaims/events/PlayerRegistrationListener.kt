a3c21ee05760e543cb9e3a4974ab3f441ec83e7f
Mizarc <mizarc@protonmail.com>
2021-12-16 20:42:45 +1000

    Add listener to register new players
    Registered players are automatically given a playerState object and
    added to the container.

---
+++ src/main/kotlin/xyz/mizarc/solidclaims/events/PlayerRegistrationListener.kt
@@ -0,0 +1,15 @@
+package xyz.mizarc.solidclaims.events
+
+import org.bukkit.event.EventHandler
+import org.bukkit.event.player.PlayerJoinEvent
+import xyz.mizarc.solidclaims.PlayerContainer
+import xyz.mizarc.solidclaims.PlayerState
+
+class PlayerRegistrationListener(val playerContainer: PlayerContainer) {
+
+    @EventHandler
+    fun onPlayerJoin(event: PlayerJoinEvent) {
+        val playerState = PlayerState(event.player.uniqueId, 5000, 5000)
+        playerContainer.addNewPlayer(playerState)
+    }
+}
