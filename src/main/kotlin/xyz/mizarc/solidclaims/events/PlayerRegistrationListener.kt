7141c1147fec39ef517592fc7150cc55d2f15cac
Mizarc <mizarc@protonmail.com>
2021-12-17 02:35:09 +1000

    Register PlayerRegistrationListener
    Need to do this for it to work properly. Also fixed database id column
    fetching for playerState.

--- src/main/kotlin/xyz/mizarc/solidclaims/events/PlayerRegistrationListener.kt
+++ src/main/kotlin/xyz/mizarc/solidclaims/events/PlayerRegistrationListener.kt
@@ -1,11 +1,12 @@
 package xyz.mizarc.solidclaims.events
 
 import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
 import xyz.mizarc.solidclaims.PlayerContainer
 import xyz.mizarc.solidclaims.PlayerState
 
-class PlayerRegistrationListener(val playerContainer: PlayerContainer) {
+class PlayerRegistrationListener(val playerContainer: PlayerContainer) : Listener {
 
     @EventHandler
     fun onPlayerJoin(event: PlayerJoinEvent) {
