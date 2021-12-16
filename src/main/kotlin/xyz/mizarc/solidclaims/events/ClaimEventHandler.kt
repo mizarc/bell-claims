ce368031adcd2e65dbcb68d26c149037d50cf24e
Raspberry-Jam <raspberryjam.chivers@gmail.com>
2021-12-16 21:57:23 +1000

    Rewrite handleClaimEvent to use PermissionExecutor
    handleClaimEvent function is now structured to use the new
    PermissionExecutor to determine the location of the event and the player
    that caused it.

--- src/main/kotlin/xyz/mizarc/solidclaims/events/ClaimEventHandler.kt
+++ src/main/kotlin/xyz/mizarc/solidclaims/events/ClaimEventHandler.kt
@@ -1,26 +1,25 @@
 package xyz.mizarc.solidclaims.events
 
+import net.md_5.bungee.api.ChatColor
+import org.bukkit.Location
+import org.bukkit.entity.Player
 import org.bukkit.event.Event
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
-import org.bukkit.event.player.PlayerEvent
 import xyz.mizarc.solidclaims.SolidClaims
 import xyz.mizarc.solidclaims.claims.ClaimContainer
+import xyz.mizarc.solidclaims.claims.PlayerAccess
 
 /**
  * Handles the registration of defined events with their associated actions.
  * @property plugin A reference to the plugin instance
  * @property claimContainer A reference to the ClaimContainer instance
  */
-class ClaimEventHandler(var solidClaims: SolidClaims, var claimContainer: ClaimContainer) : Listener {
-    companion object {
-        var handleEvents = false
-    }
-
+class ClaimEventHandler(var plugin: SolidClaims, var claimContainer: ClaimContainer) : Listener {
     init {
-        ClaimPermission.values().forEach { p ->
-            p.events.forEach { e ->
-                registerEvent(e.first, ::handleClaimEvent)
+        for (perm in ClaimPermission.values()) {
+            for (e in perm.events) {
+                registerEvent(e.eventClass, ::handleClaimEvent)
             }
         }
     }
@@ -31,19 +30,33 @@ class ClaimEventHandler(var solidClaims: SolidClaims, var claimContainer: ClaimC
      * executor has the highest priority, then invoke that executor.
      */
     private fun handleClaimEvent(listener: Listener, event: Event) {
-        if (!handleEvents) return // TODO: Remove debug
-        if (event !is PlayerEvent) return // TODO: Check for non-player events to handle
-        // NOTE: If player breaks block inside of claim while standing outside of claim, this does not reflect that
-        // TODO: Fix this behaviour
-        val location = event.player.location
-        val claim = claimContainer.getClaimAtLocation(location) ?: return
-        val player = solidClaims.databaseStorage.getPlayerClaimPermissions(event.player.uniqueId, claim.id)
+        val eventPerms = ClaimPermission.getPermissionsForEvent(event::class.java) // Get all ClaimEvents that deal with this event
 
-        val claimPerms = player?.claimPermissions ?: claim.defaultPermissions
-        val eventPerms = ClaimPermission.getPermissionsForEvent(event::class.java)
+        // Get the top PermissionExecutor that deals with this event.
+        // NOTE: This assumes that any PermissionExecutor that deals with this event will always return the same values
+        // for location and player as any other for this event would
+        val tempExecutor = ClaimPermission.getPermissionExecutorForEvent(event::class.java) ?: return
 
-        var executor: ((l: Listener, e: Event) -> Unit)? = null
+        val player: Player? = tempExecutor.source.invoke(event) // The player that caused this event, if any
+        val location: Location = tempExecutor.location.invoke(event) ?: return // If no location was found, do nothing
 
+        // Determine if this event happened inside of a claim's boundaries
+        val claim = plugin.claimContainer.getClaimPartitionAtLocation(location)?.claim ?: return
+
+        var claimTrustee: PlayerAccess? = null // The relevant claim's trustee, if the player is trusted
+        for (p in claim.playerAccesses) {
+            if (p.id == player?.uniqueId) {
+                claimTrustee = p
+                break
+            }
+        }
+
+        // Get the claim permissions to use, whether it's the trustee's individual permissions, or the claim's default permissions
+        val claimPerms = claimTrustee?.claimPermissions ?: claim.defaultPermissions
+
+        var executor: ((l: Listener, e: Event) -> Unit)? = null // The function that handles the result of this event
+
+        // Determine if the claim permissions contains any of the parent permissions to this one
         fun checkPermissionParents(p: ClaimPermission): Boolean {
             var pRef: ClaimPermission? = p
             while (pRef?.parent != null) {
@@ -55,18 +68,23 @@ class ClaimEventHandler(var solidClaims: SolidClaims, var claimContainer: ClaimC
             return false
         }
 
+        // Determine the highest priority permission for the event and sets the executor to the one found, if any
         for (e in eventPerms) {
-            if (claimPerms.contains(e) || checkPermissionParents(e)) {
-                for (ee in e.events) {
-                    if (ee.first == event::class.java) {
-                        executor = ee.second
-                        break
+            if (!checkPermissionParents(e)) { // First check if claimPerms does not contain the parent of this permission
+                if (!claimPerms.contains(e)) { // If not, check if it does not contain this permission
+                    for (ee in e.events) { // If so, determine the executor to use
+                        if (ee.eventClass == event::class.java) {
+                            executor = ee.executor
+                            break
+                        }
                     }
                 }
             }
         }
 
         executor?.invoke(listener, event)
+        // If nothing was executed then the player has permissions to enact this event, so do not send a warning.
+        if (executor != null) player?.sendMessage("${ChatColor.RED}You are not allowed to do that here!")
     }
 
     /**
