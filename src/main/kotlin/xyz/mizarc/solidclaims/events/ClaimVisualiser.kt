fc171b7e276b045df60e6884a7250d12f9b51f60
Raspberry-Jam <raspberryjam.chivers@gmail.com>
2021-12-19 02:25:23 +1000

    Rework claim border visualisation
    Determine a player's visualisation state (whether they should be seeing
    claim borders or not) using multiple events now, instead of just
    PlayerItemHeldEvent.
    
    Schedule visualisation updates for later instead of blocking the thread.
    
    Visualisation now only sends block updates for blocks that it considers
    "visible", meaning that they are either a roof or ceiling. This does
    not include blocks that are literally visible but have solid blocks both
    above and below them.
    
    Visualisation now also only considers blocks within a 50 block range
    of the player's Y location, to limit the work done by the server's CPU.

--- src/main/kotlin/xyz/mizarc/solidclaims/events/ClaimVisualiser.kt
+++ src/main/kotlin/xyz/mizarc/solidclaims/events/ClaimVisualiser.kt
@@ -2,9 +2,13 @@ package xyz.mizarc.solidclaims.events
 
 import org.bukkit.Location
 import org.bukkit.Material
+import org.bukkit.entity.EntityType
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.entity.EntityPickupItemEvent
+import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.event.player.PlayerDropItemEvent
 import org.bukkit.event.player.PlayerItemHeldEvent
 import xyz.mizarc.solidclaims.SolidClaims
 import xyz.mizarc.solidclaims.claims.ClaimContainer
@@ -14,15 +18,26 @@ import xyz.mizarc.solidclaims.getClaimTool
 class ClaimVisualiser(val plugin: SolidClaims) : Listener {
     var playerVisualisingState: MutableMap<Player, Boolean> = HashMap()
 
-    @EventHandler
-    fun onHoldClaimTool(event: PlayerItemHeldEvent) {
-        val holdingClaimTool = event.player.inventory.getItem(event.newSlot) == getClaimTool() || event.player.inventory.itemInOffHand == getClaimTool()
-        val state: Boolean = playerVisualisingState[event.player] ?: !holdingClaimTool // If map does not contain player, set value to opposite of holdingClaimTool to force it to update
+    companion object {
+        // TODO: Cover more transparent materials
+        private val transparentMaterials = arrayOf(
+            Material.AIR,
+            Material.WATER,
+            Material.LAVA,
+            Material.GLASS
+        )
+    }
+
+    /**
+     * Determine if [player] should be seeing the borders of nearby claims, and if so, send fake block data representing those bounds.
+     */
+    private fun updateVisualisation(player: Player) {
+        val holdingClaimTool = player.inventory.itemInMainHand == getClaimTool() || player.inventory.itemInOffHand == getClaimTool()
+        val state = playerVisualisingState[player] ?: !holdingClaimTool
         if (state == holdingClaimTool) return
+        playerVisualisingState[player] = holdingClaimTool
 
-        playerVisualisingState[event.player] = holdingClaimTool // Register state to the cache
-
-        val chunks = getSurroundingChunks(ClaimContainer.getChunkLocation(ClaimContainer.getPositionFromLocation(event.player.location)), 2)
+        val chunks = getSurroundingChunks(ClaimContainer.getChunkLocation(ClaimContainer.getPositionFromLocation(player.location)), plugin.server.viewDistance)
         val partitions = getClaimPartitionsInChunks(chunks)
         if (partitions.isEmpty()) return
 
@@ -30,18 +45,53 @@ class ClaimVisualiser(val plugin: SolidClaims) : Listener {
         for (part in partitions) {
             borders.addAll(part.getEdgeBlockPositions())
         }
-        // TODO: Make this not flood packets please
+
         for (block in borders) {
-            for (y in -64..255) {
+            for (y in player.location.blockY-25..player.location.blockY+25) { // Get all blocks on claim borders within 25 blocks up and down from the player's current position
                 var blockData = Material.CYAN_GLAZED_TERRACOTTA.createBlockData() // Set the visualisation block
-                val blockLocation = Location(event.player.location.world, block.first.toDouble(), y.toDouble(), block.second.toDouble())
-                if (blockLocation.block.blockData.material == Material.AIR) continue
-                if (!playerVisualisingState[event.player]!!) blockData = event.player.world.getBlockAt(blockLocation).blockData // If visualisation is being disabled, get the real block data
-                event.player.sendBlockChange(blockLocation, blockData)
+                val blockLocation = Location(player.location.world, block.first.toDouble(), y.toDouble(), block.second.toDouble()) // Get the location of the block being considered currently
+                if (transparentMaterials.contains(blockLocation.block.blockData.material)) continue // If the block is transparent, skip it
+                if (!isBlockVisible(blockLocation)) continue // If the block isn't considered to be visible, skip it
+                if (!playerVisualisingState[player]!!) blockData = player.world.getBlockAt(blockLocation).blockData // If visualisation is being disabled, get the real block data
+                player.sendBlockChange(blockLocation, blockData) // Send the player block updates
             }
         }
     }
 
+    /**
+     * Determine if a block is a floor/ceiling and therefore should be considered visible
+     */
+    private fun isBlockVisible(loc: Location): Boolean {
+        val above = Location(loc.world, loc.x, loc.y+1, loc.z).block.blockData.material
+        val below = Location(loc.world, loc.x, loc.y-1, loc.z).block.blockData.material
+        return transparentMaterials.contains(above) || transparentMaterials.contains(below)
+    }
+
+    @EventHandler
+    fun onHoldClaimTool(event: PlayerItemHeldEvent) {
+        plugin.server.scheduler.runTask(plugin, Runnable { updateVisualisation(event.player) }) // Run task after this tick
+    }
+
+    @EventHandler
+    fun onPickupClaimTool(event: EntityPickupItemEvent) {
+        if (event.entityType != EntityType.PLAYER) return
+        plugin.server.scheduler.runTask(plugin, Runnable { updateVisualisation(event.entity as Player) }) // Run task after this tick
+    }
+
+    @EventHandler
+    fun onDropClaimTool(event: PlayerDropItemEvent) {
+        plugin.server.scheduler.runTask(plugin, Runnable { updateVisualisation(event.player) }) // Run task after this tick
+    }
+
+    @EventHandler
+    fun onClaimToolInventoryInteract(event: InventoryClickEvent) {
+        val player = event.whoClicked as Player
+        plugin.server.scheduler.runTask(plugin, Runnable { updateVisualisation(player) }) // Run task after this tick
+    }
+
+    /**
+     * Determine what claim partitions are within [chunks]
+     */
     private fun getClaimPartitionsInChunks(chunks: Array<Pair<Int, Int>>): Array<ClaimPartition> {
         val claims: ArrayList<ClaimPartition> = ArrayList()
 
@@ -55,9 +105,12 @@ class ClaimVisualiser(val plugin: SolidClaims) : Listener {
         return claims.toTypedArray()
     }
 
+    /**
+     * Get a square of chunks centering on [loc] with a size of [radius]
+     */
     @Suppress("SameParameterValue")
     private fun getSurroundingChunks(loc: Pair<Int, Int>, radius: Int): Array<Pair<Int, Int>> {
-        val sideLength = (radius * 2) + 1 // Make it always odd (eg. radius of 2 results in 5x5 square
+        val sideLength = (radius * 2) + 1 // Make it always odd (eg. radius of 2 results in 5x5 square)
         val chunks: Array<Pair<Int, Int>> = Array(sideLength * sideLength) {Pair(0, 0)}
 
         for (x in 0 until sideLength) {
