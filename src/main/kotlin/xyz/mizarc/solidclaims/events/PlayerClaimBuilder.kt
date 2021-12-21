2d86a20a50cef32aef21fafa45ac74b1211c77a2
Mizarc <mizarc@protonmail.com>
2021-12-22 00:40:24 +1000

    Require first location value for claim builder
    There are almost no cases where the first claim builder shouldn't be
    added upon creating the object. This puts a hard requirement on it.

--- src/main/kotlin/xyz/mizarc/solidclaims/events/PlayerClaimBuilder.kt
+++ src/main/kotlin/xyz/mizarc/solidclaims/events/PlayerClaimBuilder.kt
@@ -11,7 +11,7 @@ import kotlin.math.absoluteValue
  * @property firstLocation The location of the first claim corner.
  * @property secondLocation The location of the second claim corner.
  */
-class PlayerClaimBuilder(var playerId: UUID, var firstLocation: Location?, var secondLocation: Location?) {
+class PlayerClaimBuilder(var playerId: UUID, var firstLocation: Location, var secondLocation: Location?) {
     /**
      * Creates a PlayerClaimBuilder object with only the first location value.
      * @param playerId The unique identifier of the player.
@@ -20,11 +20,11 @@ class PlayerClaimBuilder(var playerId: UUID, var firstLocation: Location?, var s
     constructor(playerId: UUID, firstLocation: Location) : this(playerId, firstLocation, null)
 
     fun getBlockCount() : Int? {
-        if (firstLocation == null || secondLocation == null) {
+        if (secondLocation == null) {
             return null
         }
 
-        return ((secondLocation!!.x.toInt() - firstLocation!!.x.toInt() + 1) *
-                (secondLocation!!.z.toInt() - firstLocation!!.z.toInt() + 1)).absoluteValue
+        return ((secondLocation!!.x.toInt() - firstLocation.x.toInt() + 1) *
+                (secondLocation!!.z.toInt() - firstLocation.z.toInt() + 1)).absoluteValue
     }
 }
