4a10461ccd7bafd78a85b254436cac681bd71436
Mizarc <mizarc@protonmail.com>
2021-12-26 21:44:11 +1000

    Change claim builder to use int positions
    Rather than saving location objects for claim builder, it saves integer
    positions instead to make calculations easier.

--- src/main/kotlin/xyz/mizarc/solidclaims/events/PlayerClaimBuilder.kt
+++ src/main/kotlin/xyz/mizarc/solidclaims/events/PlayerClaimBuilder.kt
@@ -11,28 +11,99 @@ import kotlin.math.absoluteValue
  * @property firstLocation The location of the first claim corner.
  * @property secondLocation The location of the second claim corner.
  */
-class PlayerClaimBuilder(var playerId: UUID, var firstLocation: Location, var secondLocation: Location?) {
+class PlayerClaimBuilder(var playerId: UUID, var firstPosition: Pair<Int, Int>, var secondPosition: Pair<Int, Int>?) {
     /**
      * Creates a PlayerClaimBuilder object with only the first location value.
      * @param playerId The unique identifier of the player.
      * @param firstLocation The location of the first corner.
      */
-    constructor(playerId: UUID, firstLocation: Location) : this(playerId, firstLocation, null)
+    constructor(playerId: UUID, firstLocation: Pair<Int, Int>) : this(playerId, firstLocation, null)
 
     fun getBlockCount() : Int? {
-        if (secondLocation == null) {
+        if (secondPosition == null) {
             return null
         }
 
-        return ((secondLocation!!.x.toInt() - firstLocation.x.toInt() + 1) *
-                (secondLocation!!.z.toInt() - firstLocation.z.toInt() + 1)).absoluteValue
+        return ((secondPosition!!.first - firstPosition.first + 1) *
+                (secondPosition!!.second - firstPosition.second + 1)).absoluteValue
     }
 
     fun getXLength() : Int {
-        return (firstLocation.x - secondLocation!!.x).absoluteValue.toInt()
+        return (firstPosition.first - secondPosition!!.first).absoluteValue
     }
 
     fun getZLength() : Int {
-        return (firstLocation.z - secondLocation!!.z).absoluteValue.toInt()
+        return (firstPosition.second - secondPosition!!.second).absoluteValue
+    }
+
+    /**
+     * Gets the list of X and Z block positions that define the edges of a claim.
+     * @return A set of Integer pairs specifying the X and Z coordinates of each position.
+     */
+    fun getEdgeBlockPositions() : Array<Pair<Int, Int>> {
+        val blocks : ArrayList<Pair<Int, Int>> = ArrayList()
+        for (block in firstPosition.first..secondPosition!!.first) {
+            blocks.add(Pair(block, firstPosition.second))
+        }
+        for (block in firstPosition.first..secondPosition!!.first) {
+            blocks.add(Pair(block, secondPosition!!.second))
+        }
+        for (block in firstPosition.second..secondPosition!!.second) {
+            blocks.add(Pair(firstPosition.first, block))
+        }
+        for (block in firstPosition.second..secondPosition!!.second) {
+            blocks.add(Pair(secondPosition!!.first, block))
+        }
+        return blocks.toTypedArray()
+    }
+
+    fun getTopEdgeBlockPositions() : ArrayList<Pair<Int, Int>> {
+        val blocks : ArrayList<Pair<Int, Int>> = ArrayList()
+        for (block in firstPosition.first..secondPosition!!.first) {
+            println("oof")
+            blocks.add(Pair(block, secondPosition!!.second))
+        }
+        println("ree")
+        return blocks
+    }
+
+    fun getBottomEdgeBlockPositions() : ArrayList<Pair<Int, Int>> {
+        val blocks : ArrayList<Pair<Int, Int>> = ArrayList()
+        for (block in firstPosition.first..secondPosition!!.first.toInt()) {
+            blocks.add(Pair(block, firstPosition.second))
+        }
+        return blocks
+    }
+
+    fun getLeftEdgeBlockPositions() : ArrayList<Pair<Int, Int>> {
+        val blocks : ArrayList<Pair<Int, Int>> = ArrayList()
+        for (block in firstPosition.second..secondPosition!!.second) {
+            blocks.add(Pair(firstPosition.first, block))
+        }
+        return blocks
+    }
+
+    fun getRightEdgeBlockPositions() : ArrayList<Pair<Int, Int>> {
+        val blocks : ArrayList<Pair<Int, Int>> = ArrayList()
+        for (block in firstPosition.second..secondPosition!!.second) {
+            blocks.add(Pair(secondPosition!!.first, block))
+        }
+        return blocks
+    }
+
+    fun sortPositionSizes() {
+        if (firstPosition.first > secondPosition!!.first) {
+            val newFirstPosition = Pair(secondPosition!!.first, firstPosition.second)
+            val newSecondPosition = Pair(firstPosition.first, secondPosition!!.second)
+            firstPosition = newFirstPosition
+            secondPosition = newSecondPosition
+        }
+
+        if (firstPosition.second > secondPosition!!.second) {
+            val newFirstPosition = Pair(firstPosition.first, secondPosition!!.second)
+            val newSecondPosition = Pair(secondPosition!!.first, firstPosition.second)
+            firstPosition = newFirstPosition
+            secondPosition = newSecondPosition
+        }
     }
 }
