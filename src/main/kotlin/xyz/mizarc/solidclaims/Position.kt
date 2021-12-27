81386f1e36904b905a5e30678a1d680b1a703596
Mizarc <mizarc@protonmail.com>
2021-12-27 12:51:13 +1000

    Add new Position and Area objects to define claims
    Rather than making use of an integer pair to define a position value,
    the Position object explicitly defines an x and z coordinate. This
    reduces confusion in using the first and second keyword of a Pair object
    to get coordinates.
    
    The Area object reduces the amount of duplicate code that is generated
    in the partition, builder, and resizer objects. Rather than having to
    store the position values individually, this handles the storage of both
    position values to define an area, with functions that handle fetching
    position data moved to this class.

---
+++ src/main/kotlin/xyz/mizarc/solidclaims/Position.kt
@@ -0,0 +1,8 @@
+package xyz.mizarc.solidclaims
+
+/**
+ * Stores two integers to define a flat position in the world.
+ * @property x The X-Axis position.
+ * @property z The Z-Axis position.
+ */
+data class Position(val x: Int, val z: Int)
