205f276faca4b928b4af6f030a8a4a9a63b1454c
Mizarc <mizarc@protonmail.com>
2021-12-28 00:50:13 +1000

    Add alternate location constructor to position
    To make easy conversions from location to position objects, a
    constructor has been made which just takes a location object and
    converts it to a position.

--- src/main/kotlin/xyz/mizarc/solidclaims/Position.kt
+++ src/main/kotlin/xyz/mizarc/solidclaims/Position.kt
@@ -1,8 +1,12 @@
 package xyz.mizarc.solidclaims
 
+import org.bukkit.Location
+
 /**
  * Stores two integers to define a flat position in the world.
  * @property x The X-Axis position.
  * @property z The Z-Axis position.
  */
-data class Position(val x: Int, val z: Int)
+data class Position(val x: Int, val z: Int) {
+    constructor(location: Location): this(location.blockX, location.blockZ)
+}
