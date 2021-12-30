1886d7ab288a0b85969c2d0e686c920b02ddcc27
Mizarc <mizarc@protonmail.com>
2021-12-31 00:02:31 +1000

    Add adjacent partition owner check
    New partition creations will not link to an existing partition if the
    player does not own the adjacent partition. It will instead create a new
    claim for the player.

--- src/main/kotlin/xyz/mizarc/solidclaims/events/ClaimToolListener.kt
+++ src/main/kotlin/xyz/mizarc/solidclaims/events/ClaimToolListener.kt
@@ -113,21 +113,22 @@ class ClaimToolListener(val claimContainer: ClaimContainer, val playerContainer:
             return player.sendMessage("That selection overlaps an existing claim.")
         }
 
+        // Check if claim meets minimum size
         if (area.getXLength() < 5 || area.getZLength() < 5) {
             return player.sendMessage("The claim must be at least 5x5 blocks.")
         }
 
+        // Check if selection is greater than the player's remaining claim blocks
         val remainingClaimBlockCount = playerContainer.getPlayer(player.uniqueId)!!.getRemainingClaimBlockCount()
         val remainingClaimCount = playerContainer.getPlayer(player.uniqueId)!!.getRemainingClaimCount()
-
-        // Check if selection is greater than the player's remaining claim blocks
         if (area.getBlockCount() > remainingClaimBlockCount) {
             return player.sendMessage("That selection would require an additional " +
                     "${area.getBlockCount() - remainingClaimCount} claim blocks")
         }
 
+        // Check if new partition is adjacent to an existing claim.
         val adjacentPartition = claimContainer.getPartitionAdjacent(area, location.world!!)
-        if (adjacentPartition != null) {
+        if (adjacentPartition != null && player.uniqueId == adjacentPartition.claim.owner.uniqueId) {
             appendPartitionToClaim(player, claimBuilder, adjacentPartition.claim)
             return
         }
