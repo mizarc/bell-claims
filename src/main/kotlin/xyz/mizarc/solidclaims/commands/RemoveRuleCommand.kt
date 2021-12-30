b1b21c9848a7106459c94d797bceecae17db07e2
Mizarc <mizarc@protonmail.com>
2021-12-30 23:50:08 +1000

    Add permission overrides for commands
    If the player has claim override set, they are able to call these
    commands even if they don't own the claim.

--- src/main/kotlin/xyz/mizarc/solidclaims/commands/RemoveRuleCommand.kt
+++ src/main/kotlin/xyz/mizarc/solidclaims/commands/RemoveRuleCommand.kt
@@ -17,6 +17,17 @@ class RemoveRuleCommand : ClaimCommand() {
             return true
         }
 
+        // Check if player state exists
+        val playerState = plugin.playerContainer.getPlayer(player.uniqueId)
+        if (playerState == null) {
+            player.sendMessage("Somehow, your player data doesn't exist. Please contact an administrator.")
+            return true
+        }
+
+        if (playerState.claimOverride) {
+            return false
+        }
+
         if (player.uniqueId != claimPartition.claim.owner.uniqueId) {
             player.sendMessage("You don't have permission to modify this claim.")
             return true
