5a215643c7d852442b38bf1c7456e51311e04639
Mizarc <mizarc@protonmail.com>
2022-01-12 21:35:44 +1000

    Separate Partition references from Claim container
    The Partition object now removes the direct object reference to claim,
    removing the circular dependency in having to fetch partitions with an
    associated claim. This means that each function is now in reference to
    only the partition object itself, rather than making use of world or
    claim reference to get results for certain functions.
    
    Partitions have been moved to their own container, aptly named
    Repository. This new name will be the basis for future refactorings, as
    it better suits the name of the pattern it is based on. Functions in
    this are suited to only that of Partitions, usually involving their
    space in the world.

--- src/main/kotlin/xyz/mizarc/solidclaims/claims/Partition.kt
+++ src/main/kotlin/xyz/mizarc/solidclaims/claims/Partition.kt
@@ -1,6 +1,8 @@
 package xyz.mizarc.solidclaims.claims
 
 import org.bukkit.World
+import java.util.*
+import kotlin.collections.ArrayList
 
 /**
  * A partition of a claim. Claims can be made up of multiple partitions that defines the overall shape. A single
@@ -9,36 +11,60 @@ import org.bukkit.World
  * @property claim The claim linked to this partition.
  * @property area The area defining the space of this partition.
  */
-class Partition(var claim: Claim, var area: Area) {
-    fun isPositionInCorner(position: Position, world: World): Boolean {
-        return (area.isPositionInCorner(position) && (world.uid == claim.worldId))
-    }
+class Partition(var id: UUID, var claimId: UUID, var area: Area) {
     /**
-     * Checks whether the specified position in the world is within the bounds of this claim.
+     * Checks whether the specified position is within the bounds of this claim.
      * @param position The position to check for.
      * @param world The world of the position.
-     * @return True if the position is within the claim.
+     * @return True if the position is within the partition.
      */
-    fun isPositionInPartition(position: Position, world: World): Boolean {
-        return (area.isPositionInArea(position)) && (world.uid == claim.worldId)
+    fun isPositionInPartition(position: Position): Boolean {
+        return (area.isPositionInArea(position))
     }
 
     /**
-     * Checks whether the specified area in the world overlaps this claim
+     * Checks whether the specified position is in one of the corners of this claim.
+     * @param position The position to check for.
+     * @return True if the position is in a corner.
+     */
+    fun isPositionInCorner(position: Position): Boolean {
+        return (area.isPositionInCorner(position))
+    }
+
+    /**
+     * Checks whether the specified area overlaps this claim.
      * @param areaQuery The area to check.
      * @param world The world of the position.
      * @return True if the position is within the claim.
      */
-    fun isAreaOverlap(areaQuery: Area, world: World): Boolean {
-        return (area.isAreaOverlap(areaQuery)) && (world.uid == claim.worldId)
+    fun isAreaOverlap(areaQuery: Area): Boolean {
+        return (area.isAreaOverlap(areaQuery))
     }
 
     /**
-     * Checks if area is directly adjacent to this one.
+     * Checks if an area is directly adjacent to this one.
      * @param areaQuery The area to check.
      * @return True if area is adjacent.
      */
-    fun isAreaAdjacent(areaQuery: Area, world: World): Boolean {
-        return area.isAreaAdjacent(areaQuery) && (world.uid == claim.worldId)
+    fun isAreaAdjacent(areaQuery: Area): Boolean {
+        return area.isAreaAdjacent(areaQuery)
+    }
+
+    /**
+     * Gets a list of the chunks that this claim takes up.
+     * @return List of chunk positions.
+     */
+    fun getChunks(): ArrayList<Position> {
+        val firstChunk = area.lowerPosition.toChunk()
+        val secondChunk = area.upperPosition.toChunk()
+
+        val chunks: ArrayList<Position> = ArrayList()
+        for (x in firstChunk.x..secondChunk.x) {
+            for (z in firstChunk.z..secondChunk.z) {
+                chunks.add(Position(x, z))
+            }
+        }
+
+        return chunks
     }
 }
