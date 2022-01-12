aeaa639dd2c2a27820f6859c1f7da1d80d417ddc
Mizarc <mizarc@protonmail.com>
2022-01-12 21:35:44 +1000

    Separate Partition references from Claim container
    The Partition object now removes the direct object reference to claim,
    removing the circular dependency in having to fetch partitions with an
    associated claim. This means that each function is now in reference to
    only the partition object itself, rather than making use of world or
    claim reference to get results for certain functions.
    
    Partitions have been moved to their own container, aptly named
    Repository. This new name will be the basis for future refactorings, as
    it better suits the name of the pattern it is based on. Functions in
    this are suited to only that of Partitions, usually involving their
    space in the world.

---
+++ src/main/kotlin/xyz/mizarc/solidclaims/claims/PartitionRepository.kt
@@ -0,0 +1,124 @@
+package xyz.mizarc.solidclaims.claims
+
+import org.bukkit.World
+import xyz.mizarc.solidclaims.Container
+import xyz.mizarc.solidclaims.DatabaseStorage
+import java.sql.SQLException
+import java.util.*
+import kotlin.collections.ArrayList
+
+class PartitionRepository(private val storage: DatabaseStorage): Container<Partition> {
+    var partitions: ArrayList<Partition> = ArrayList()
+    var chunkPartitions: MutableMap<Position, ArrayList<Partition>> = mutableMapOf()
+
+    init {
+        createTable()
+    }
+
+    override fun getAll(): ArrayList<Partition> {
+        return partitions
+    }
+
+    fun getById(id: UUID): Partition? {
+        for (partition in partitions) {
+            if (partition.id == id) {
+                return partition
+            }
+        }
+        return null
+    }
+
+    fun getByClaim(claimId: UUID): ArrayList<Partition> {
+        val foundPartitions = ArrayList<Partition>()
+        for (partition in partitions) {
+            if (partition.claimId == claimId) {
+                foundPartitions.add(partition)
+            }
+        }
+        return foundPartitions
+    }
+
+    fun getByChunk(position: Position, world: World): ArrayList<Partition> {
+        if (chunkPartitions[position.toChunk()] == null) {
+            return ArrayList()
+        }
+        return chunkPartitions[position.toChunk()]!!
+    }
+
+    fun getByPosition(position: Position, world: World): Partition? {
+        val partitionsInChunk = getByChunk(position, world)
+        for (partition in partitionsInChunk) {
+            if (partition.isPositionInPartition(position)) {
+                return partition
+            }
+        }
+        return null
+    }
+
+    override fun add(entity: Partition) {
+        addToMemory(entity)
+        try {
+            storage.connection.executeUpdate("INSERT INTO claimPartitions (claimId, lowerPositionX, " +
+                    "lowerPositionZ, upperPositionX, upperPositionZ, main) VALUES (?,?,?,?,?,?);",
+                entity.claimId, entity.area.lowerPosition.x, entity.area.lowerPosition.z, entity.area.upperPosition.x,
+                entity.area.upperPosition.z)
+            return
+        } catch (error: SQLException) {
+            error.printStackTrace()
+        }
+    }
+
+    override fun update(entity: Partition) {
+        removeFromMemory(entity)
+        addToMemory(entity)
+        try {
+            storage.connection.executeUpdate("UPDATE claimPartitions SET lowerPositionX=?, lowerPositionZ=?, " +
+                    "upperPositionX=?, upperPositionZ=? WHERE id=?;", entity.area.lowerPosition.x,
+                entity.area.lowerPosition.z, entity.area.upperPosition.x, entity.area.upperPosition.z, entity.id)
+        } catch (error: SQLException) {
+            error.printStackTrace()
+        }
+    }
+
+    override fun remove(entity: Partition) {
+        removeFromMemory(entity)
+        try {
+            storage.connection.executeUpdate("DELETE FROM claimPartitions WHERE id=?;", entity.id)
+        } catch (error: SQLException) {
+            error.printStackTrace()
+        }
+        return
+    }
+
+    private fun addToMemory(entity: Partition) {
+        partitions.add(entity)
+        val claimChunks = entity.getChunks()
+        for (chunk in claimChunks) {
+            if (chunkPartitions[chunk] == null) {
+                chunkPartitions[chunk] = ArrayList()
+            }
+            chunkPartitions[chunk]?.add(entity)
+        }
+    }
+
+    private fun removeFromMemory(entity: Partition) {
+        partitions.remove(entity)
+        for (chunk in entity.area.getChunks()) {
+            val savedChunk = chunkPartitions[chunk] ?: return
+            savedChunk.remove(entity)
+        }
+    }
+
+    /**
+     * Creates a new table to store claim partition data if it doesn't exist.
+     */
+    private fun createTable() {
+        try {
+            storage.connection.executeUpdate("CREATE TABLE IF NOT EXISTS claimPartitions " +
+                    "(id TEXT, claimId TEXT, lowerPositionX INTEGER NOT NULL, lowerPositionZ INTEGER NOT NULL, " +
+                    "upperPositionX INTEGER NOT NULL, upperPositionZ INTEGER NOT NULL);")
+        } catch (error: SQLException) {
+            error.printStackTrace()
+        }
+    }
+}
