bf4944a18751a17dd9267e9cbde9f5ddcbe1dc64
Mizarc <mizarc@proton.me>
2023-07-12 23:55:47 +1000

    Change claim repo to support 3D position and icon

--- src/main/kotlin/xyz/mizarc/solidclaims/claims/ClaimRepository.kt
+++ src/main/kotlin/xyz/mizarc/solidclaims/claims/ClaimRepository.kt
@@ -1,7 +1,10 @@
 package xyz.mizarc.solidclaims.claims
 
 import org.bukkit.Bukkit
+import org.bukkit.Material
+import org.bukkit.OfflinePlayer
 import xyz.mizarc.solidclaims.partitions.Position
+import xyz.mizarc.solidclaims.partitions.Position3D
 import xyz.mizarc.solidclaims.storage.DatabaseStorage
 import java.sql.SQLException
 import java.time.Instant
@@ -24,8 +27,9 @@ class ClaimRepository(private val storage: DatabaseStorage) {
                     UUID.fromString(result.getString("worldId")),
                     Bukkit.getOfflinePlayer(UUID.fromString(result.getString("ownerId"))),
                     Instant.parse(result.getString("creationTime")), result.getString("name"),
-                    result.getString("description"), Position(result.getInt("positionX"),
-                    result.getInt("positionZ")))
+                    result.getString("description"), Position3D(result.getInt("positionX"),
+                        result.getInt("positionY"), result.getInt("positionZ")),
+                    Material.valueOf(result.getString("icon")))
                 claims[claim.id] = claim
             }
         } catch (error: SQLException) {
@@ -41,17 +45,15 @@ class ClaimRepository(private val storage: DatabaseStorage) {
         return claims[id]
     }
 
-    fun getByPlayer(playerId: UUID): Set<Claim> {
+    fun getByPlayer(player: OfflinePlayer): Set<Claim> {
         val foundClaims: MutableSet<Claim> = mutableSetOf()
         for (claim in claims) {
-            if (claim.value.owner == Bukkit.getOfflinePlayer(playerId)) {
-                foundClaims.add(claim.value)
-            }
+            foundClaims.add(claim.value)
         }
         return foundClaims
     }
 
-    fun getByPosition(position: Position): Claim? {
+    fun getByPosition(position: Position3D): Claim? {
         return claims.values.firstOrNull { it.position == position }
     }
 
@@ -59,8 +61,8 @@ class ClaimRepository(private val storage: DatabaseStorage) {
         claims[claim.id] = claim
         try {
             storage.connection.executeUpdate("INSERT INTO claims (id, worldId, ownerId, creationTime, " +
-                    "positionX, positionZ) VALUES (?,?,?,?,?,?);", claim.id, claim.worldId, claim.owner,
-                claim.creationTime, claim.position.x, claim.position.z)
+                    "positionX, positionY, positionZ, icon) VALUES (?,?,?,?,?,?,?,?);", claim.id, claim.worldId,
+                claim.owner, claim.creationTime, claim.position.x, claim.position.y, claim.position.z, claim.icon.name)
         } catch (error: SQLException) {
             error.printStackTrace()
         }
@@ -71,8 +73,8 @@ class ClaimRepository(private val storage: DatabaseStorage) {
         claims[claim.id] = claim
         try {
             storage.connection.executeUpdate("UPDATE claims SET worldId=?, ownerId=?, creationTime=?, " +
-                    "positionX=?, positionZ=? WHERE id=?;", claim.worldId, claim.owner,
-                claim.creationTime, claim.position.x, claim.position.z, claim.id)
+                    "positionX=?, positionY=?, positionZ=? icon=? WHERE id=?;", claim.worldId, claim.owner,
+                claim.creationTime, claim.position.x, claim.position.y, claim.position.z, claim.icon, claim.id)
         } catch (error: SQLException) {
             error.printStackTrace()
         }
@@ -94,7 +96,7 @@ class ClaimRepository(private val storage: DatabaseStorage) {
         try {
             storage.connection.executeUpdate("CREATE TABLE IF NOT EXISTS claims (id TEXT PRIMARY KEY, " +
                     "worldId TEXT NOT NULL, ownerId TEXT NOT NULL, creationTime TEXT NOT NULL, name TEXT, " +
-                    "description TEXT, positionX TEXT, positionZ, NOT NULL);")
+                    "description TEXT, positionX TEXT, positionY TEXT, positionZ TEXT, icon TEXT, NOT NULL);")
         } catch (error: SQLException) {
             error.printStackTrace()
         }
