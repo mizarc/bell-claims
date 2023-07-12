d8fb5b7cb5ff85d7880b79cb8175c3218aa7b8c4
Mizarc <mizarc@proton.me>
2023-07-12 23:55:26 +1000

    Change claim builder to have the default icon be a bell

--- src/main/kotlin/xyz/mizarc/solidclaims/claims/Claim.kt
+++ src/main/kotlin/xyz/mizarc/solidclaims/claims/Claim.kt
@@ -1,6 +1,7 @@
 package xyz.mizarc.solidclaims.claims
 
 import org.bukkit.Bukkit
+import org.bukkit.Material
 import org.bukkit.OfflinePlayer
 import org.bukkit.World
 import org.bukkit.entity.Player
@@ -8,6 +9,7 @@ import xyz.mizarc.solidclaims.partitions.Position
 import xyz.mizarc.solidclaims.partitions.Position3D
 import java.time.Instant
 import java.util.*
+import javax.swing.Icon
 
 /**
  * A claim object holds the data for the world its in and the players associated with it. It relies on partitions to
@@ -21,18 +23,18 @@ import java.util.*
  * @property partitions The partitions linked to this claim.
  */
 class Claim(var id: UUID, var worldId: UUID, var owner: OfflinePlayer, val creationTime: Instant,
-            var name: String, var description: String, var position: Position3D) {
+            var name: String, var description: String, var position: Position3D, var icon: Material) {
 
     /**
      * Compiles a new claim based on the world and owning player.
      * @param worldId The unique identifier of the world the claim is to be made in.
      * @param owner A reference to the owning player.
      */
-    constructor(worldId: UUID, owner: OfflinePlayer, creationTime: Instant, position: Position3D) : this(
-        UUID.randomUUID(), worldId, owner, creationTime, "", "", position)
+    constructor(worldId: UUID, owner: OfflinePlayer, creationTime: Instant, position: Position3D, icon:Material) : this(
+        UUID.randomUUID(), worldId, owner, creationTime, "", "", position, icon)
 
     constructor(builder: Builder): this(UUID.randomUUID(), builder.world.uid, builder.player, Instant.now(),
-        builder.name, builder.description, builder.position)
+        builder.name, builder.description, builder.position, builder.icon)
 
     /**
      * Gets a reference to the world if available.
@@ -45,6 +47,7 @@ class Claim(var id: UUID, var worldId: UUID, var owner: OfflinePlayer, val creat
     class Builder(val player: Player, val world: World, val position: Position3D) {
         var name = ""
         var description = ""
+        var icon = Material.BELL
 
         fun build() = Claim(this)
     }
