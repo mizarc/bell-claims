4821106af8e52dd289565f0942eff0a1aab1a477
Mizarc <mizarc@proton.me>
2023-07-13 19:57:37 +1000

    Add listener to open claim menu through bell interaction
    By shift right clicking on a bell block, the claim management menu will
    be displayed to the player. This will open either the creation menu or
    the edit menu depending on whether or not a claim already exists there.

---
+++ src/main/kotlin/xyz/mizarc/solidclaims/listeners/ClaimManagementListener.kt
@@ -0,0 +1,34 @@
+package xyz.mizarc.solidclaims.listeners
+
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.TextColor
+import org.bukkit.Bukkit
+import org.bukkit.Material
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.block.Action
+import org.bukkit.event.player.PlayerInteractEvent
+import xyz.mizarc.solidclaims.claims.Claim
+import xyz.mizarc.solidclaims.claims.ClaimRepository
+import xyz.mizarc.solidclaims.claims.ClaimRuleRepository
+import xyz.mizarc.solidclaims.claims.PlayerAccessRepository
+import xyz.mizarc.solidclaims.menus.ClaimManagementMenu
+import xyz.mizarc.solidclaims.partitions.Position3D
+
+class ClaimManagementListener(private val claimRepository: ClaimRepository,
+                              private val claimRuleRepository: ClaimRuleRepository,
+                              private val playerAccessRepository: PlayerAccessRepository): Listener {
+
+    @EventHandler
+    fun onPlayerClaimObjectInteract(event: PlayerInteractEvent) {
+        if (event.action != Action.RIGHT_CLICK_BLOCK) return
+        if ((event.clickedBlock?.type ?: return) != Material.BELL) return
+
+        if (event.player.isSneaking) {
+            val claimBuilder = Claim.Builder(event.player,
+                event.clickedBlock!!.location.world, Position3D(event.clickedBlock!!.location))
+            ClaimManagementMenu(claimRepository, playerAccessRepository, claimRuleRepository, claimBuilder)
+                .openClaimManagementMenu()
+        }
+    }
+}
