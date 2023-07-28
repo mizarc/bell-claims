7d9434a814680d13e5845333be6806dd42e24a43
Mizarc <mizarc@proton.me>
2023-07-28 15:32:58 +1000

    Fix being able to spread sculk onto claim areas
    Sculk can still spread if growing from within the claim, but you can't
    grow sculk from outside.

--- src/main/kotlin/dev/mizarc/bellclaims/listeners/MiscPreventions.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/listeners/MiscPreventions.kt
@@ -2,8 +2,11 @@ package dev.mizarc.bellclaims.listeners
 
 import dev.mizarc.bellclaims.ClaimService
 import dev.mizarc.bellclaims.PartitionService
+import org.bukkit.Bukkit
+import org.bukkit.Material
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.block.BlockSpreadEvent
 import org.bukkit.event.world.StructureGrowEvent
 
 class MiscPreventions(private val claimService: ClaimService,
@@ -31,6 +34,22 @@ class MiscPreventions(private val claimService: ClaimService,
             if (partitionService.getByLocation(block.location) != null) {
                 event.isCancelled = true
                 return
+            }
+        }
+    }
+
+    @EventHandler
+    fun onSculkSpread(event: BlockSpreadEvent) {
+        if (event.source.type == Material.SCULK_CATALYST) {
+            val partition = partitionService.getByLocation(event.block.location)
+            val sourcePartition = partitionService.getByLocation(event.source.location)
+
+            if (sourcePartition == partition) {
+                return
+            }
+
+            if (sourcePartition == null) {
+                event.isCancelled = true
             }
         }
     }
