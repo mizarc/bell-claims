98f70351076c9ec7f145c2a5909666d05b16f241
Mizarc <mizarc@proton.me>
2023-08-08 19:13:40 +1000

    Removed unused print statement

--- src/main/kotlin/dev/mizarc/bellclaims/listeners/ClaimEventHandler.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/listeners/ClaimEventHandler.kt
@@ -81,7 +81,6 @@ class ClaimEventHandler(var plugin: BellClaims,
      * executor has the highest priority, then invoke that executor.
      */
     private fun handleClaimPermission(listener: Listener, event: Event) {
-        Bukkit.getLogger().info(event.eventName)
         val eventPerms = ClaimPermission.getPermissionsForEvent(event::class.java).toMutableList() // Get all ClaimPermissions that deal with this event
 
         // Get the top PermissionExecutor that deals with this event.
