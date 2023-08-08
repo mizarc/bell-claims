2737722e4ab5549d0161fa4a583179802578c384
Mizarc <mizarc@proton.me>
2023-08-08 17:09:59 +1000

    Change entity permission to use "Mob" name

--- src/main/kotlin/dev/mizarc/bellclaims/utils/PermissionDisplay.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/utils/PermissionDisplay.kt
@@ -8,15 +8,15 @@ fun ClaimPermission.getIcon(): ItemStack {
     return when (this) {
         ClaimPermission.Build -> ItemStack(Material.DIAMOND_PICKAXE)
         ClaimPermission.ContainerInspect -> ItemStack(Material.CHEST)
-        ClaimPermission.DisplayTake -> ItemStack(Material.ARMOR_STAND)
+        ClaimPermission.DisplayManipulate -> ItemStack(Material.ARMOR_STAND)
         ClaimPermission.VehicleDeploy -> ItemStack(Material.MINECART)
         ClaimPermission.SignEdit -> ItemStack(Material.OAK_SIGN)
         ClaimPermission.RedstoneUse -> ItemStack(Material.LEVER)
         ClaimPermission.DoorOpen -> ItemStack(Material.ACACIA_DOOR)
         ClaimPermission.VillagerTrade -> ItemStack(Material.EMERALD)
-        ClaimPermission.EntityHurt -> ItemStack(Material.IRON_SWORD)
-        ClaimPermission.EntityLeash -> ItemStack(Material.LEAD)
-        ClaimPermission.EntityShear -> ItemStack(Material.SHEARS)
+        ClaimPermission.MobHurt -> ItemStack(Material.IRON_SWORD)
+        ClaimPermission.MobLeash -> ItemStack(Material.LEAD)
+        ClaimPermission.MobShear -> ItemStack(Material.SHEARS)
     }
 }
 
@@ -24,15 +24,15 @@ fun ClaimPermission.getDisplayName(): String {
     return when (this) {
         ClaimPermission.Build -> "Build"
         ClaimPermission.ContainerInspect -> "Container Inspect"
-        ClaimPermission.DisplayTake -> "Display Manipulate"
+        ClaimPermission.DisplayManipulate -> "Display Manipulate"
         ClaimPermission.VehicleDeploy -> "Vehicle Deploy"
         ClaimPermission.SignEdit -> "Sign Edit"
         ClaimPermission.RedstoneUse -> "Redstone Interact"
         ClaimPermission.DoorOpen -> "Door Open"
         ClaimPermission.VillagerTrade -> "Villager Trade"
-        ClaimPermission.EntityHurt -> "Mob Hurt"
-        ClaimPermission.EntityLeash -> "Mob Leash"
-        ClaimPermission.EntityShear -> "Mob Shear"
+        ClaimPermission.MobHurt -> "Mob Hurt"
+        ClaimPermission.MobLeash -> "Mob Leash"
+        ClaimPermission.MobShear -> "Mob Shear"
     }
 }
 
@@ -40,14 +40,14 @@ fun ClaimPermission.getDescription(): String {
     return when (this) {
         ClaimPermission.Build -> "Grants permission to build"
         ClaimPermission.ContainerInspect -> "Grants permission to open inventories (Chest, Furnace, Anvil)"
-        ClaimPermission.DisplayTake -> "Grants permission to manipulate display items (Item Frames, Armour Stands, Flower Pots)"
+        ClaimPermission.DisplayManipulate -> "Grants permission to manipulate display items (Item Frames, Armour Stands, Flower Pots)"
         ClaimPermission.VehicleDeploy -> "Grants permission to break and place vehicles (Boats, Minecarts)"
         ClaimPermission.SignEdit -> "Grants permission to edit signs"
         ClaimPermission.RedstoneUse -> "Grants permission to use redstone interactions (Buttons, Levers, Pressure Plates)"
         ClaimPermission.DoorOpen -> "Grants permission to open and close doors"
         ClaimPermission.VillagerTrade -> "Grants permission to trade with villagers"
-        ClaimPermission.EntityHurt -> "Grants permission to damage passive mobs"
-        ClaimPermission.EntityLeash -> "Grants permission to use leads on passive mobs"
-        ClaimPermission.EntityShear -> "Grants permission to use shears on passive mobs"
+        ClaimPermission.MobHurt -> "Grants permission to damage passive mobs"
+        ClaimPermission.MobLeash -> "Grants permission to use leads on passive mobs"
+        ClaimPermission.MobShear -> "Grants permission to use shears on passive mobs"
     }
 }
