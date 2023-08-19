ab21eaec7a880007374b72e4b03e1a754a9ac9b5
Mizarc <mizarc@proton.me>
2023-08-20 03:35:30 +1000

    Add basic structure of service interfaces
    Methods are very heavily subject to change. Since API methods will need
    to avoid changing as much as possible, the methods that are included
    will need heavy consideration.

--- src/main/kotlin/dev/mizarc/bellclaims/api/partitions/PartitionEditingService.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/api/partitions/PartitionEditingService.kt
@@ -1,7 +1,14 @@
 package dev.mizarc.bellclaims.api.partitions
 
+import dev.mizarc.bellclaims.domain.claims.Claim
+import dev.mizarc.bellclaims.domain.partitions.Area
+import dev.mizarc.bellclaims.domain.partitions.Partition
+import java.util.UUID
+
 interface PartitionEditingService {
-    fun addPartition()
-    fun resizePartition()
-    fun deletePartition()
+    fun getParititonById(uuid: UUID)
+    fun getPartitionsByClaim(claim: Claim)
+    fun addPartition(partition: Partition, claim: Claim)
+    fun resizePartition(partition: Partition, area: Area)
+    fun deletePartition(partition: Partition)
 }
