aed499a7a7f85a039b5197f8da8af4c1e54e8b08
Mizarc <mizarc@proton.me>
2023-08-23 03:22:38 +1000

    Change to interfaces in main BellClaims class
    This moves the concrete references to the onEnable method, rather than
    be a hardcoded part of the fields.

--- src/main/kotlin/dev/mizarc/bellclaims/BellClaims.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/BellClaims.kt
@@ -1,22 +1,26 @@
 package dev.mizarc.bellclaims
 
 import co.aikar.commands.PaperCommandManager
+import co.aikar.commands.annotation.Default
+import dev.mizarc.bellclaims.api.*
 import dev.mizarc.bellclaims.domain.claims.ClaimRepository
-import dev.mizarc.bellclaims.api.claims.ClaimService
+import dev.mizarc.bellclaims.domain.claims.ClaimPermissionRepository
+import dev.mizarc.bellclaims.domain.claims.ClaimRuleRepository
+import dev.mizarc.bellclaims.domain.claims.PlayerAccessRepository
 import dev.mizarc.bellclaims.domain.partitions.PartitionRepository
+import dev.mizarc.bellclaims.domain.players.PlayerStateRepository
 import net.milkbowl.vault.chat.Chat
 import org.bukkit.plugin.RegisteredServiceProvider
 import org.bukkit.plugin.java.JavaPlugin
-import dev.mizarc.bellclaims.infrastructure.Config
-import dev.mizarc.bellclaims.infrastructure.PartitionService
-import dev.mizarc.bellclaims.infrastructure.claims.ClaimPermissionRepository
-import dev.mizarc.bellclaims.infrastructure.claims.ClaimRepositorySQLite
-import dev.mizarc.bellclaims.infrastructure.claims.ClaimRuleRepository
-import dev.mizarc.bellclaims.infrastructure.claims.PlayerAccessRepository
-import dev.mizarc.bellclaims.infrastructure.ClaimServiceImpl
-import dev.mizarc.bellclaims.infrastructure.partitions.PartitionRepositorySQLite
-import dev.mizarc.bellclaims.infrastructure.players.PlayerStateRepository
-import dev.mizarc.bellclaims.infrastructure.storage.DatabaseStorage
+import dev.mizarc.bellclaims.infrastructure.persistence.Config
+import dev.mizarc.bellclaims.infrastructure.persistence.claims.ClaimRepositorySQLite
+import dev.mizarc.bellclaims.infrastructure.services.ClaimServiceImpl
+import dev.mizarc.bellclaims.infrastructure.persistence.partitions.PartitionRepositorySQLite
+import dev.mizarc.bellclaims.infrastructure.persistence.players.PlayerStateRepositoryMemory
+import dev.mizarc.bellclaims.infrastructure.persistence.DatabaseStorage
+import dev.mizarc.bellclaims.infrastructure.persistence.claims.ClaimRuleRepositorySQLite
+import dev.mizarc.bellclaims.infrastructure.persistence.claims.PlayerAccessRepositorySQLite
+import dev.mizarc.bellclaims.infrastructure.services.PartitionServiceImpl
 import dev.mizarc.bellclaims.interaction.commands.*
 import dev.mizarc.bellclaims.interaction.listeners.*
 
@@ -27,19 +31,31 @@ class BellClaims : JavaPlugin() {
     val storage = DatabaseStorage(this)
     private lateinit var claimRepo: ClaimRepository
     private lateinit var partitionRepo: PartitionRepository
-    val claimPermissionRepo = ClaimPermissionRepository(storage)
-    val claimRuleRepo = ClaimRuleRepository(storage)
-    val playerAccessRepo = PlayerAccessRepository(storage)
-    val playerStateRepo = PlayerStateRepository()
+    private lateinit var claimPermissionRepo: ClaimPermissionRepository
+    private lateinit var claimRuleRepo: ClaimRuleRepository
+    private lateinit var playerAccessRepo: PlayerAccessRepository
+    private lateinit var playerStateRepo: PlayerStateRepository
     private lateinit var claimService: ClaimService
-    val partitionService = PartitionService(config, claimService, partitionRepo)
+    private lateinit var partitionService: PartitionService
+    private lateinit var claimPermissionService: DefaultPermissionService
+    private lateinit var playerPermissionService: PlayerPermissionService
+    private lateinit var visualisationService: VisualisationService
+    private lateinit var playerStateService: PlayerStateService
     val claimVisualiser = ClaimVisualiser(this, claimService, partitionService, playerStateRepo)
 
     override fun onEnable() {
+        // Initialise Repositories
         claimRepo = ClaimRepositorySQLite(storage)
         partitionRepo = PartitionRepositorySQLite(storage)
+        claimRuleRepo = ClaimRuleRepositorySQLite(storage)
+        playerAccessRepo = PlayerAccessRepositorySQLite(storage)
+        playerStateRepo = PlayerStateRepositoryMemory()
+
+        // Initialise Services
         claimService = ClaimServiceImpl(claimRepo, partitionRepo, claimRuleRepo,
-            claimPermissionRepo, playerAccessRepo, playerStateRepo)
+            claimPermissionRepo, partitionService, playerStateService)
+        partitionService = PartitionServiceImpl(config, claimService, partitionRepo)
+
         logger.info(Chat::class.java.toString())
         val serviceProvider: RegisteredServiceProvider<Chat> = server.servicesManager.getRegistration(Chat::class.java)!!
         commandManager = PaperCommandManager(this)
@@ -60,7 +76,7 @@ class BellClaims : JavaPlugin() {
         commandManager.registerDependency(ClaimRuleRepository::class.java, claimRuleRepo)
         commandManager.registerDependency(ClaimPermissionRepository::class.java, claimPermissionRepo)
         commandManager.registerDependency(PlayerAccessRepository::class.java, playerAccessRepo)
-        commandManager.registerDependency(PlayerStateRepository::class.java, playerStateRepo)
+        commandManager.registerDependency(PlayerStateRepositoryMemory::class.java, playerStateRepo)
         commandManager.registerDependency(ClaimVisualiser::class.java, claimVisualiser)
         commandManager.registerDependency(ClaimService::class.java, claimService)
         commandManager.registerDependency(PartitionService::class.java, partitionService)
