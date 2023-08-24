fe764cf3d06a3829bc4e606daa449da0709d4a78
Mizarc <mizarc@proton.me>
2023-08-24 23:13:49 +1000

    Shorten method names for all services
    The change worked well for the partition service, so doing it to all
    services now.

--- src/main/kotlin/dev/mizarc/bellclaims/api/DefaultPermissionService.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/api/DefaultPermissionService.kt
@@ -3,11 +3,11 @@ package dev.mizarc.bellclaims.api
 import dev.mizarc.bellclaims.api.enums.DefaultPermissionChangeResult
 import dev.mizarc.bellclaims.domain.claims.Claim
 import dev.mizarc.bellclaims.interaction.listeners.ClaimPermission
-import org.bukkit.OfflinePlayer
 
 interface DefaultPermissionService {
-    fun getPermissions(claim: Claim): Set<ClaimPermission>
-    fun addAllPermissions(claim: Claim): DefaultPermissionChangeResult
-    fun removePermission(claim: Claim, claimPermission: ClaimPermission): DefaultPermissionChangeResult
-    fun removeAllPermissions(claim: Claim): DefaultPermissionChangeResult
+    fun getByClaim(claim: Claim): Set<ClaimPermission>
+    fun add(claim: Claim, permission: ClaimPermission): DefaultPermissionChangeResult
+    fun addAll(claim: Claim): DefaultPermissionChangeResult
+    fun remove(claim: Claim, permission: ClaimPermission): DefaultPermissionChangeResult
+    fun removeAll(claim: Claim): DefaultPermissionChangeResult
 }
