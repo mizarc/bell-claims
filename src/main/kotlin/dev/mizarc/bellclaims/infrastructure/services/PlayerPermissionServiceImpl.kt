9ce5af679a477e516b450f3a19b2e2a76df4d45b
Mizarc <mizarc@proton.me>
2023-08-25 00:06:35 +1000

    Add player permission service implementation

---
+++ src/main/kotlin/dev/mizarc/bellclaims/infrastructure/services/PlayerPermissionServiceImpl.kt
@@ -0,0 +1,50 @@
+package dev.mizarc.bellclaims.infrastructure.services
+
+import dev.mizarc.bellclaims.api.PlayerPermissionService
+import dev.mizarc.bellclaims.api.enums.DefaultPermissionChangeResult
+import dev.mizarc.bellclaims.api.enums.PlayerPermissionChangeResult
+import dev.mizarc.bellclaims.domain.claims.Claim
+import dev.mizarc.bellclaims.domain.claims.PlayerAccessRepository
+import dev.mizarc.bellclaims.interaction.listeners.ClaimPermission
+import org.bukkit.OfflinePlayer
+
+class PlayerPermissionServiceImpl(private val
+                                  playerAccessRepo: PlayerAccessRepository): PlayerPermissionService {
+    override fun getByPlayer(claim: Claim, player: OfflinePlayer): Set<ClaimPermission> {
+        return playerAccessRepo.getByPlayer(claim, player)
+    }
+
+    override fun addForPlayer(claim: Claim, player: OfflinePlayer,
+                              permission: ClaimPermission): PlayerPermissionChangeResult {
+        if (permission in getByPlayer(claim, player)) return PlayerPermissionChangeResult.PERMISSION_STATE_UNCHANGED
+        playerAccessRepo.add(claim, player, permission)
+        return PlayerPermissionChangeResult.SUCCESS
+    }
+
+    override fun addAllForPlayer(claim: Claim, player: OfflinePlayer): PlayerPermissionChangeResult {
+        val permissionsToAdd = ClaimPermission.values().toMutableList() - getByPlayer(claim, player)
+        if (permissionsToAdd.isEmpty()) DefaultPermissionChangeResult.PERMISSION_STATE_UNCHANGED
+
+        for (permission in permissionsToAdd) {
+            playerAccessRepo.add(claim, player, permission)
+        }
+        return PlayerPermissionChangeResult.SUCCESS
+    }
+
+    override fun removeForPlayer(claim: Claim, player: OfflinePlayer,
+                                 permission: ClaimPermission): PlayerPermissionChangeResult {
+        if (permission !in getByPlayer(claim, player)) return PlayerPermissionChangeResult.PERMISSION_STATE_UNCHANGED
+        playerAccessRepo.remove(claim, player, permission)
+        return PlayerPermissionChangeResult.SUCCESS
+    }
+
+    override fun removeAllForPlayer(claim: Claim, player: OfflinePlayer): PlayerPermissionChangeResult {
+        val permissionsToRemove = getByPlayer(claim, player)
+        if (permissionsToRemove.isEmpty()) DefaultPermissionChangeResult.PERMISSION_STATE_UNCHANGED
+
+        for (permission in permissionsToRemove) {
+            playerAccessRepo.remove(claim, player, permission)
+        }
+        return PlayerPermissionChangeResult.SUCCESS
+    }
+}
