897db1434ba9e90ba25520555397e06f71420f60
Mizarc <mizarc@proton.me>
2023-08-24 21:55:25 +1000

    Fix default permission service method using player argument
    Player shouldn't be part of the argument since this is for default claim
    permissions.

--- src/main/kotlin/dev/mizarc/bellclaims/api/DefaultPermissionService.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/api/DefaultPermissionService.kt
@@ -6,7 +6,7 @@ import dev.mizarc.bellclaims.interaction.listeners.ClaimPermission
 import org.bukkit.OfflinePlayer
 
 interface DefaultPermissionService {
-    fun getPermissions(claim: Claim, player: OfflinePlayer): Set<ClaimPermission>
+    fun getPermissions(claim: Claim): Set<ClaimPermission>
     fun addAllPermissions(claim: Claim): DefaultPermissionChangeResult
     fun removePermission(claim: Claim, claimPermission: ClaimPermission): DefaultPermissionChangeResult
     fun removeAllPermissions(claim: Claim): DefaultPermissionChangeResult
