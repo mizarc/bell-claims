0f61cde5aa4a93ed91b147d5b227caa5d01fda14
Mizarc <mizarc@proton.me>
2023-08-26 01:03:06 +1000

    Add doesPlayerHavePermission method to permission service
    This checks to see if the player has a permission within a claim.
    Basically a shortcut to getting the permissions for the player first.

--- src/main/kotlin/dev/mizarc/bellclaims/api/PlayerPermissionService.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/api/PlayerPermissionService.kt
@@ -4,9 +4,11 @@ import dev.mizarc.bellclaims.api.enums.PlayerPermissionChangeResult
 import dev.mizarc.bellclaims.domain.claims.Claim
 import dev.mizarc.bellclaims.interaction.listeners.ClaimPermission
 import org.bukkit.OfflinePlayer
+import java.util.*
 
 interface PlayerPermissionService {
-    fun getByClaim(claim: Claim): Map<OfflinePlayer, Set<ClaimPermission>>
+    fun doesPlayerHavePermission(claim: Claim, player: OfflinePlayer, permission: ClaimPermission): Boolean
+    fun getByClaim(claim: Claim): Map<UUID, Set<ClaimPermission>>
     fun getByPlayer(claim: Claim, player: OfflinePlayer): Set<ClaimPermission>
     fun addForPlayer(claim: Claim, player: OfflinePlayer, permission: ClaimPermission): PlayerPermissionChangeResult
     fun addAllForPlayer(claim: Claim, player: OfflinePlayer): PlayerPermissionChangeResult
