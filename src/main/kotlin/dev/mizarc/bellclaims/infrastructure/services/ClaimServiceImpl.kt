636a4596f551b898cdaa6c0f313350125c9113eb
Mizarc <mizarc@proton.me>
2023-08-29 02:06:05 +1000

    Move methods that involve both claim and partition to new service
    Due to a circular dependency, methods in the claim service that require
    queries to the partition service have been moved to its own service.
    This new services focuses on queries and actions done to claims by world
    information.

--- src/main/kotlin/dev/mizarc/bellclaims/infrastructure/services/ClaimServiceImpl.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/infrastructure/services/ClaimServiceImpl.kt
@@ -1,10 +1,6 @@
 package dev.mizarc.bellclaims.infrastructure.services
 
 import dev.mizarc.bellclaims.api.ClaimService
-import dev.mizarc.bellclaims.api.PartitionService
-import dev.mizarc.bellclaims.api.PlayerStateService
-import dev.mizarc.bellclaims.api.enums.ClaimCreationResult
-import dev.mizarc.bellclaims.api.enums.ClaimMoveResult
 import dev.mizarc.bellclaims.domain.claims.*
 import dev.mizarc.bellclaims.domain.partitions.*
 import org.bukkit.Location
@@ -16,8 +12,7 @@ class ClaimServiceImpl(private val claimRepo: ClaimRepository,
                        private val partitionRepo: PartitionRepository,
                        private val claimRuleRepo: ClaimRuleRepository,
                        private val claimPermissionRepo: ClaimPermissionRepository,
-                       private val partitionService: PartitionService,
-                       private val playerStateService: PlayerStateService): ClaimService {
+                       private val playerPermissionRepo: PlayerAccessRepository): ClaimService {
 
     override fun getById(id: UUID): Claim? {
         return claimRepo.getById(id)
@@ -25,10 +20,6 @@ class ClaimServiceImpl(private val claimRepo: ClaimRepository,
 
     override fun getByPlayer(player: OfflinePlayer): Set<Claim> {
         return claimRepo.getByPlayer(player)
-    }
-
-    override fun getByLocation(location: Location): Claim? {
-        return claimRepo.getByPosition(Position3D(location))
     }
 
     override fun getBlockCount(claim: Claim): Int {
@@ -42,30 +33,6 @@ class ClaimServiceImpl(private val claimRepo: ClaimRepository,
 
     override fun getPartitionCount(claim: Claim): Int {
         return partitionRepo.getByClaim(claim).count()
-    }
-
-    override fun create(name: String, location: Location, player: OfflinePlayer): ClaimCreationResult {
-        val claim = Claim(location.world.uid, player, Position3D(location), name)
-        val area = Area(
-            Position2D(claim.position.x - 5, claim.position.z - 5),
-            Position2D(claim.position.x + 5, claim.position.z + 5))
-
-        // Handle failure types
-        if (location.block.type != Material.BELL) {
-            return ClaimCreationResult.NOT_A_BELL
-        }
-        else if (partitionService.isAreaValid(area, claim)) {
-            return ClaimCreationResult.TOO_CLOSE
-        }
-        else if (playerStateService.getRemainingClaimBlockCount(player) <= 0) {
-            return ClaimCreationResult.OUT_OF_CLAIMS
-        }
-
-        // Store the claim and associated partition
-        val partition = Partition(claim.id, area)
-        claimRepo.add(claim)
-        partitionRepo.add(partition)
-        return ClaimCreationResult.SUCCESS
     }
 
     override fun changeName(claim: Claim, name: String) {
@@ -83,23 +50,11 @@ class ClaimServiceImpl(private val claimRepo: ClaimRepository,
         claimRepo.update(claim)
     }
 
-    override fun changeLocation(claim: Claim, location: Location): ClaimMoveResult {
-        // Handle failure types
-        val partition = partitionService.getByLocation(location)
-        
-        if (partition == null || partition.claimId != claim.id) {
-            return ClaimMoveResult.OUTSIDE_OF_AREA
-        }
-
-        claim.position = Position3D(location)
-        claimRepo.update(claim)
-        return ClaimMoveResult.SUCCESS
-    }
-
     override fun destroy(claim: Claim) {
         partitionRepo.removeByClaim(claim)
         claimRuleRepo.removeByClaim(claim)
         claimPermissionRepo.removeByClaim(claim)
+        playerPermissionRepo.removeByClaim(claim)
         claimRepo.remove(claim)
     }
 }
---
+++ src/main/kotlin/dev/mizarc/bellclaims/infrastructure/services/ClaimWorldServiceImpl.kt
@@ -0,0 +1,59 @@
+package dev.mizarc.bellclaims.infrastructure.services
+
+import dev.mizarc.bellclaims.api.ClaimWorldService
+import dev.mizarc.bellclaims.api.PartitionService
+import dev.mizarc.bellclaims.api.PlayerStateService
+import dev.mizarc.bellclaims.api.enums.ClaimCreationResult
+import dev.mizarc.bellclaims.api.enums.ClaimMoveResult
+import dev.mizarc.bellclaims.domain.claims.Claim
+import dev.mizarc.bellclaims.domain.claims.ClaimRepository
+import dev.mizarc.bellclaims.domain.partitions.Area
+import dev.mizarc.bellclaims.domain.partitions.Position2D
+import dev.mizarc.bellclaims.domain.partitions.Position3D
+import org.bukkit.Location
+import org.bukkit.Material
+import org.bukkit.OfflinePlayer
+
+class ClaimWorldServiceImpl(private val claimRepo: ClaimRepository,
+                            private val partitionService: PartitionService,
+                            private val playerStateService: PlayerStateService): ClaimWorldService {
+    override fun isNewLocationValid(location: Location): Boolean {
+        val area = Area(
+            Position2D(location.blockX - 5, location.blockZ - 5),
+            Position2D(location.blockX + 5, location.blockZ + 5))
+        return partitionService.isAreaValid(area, location.world)
+    }
+
+    override fun isMoveLocationValid(claim: Claim, location: Location): Boolean {
+        val partition = partitionService.getByLocation(location) ?: return false
+        return partition.claimId == claim.id
+    }
+
+    override fun getByLocation(location: Location): Claim? {
+        return claimRepo.getByPosition(Position3D(location), location.world.uid)
+    }
+
+    override fun create(name: String, location: Location, player: OfflinePlayer): ClaimCreationResult {
+        val area = Area(
+            Position2D(location.blockX - 5, location.blockZ - 5),
+            Position2D(location.blockX + 5, location.blockZ + 5))
+
+        // Handle failure types
+        if (location.block.type != Material.BELL) return ClaimCreationResult.NOT_A_BELL
+        else if (partitionService.isAreaValid(area, location.world)) return ClaimCreationResult.TOO_CLOSE
+        else if (playerStateService.getRemainingClaimBlockCount(player) <= 0) return ClaimCreationResult.OUT_OF_CLAIMS
+
+        // Store the claim and associated partition
+        val claim = Claim(location.world.uid, player, Position3D(location), name)
+        claimRepo.add(claim)
+        partitionService.append(area, claim)
+        return ClaimCreationResult.SUCCESS
+    }
+
+    override fun move(claim: Claim, location: Location): ClaimMoveResult {
+        if (isMoveLocationValid(claim, location)) return ClaimMoveResult.OUTSIDE_OF_AREA
+        claim.position = Position3D(location)
+        claimRepo.update(claim)
+        return ClaimMoveResult.SUCCESS
+    }
+}
