a1e103392e0b6a022e5003eeaef57f8349b54fef
Mizarc <mizarc@proton.me>
2023-08-29 21:25:34 +1000

    Add method to player state service to get by player
    You can now use an OfflinePlayer reference directly instead of having to
    extract the id from it first.

--- src/main/kotlin/dev/mizarc/bellclaims/api/PlayerStateService.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/api/PlayerStateService.kt
@@ -1,12 +1,14 @@
 package dev.mizarc.bellclaims.api
 
 import dev.mizarc.bellclaims.domain.players.PlayerState
+import dev.mizarc.bellclaims.domain.players.PlayerStateRepository
 import org.bukkit.OfflinePlayer
 import java.util.UUID
 
 interface PlayerStateService {
     fun getAllOnline(): Set<PlayerState>
     fun getById(id: UUID): PlayerState?
+    fun getByPlayer(player: OfflinePlayer): PlayerState?
     fun getTotalClaimCount(player: OfflinePlayer): Int
     fun getTotalClaimBlockCount(player: OfflinePlayer): Int
     fun getUsedClaimsCount(player: OfflinePlayer): Int
