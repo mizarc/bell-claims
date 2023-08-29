9ededfd625757dfc78dbcde10da1f2fbd7322482
Mizarc <mizarc@proton.me>
2023-08-29 21:27:35 +1000

    Update additional listeners to use service references

--- src/main/kotlin/dev/mizarc/bellclaims/interaction/listeners/PlayerRegistrationListener.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/interaction/listeners/PlayerRegistrationListener.kt
@@ -4,17 +4,16 @@ import net.milkbowl.vault.chat.Chat
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
-import dev.mizarc.bellclaims.infrastructure.Config
-import dev.mizarc.bellclaims.infrastructure.players.PlayerStateRepository
+import dev.mizarc.bellclaims.infrastructure.persistence.Config
 import dev.mizarc.bellclaims.domain.players.PlayerState
+import dev.mizarc.bellclaims.domain.players.PlayerStateRepository
 
 class PlayerRegistrationListener(val config: Config, val metadata: Chat,
-                                 val playerStateRepository: PlayerStateRepository
-) : Listener {
+                                 val playerStateRepo: PlayerStateRepository) : Listener {
 
     @EventHandler
     fun onPlayerJoin(event: PlayerJoinEvent) {
         val playerState = PlayerState(event.player, config, metadata)
-        playerStateRepository.add(playerState)
+        playerStateRepo.add(playerState)
     }
 }
