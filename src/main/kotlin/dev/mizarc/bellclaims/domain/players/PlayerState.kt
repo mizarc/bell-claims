18cbd8525a8a1ad9333f160c7c7cf8cc79abc878
Mizarc <mizarc@proton.me>
2023-08-30 01:32:05 +1000

    Remove metadata and config dependencies for PlayerState

--- src/main/kotlin/dev/mizarc/bellclaims/domain/players/PlayerState.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/domain/players/PlayerState.kt
@@ -1,14 +1,12 @@
 package dev.mizarc.bellclaims.domain.players
 
-import net.milkbowl.vault.chat.Chat
 import org.bukkit.Bukkit
 import org.bukkit.OfflinePlayer
 import org.bukkit.entity.Player
-import dev.mizarc.bellclaims.infrastructure.persistence.Config
 import dev.mizarc.bellclaims.domain.claims.Claim
 import dev.mizarc.bellclaims.domain.partitions.Position3D
 
-class PlayerState(private val player: OfflinePlayer, private val config: Config, private val metadata: Chat) {
+class PlayerState(val player: OfflinePlayer) {
     var claimOverride = false
     var claimToolMode = 0
     var isHoldingClaimTool = false
