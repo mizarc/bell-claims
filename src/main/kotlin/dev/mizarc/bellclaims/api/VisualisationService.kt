71b490012d9ad64f8477027abbbeedb24eace5e7
Mizarc <mizarc@proton.me>
2023-08-29 16:01:50 +1000

    Refactor methods in Visualiser service to explicitly say 3D
    By having it named this way, it shows that these methods are used to get
    exact 3D coordinates for the positions rather than 2D coordinates.

--- src/main/kotlin/dev/mizarc/bellclaims/api/VisualisationService.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/api/VisualisationService.kt
@@ -8,6 +8,6 @@ import dev.mizarc.bellclaims.domain.partitions.Position3D
 interface VisualisationService {
     fun getOuterBorders(claim: Claim): Set<Position2D>
     fun getPartitionedBorders(claim: Claim): Map<Partition, Set<Position2D>>
-    fun getSpecificOuterBorders(claim: Claim): Set<Position3D>
-    fun getSpecificPartitionedBorders(claim: Claim): Map<Partition, Set<Position3D>>
+    fun get3DOuterBorders(claim: Claim): Set<Position3D>
+    fun get3DPartitionedBorders(claim: Claim): Map<Partition, Set<Position3D>>
 }
