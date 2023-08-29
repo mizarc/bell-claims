2db3b56157477b434971496430b990cf9ca75b1f
Mizarc <mizarc@proton.me>
2023-08-30 01:30:20 +1000

    Remove claim limit methods from PlayerState
    No need to have these methods here since they don't rely on any data
    that exists in the object. They exist in the service layer now.

--- src/main/kotlin/dev/mizarc/bellclaims/domain/players/PlayerState.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/domain/players/PlayerState.kt
@@ -4,11 +4,11 @@ import net.milkbowl.vault.chat.Chat
 import org.bukkit.Bukkit
 import org.bukkit.OfflinePlayer
 import org.bukkit.entity.Player
-import dev.mizarc.bellclaims.infrastructure.Config
+import dev.mizarc.bellclaims.infrastructure.persistence.Config
 import dev.mizarc.bellclaims.domain.claims.Claim
 import dev.mizarc.bellclaims.domain.partitions.Position3D
 
-class PlayerState(val player: OfflinePlayer, val config: Config, val metadata: Chat) {
+class PlayerState(private val player: OfflinePlayer, private val config: Config, private val metadata: Chat) {
     var claimOverride = false
     var claimToolMode = 0
     var isHoldingClaimTool = false
@@ -18,12 +18,4 @@ class PlayerState(val player: OfflinePlayer, val config: Config, val metadata: C
     fun getOnlinePlayer(): Player? {
         return Bukkit.getPlayer(player.uniqueId)
     }
-
-    fun getClaimLimit(): Int =
-        metadata.getPlayerInfoInteger(getOnlinePlayer(), "bellclaims.claim_limit", config.claimLimit)
-            .takeIf { it > -1 } ?: -1
-
-    fun getClaimBlockLimit(): Int =
-        metadata.getPlayerInfoInteger(getOnlinePlayer(), "bellclaims.claim_block_limit", config.claimBlockLimit)
-            .takeIf { it > -1 } ?: -1
 }
