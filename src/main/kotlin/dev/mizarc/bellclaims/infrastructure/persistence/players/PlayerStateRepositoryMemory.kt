801ea2db4ace36e0e65ae3c86428e543328f74b1
Mizarc <mizarc@proton.me>
2023-08-30 01:43:46 +1000

    Change implementation of player state repo to actually use overrides
    Now it properly implements the methods as required by the repo.

--- src/main/kotlin/dev/mizarc/bellclaims/infrastructure/persistence/players/PlayerStateRepositoryMemory.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/infrastructure/persistence/players/PlayerStateRepositoryMemory.kt
@@ -1,34 +1,29 @@
-package dev.mizarc.bellclaims.infrastructure.players
+package dev.mizarc.bellclaims.infrastructure.persistence.players
 
 import dev.mizarc.bellclaims.domain.players.PlayerState
+import dev.mizarc.bellclaims.domain.players.PlayerStateRepository
 import org.bukkit.OfflinePlayer
 import java.util.*
 
 /**
  * Holds a collection of every player on the server.
  */
-class PlayerStateRepository {
+class PlayerStateRepositoryMemory: PlayerStateRepository {
     var playerStates: MutableMap<UUID, PlayerState> = mutableMapOf()
 
-    /**
-     * Gets a specific player state.
-     * @param player The player to fetch.
-     * @return A PlayerState object of the player. May return null.
-     */
-    fun getAll() : Set<PlayerState> {
+    override fun getAll() : Set<PlayerState> {
         return playerStates.values.toSet()
     }
 
-
-    fun get(player: OfflinePlayer) : PlayerState? {
+    override fun get(player: OfflinePlayer) : PlayerState? {
         return playerStates[player.uniqueId]
     }
 
-    fun add(playerState: PlayerState) {
+    override fun add(playerState: PlayerState) {
         playerStates[playerState.player.uniqueId] = playerState
     }
 
-    fun removePlayer(playerState: PlayerState){
+    override fun remove(playerState: PlayerState){
         playerStates.remove(playerState.player.uniqueId)
     }
 }
