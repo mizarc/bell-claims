f552431be957e0fd32d4dd4e69c1f92a3732d327
Mizarc <mizarc@proton.me>
2023-08-30 13:24:37 +1000

    Change registration listener to use new service methods

--- src/main/kotlin/dev/mizarc/bellclaims/interaction/listeners/PlayerRegistrationListener.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/interaction/listeners/PlayerRegistrationListener.kt
@@ -1,19 +1,13 @@
 package dev.mizarc.bellclaims.interaction.listeners
 
-import net.milkbowl.vault.chat.Chat
+import dev.mizarc.bellclaims.api.PlayerStateService
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
-import dev.mizarc.bellclaims.infrastructure.persistence.Config
-import dev.mizarc.bellclaims.domain.players.PlayerState
-import dev.mizarc.bellclaims.domain.players.PlayerStateRepository
 
-class PlayerRegistrationListener(val config: Config, val metadata: Chat,
-                                 val playerStateRepo: PlayerStateRepository) : Listener {
-
+class PlayerRegistrationListener(private val playerStateService: PlayerStateService) : Listener {
     @EventHandler
     fun onPlayerJoin(event: PlayerJoinEvent) {
-        val playerState = PlayerState(event.player, config, metadata)
-        playerStateRepo.add(playerState)
+        playerStateService.registerPlayer(event.player)
     }
 }
