31a63f54bbdacf85ceb62ab8de404b6ba9777cde
Mizarc <mizarc@proton.me>
2023-08-30 12:27:23 +1000

    Fill in implementation details of PartitionModificationEvent
    Don't know if this works yet, but it now implements the Event class that
    is used by Bukkit.

--- src/main/kotlin/dev/mizarc/bellclaims/api/events/PartitionModificationEvent.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/api/events/PartitionModificationEvent.kt
@@ -1,4 +1,12 @@
 package dev.mizarc.bellclaims.api.events
 
-class PartitionModificationEvent {
+import dev.mizarc.bellclaims.domain.partitions.Partition
+import org.bukkit.event.Event
+import org.bukkit.event.HandlerList
+
+class PartitionModificationEvent(val partition: Partition): Event() {
+    private val handlerList: HandlerList = HandlerList()
+    override fun getHandlers(): HandlerList {
+        return handlerList
+    }
 }
