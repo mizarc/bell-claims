c3a1a80134dc899f7115fc7a2ef71dcc1d04ee64
Mizarc <mizarc@proton.me>
2023-08-30 13:25:56 +1000

    Add listener to unregister player on quit

--- src/main/kotlin/dev/mizarc/bellclaims/interaction/listeners/PlayerRegistrationListener.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/interaction/listeners/PlayerRegistrationListener.kt
@@ -4,10 +4,16 @@ import dev.mizarc.bellclaims.api.PlayerStateService
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.event.player.PlayerQuitEvent
 
 class PlayerRegistrationListener(private val playerStateService: PlayerStateService) : Listener {
     @EventHandler
     fun onPlayerJoin(event: PlayerJoinEvent) {
         playerStateService.registerPlayer(event.player)
+    }
+
+    @EventHandler
+    fun onPlayerLeave(event: PlayerQuitEvent) {
+        playerStateService.unregisterPlayer(event.player)
     }
 }
