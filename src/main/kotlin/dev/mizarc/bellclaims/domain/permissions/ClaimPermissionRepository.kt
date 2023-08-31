a2f2738d89c1bd4dff1e97d02e59702216afd282
Mizarc <mizarc@proton.me>
2023-09-01 02:56:55 +1000

    Fix ClaimPermissionRepo not having a concrete implementation
    The concrete implementation wasn't inheriting the interface, so it
    wasn't ever registered.

--- src/main/kotlin/dev/mizarc/bellclaims/domain/permissions/ClaimPermissionRepository.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/domain/permissions/ClaimPermissionRepository.kt
@@ -3,6 +3,7 @@ package dev.mizarc.bellclaims.domain.permissions
 import dev.mizarc.bellclaims.domain.claims.Claim
 
 interface ClaimPermissionRepository {
+    fun doesClaimHavePermission(claim: Claim, permission: ClaimPermission): Boolean
     fun getByClaim(claim: Claim): MutableSet<ClaimPermission>
     fun add(claim: Claim, permission: ClaimPermission)
     fun remove(claim: Claim, permission: ClaimPermission)
