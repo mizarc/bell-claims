faca17f32c88a0a1a46db1ddb89a8e2e29511288
Mizarc <mizarc@proton.me>
2023-08-31 22:56:05 +1000

    Add more specific methods to visualisation service
    This now includes separate methods to get the main partition
    specifically. The 3D visualiser now requires a render location parameter
    in order to make use of the upper and lower ranges.

--- src/main/kotlin/dev/mizarc/bellclaims/api/VisualisationService.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/api/VisualisationService.kt
@@ -4,10 +4,17 @@ import dev.mizarc.bellclaims.domain.claims.Claim
 import dev.mizarc.bellclaims.domain.partitions.Partition
 import dev.mizarc.bellclaims.domain.partitions.Position2D
 import dev.mizarc.bellclaims.domain.partitions.Position3D
+import org.bukkit.Location
 
 interface VisualisationService {
     fun getOuterBorders(claim: Claim): Set<Position2D>
     fun getPartitionedBorders(claim: Claim): Map<Partition, Set<Position2D>>
-    fun get3DOuterBorders(claim: Claim): Set<Position3D>
-    fun get3DPartitionedBorders(claim: Claim): Map<Partition, Set<Position3D>>
+    fun getPartitionedCorners(claim: Claim): Map<Partition, Set<Position2D>>
+    fun getMainPartitionBorders(claim: Claim): Set<Position2D>
+    fun getMainPartitionCorners(claim: Claim): Set<Position2D>
+    fun get3DOuterBorders(claim: Claim, renderLocation: Location): Set<Position3D>
+    fun get3DPartitionedBorders(claim: Claim, renderLocation: Location): Map<Partition, Set<Position3D>>
+    fun get3DPartitionedCorners(claim: Claim, renderLocation: Location): Map<Partition, Set<Position2D>>
+    fun get3DMainPartitionBorders(claim: Claim, renderLocation: Location): Set<Position2D>
+    fun get3DMainPartitionCorners(claim: Claim, renderLocation: Location): Set<Position2D>
 }
