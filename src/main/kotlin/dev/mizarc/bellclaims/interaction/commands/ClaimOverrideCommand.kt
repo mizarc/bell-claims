13984636b1930fb7894fd77460dfb01f5907ac88
Mizarc <mizarc@proton.me>
2023-09-02 02:26:04 +1000

    Fix remaining interaction references to repositories
    Interactions should interface exclusively through services, rather than
    the repository directly as it is then able to go through additional
    checks and have outputs for action outcomes.

--- src/main/kotlin/dev/mizarc/bellclaims/interaction/commands/ClaimOverrideCommand.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/interaction/commands/ClaimOverrideCommand.kt
@@ -5,18 +5,20 @@ import co.aikar.commands.annotation.CommandAlias
 import co.aikar.commands.annotation.CommandPermission
 import co.aikar.commands.annotation.Default
 import co.aikar.commands.annotation.Dependency
+import dev.mizarc.bellclaims.api.PlayerStateService
+import dev.mizarc.bellclaims.domain.players.PlayerStateRepository
 import org.bukkit.entity.Player
 import dev.mizarc.bellclaims.infrastructure.persistence.players.PlayerStateRepositoryMemory
 
 @CommandAlias("claimoverride")
 class ClaimOverrideCommand: BaseCommand() {
     @Dependency
-    lateinit var players: PlayerStateRepositoryMemory
+    lateinit var playerState: PlayerStateService
 
     @Default
     @CommandPermission("bellclaims.command.claimoverride")
     fun onClaimOverride(player: Player) {
-        val playerState = players.get(player)
+        val playerState = playerState.getByPlayer(player)
         if (playerState == null) {
             player.sendMessage("§cSomehow, your player data doesn't exist. Please contact an administrator.")
             return
