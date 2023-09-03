6c878b5d6e3e98e2df24a0cd9f4a9707c885b367
Mizarc <mizarc@proton.me>
2023-09-03 17:16:29 +1000

    Change getByClaim method to return Set instead of MutableSet

--- src/main/kotlin/dev/mizarc/bellclaims/domain/flags/ClaimFlagRepository.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/domain/flags/ClaimFlagRepository.kt
@@ -3,7 +3,7 @@ package dev.mizarc.bellclaims.domain.flags
 import dev.mizarc.bellclaims.domain.claims.Claim
 
 interface ClaimFlagRepository {
-    fun getByClaim(claim: Claim): MutableSet<Flag>
+    fun getByClaim(claim: Claim): Set<Flag>
     fun add(claim: Claim, rule: Flag)
     fun remove(claim: Claim, rule: Flag)
     fun removeByClaim(claim: Claim)
