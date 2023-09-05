bfc2130784204b042c623e7e3961e11d6e2145b8
Mizarc <mizarc@proton.me>
2023-09-05 11:34:33 +1000

    Add separate given, when, and then comments for Flag Service tests

--- src/test/kotlin/dev/mizarc/bellclaims/infrastructure/services/FlagServiceImplTest.kt
+++ src/test/kotlin/dev/mizarc/bellclaims/infrastructure/services/FlagServiceImplTest.kt
@@ -30,122 +30,176 @@ class FlagServiceImplTest {
 
     @Test
     fun `doesClaimHaveFlag - returns true if flag in claim`() {
-        // Mock a claim that contains the flags "Explosions" and "Pistons"
+        // Given
         val flags = setOf(Flag.Explosions, Flag.Pistons)
         every { flagRepo.getByClaim(claim) } returns flags
 
-        assertTrue(flagService.doesClaimHaveFlag(claim, Flag.Explosions),
+        // When
+        val result1 = flagService.doesClaimHaveFlag(claim, Flag.Explosions)
+        val result2 = flagService.doesClaimHaveFlag(claim, Flag.MobGriefing)
+
+        // Then
+        assertTrue(result1,
             "Claim contains Explosions flag.")
-        assertFalse(flagService.doesClaimHaveFlag(claim, Flag.MobGriefing),
+        assertFalse(result2,
             "Claim does not contain MobGriefing flag.")
     }
 
     @Test
     fun `getByClaim - returns expected flags`() {
-        // Mock a claim that contains the flags "Explosions" and "Pistons"
+        // Given
         val flags = setOf(Flag.Explosions, Flag.Pistons)
         every { flagRepo.getByClaim(claim) } returns flags
 
-        assertEquals(setOf(Flag.Explosions, Flag.Pistons), flagService.getByClaim(claim),
+        // When
+        val result = flagService.getByClaim(claim)
+
+        // Then
+        assertEquals(setOf(Flag.Explosions, Flag.Pistons), result,
             "Claim contains Explosions and Pistons.")
-        assertNotEquals(setOf(Flag.MobGriefing, Flag.FireSpread), flagService.getByClaim(claim),
+        assertNotEquals(setOf(Flag.MobGriefing, Flag.FireSpread), result,
             "Claim does not contain MobGriefing or FireSpread.")
     }
 
     @Test
     fun `add - when flag is not in repo - returns SUCCESS`() {
+        // Given
         val flag = Flag.Explosions
         every { flagRepo.getByClaim(claim) } returns emptySet()
         every { flagRepo.add(claim, flag) } returns Unit
 
-        assertEquals(FlagChangeResult.SUCCESS, flagService.add(claim, flag))
+        // When
+        val result = flagService.add(claim, flag)
+
+        // Then
+        assertEquals(FlagChangeResult.SUCCESS, result)
         verify { flagRepo.add(claim, flag) }
     }
 
     @Test
     fun `add - when flag is in repo - returns UNCHANGED`() {
+        // Given
         val flag = Flag.Explosions
         every { flagRepo.getByClaim(claim) } returns setOf(flag)
 
-        assertEquals(FlagChangeResult.UNCHANGED, flagService.add(claim, flag))
+        // When
+        val result = flagService.add(claim, flag)
+
+        // Then
+        assertEquals(FlagChangeResult.UNCHANGED, result)
         verify(exactly = 0) { flagRepo.add(any(), any()) }
     }
 
     @Test
     fun `addAll - when no flags are in repo - returns SUCCESS`() {
+        // Given
         every { flagRepo.getByClaim(claim) } returns emptySet()
         every { flagRepo.add(claim, any()) } returns Unit
 
+        // When
         val result = flagService.addAll(claim)
 
+        // Then
         assertEquals(FlagChangeResult.SUCCESS, result)
         verify { flagRepo.add(claim, any()) }
     }
 
     @Test
     fun `addAll - when some flags are in repo - returns SUCCESS`() {
+        // Given
         val flags = setOf(Flag.Explosions, Flag.Pistons)
         every { flagRepo.getByClaim(claim) } returns flags
         every { flagRepo.add(claim, any()) } returns Unit
 
-        assertEquals(FlagChangeResult.SUCCESS, flagService.addAll(claim))
+        // When
+        val result = flagService.addAll(claim)
+
+        // Then
+        assertEquals(FlagChangeResult.SUCCESS, result)
         verify { flagRepo.add(any(), any()) }
     }
 
     @Test
     fun `addAll - when all flags are in repo - returns UNCHANGED`() {
+        // Given
         every { flagRepo.getByClaim(claim) } returns Flag.entries.toSet()
 
+        // When
         val result = flagService.addAll(claim)
 
+        // Then
         assertEquals(FlagChangeResult.UNCHANGED, result)
         verify(exactly = 0) { flagRepo.add(any(), any()) }
     }
 
     @Test
     fun `remove - when flag is in repo - returns SUCCESS`() {
+        // Given
         val flag = Flag.Explosions
         every { flagRepo.getByClaim(claim) } returns setOf(flag)
         every { flagRepo.remove(claim, flag) } returns Unit
 
-        assertEquals(FlagChangeResult.SUCCESS, flagService.remove(claim, flag))
+        // When
+        val result = flagService.remove(claim, flag)
+
+        // Then
+        assertEquals(FlagChangeResult.SUCCESS, result)
         verify { flagRepo.remove(claim, flag) }
     }
 
     @Test
     fun `remove - when flag is not in repo - returns UNCHANGED`() {
+        // Given
         val flag = Flag.Explosions
         every { flagRepo.getByClaim(claim) } returns emptySet()
 
+        // When
         val result = flagService.remove(claim, flag)
+
+        // Then
         assertEquals(FlagChangeResult.UNCHANGED, result)
         verify(exactly = 0) { flagRepo.remove(any(), any()) }
     }
 
     @Test
     fun `removeAll - when all flags are in repo - returns SUCCESS`() {
+        // Given
         every { flagRepo.getByClaim(claim) } returns Flag.entries.toSet()
         every { flagRepo.remove(claim, any()) } returns Unit
 
-        assertEquals(FlagChangeResult.SUCCESS, flagService.removeAll(claim))
+        // When
+        val result = flagService.removeAll(claim)
+
+        // Then
+        assertEquals(FlagChangeResult.SUCCESS, result)
         verify { flagRepo.remove(any(), any()) }
     }
 
     @Test
     fun `removeAll - when some flags are in repo - returns SUCCESS`() {
+        // Given
         val flags = setOf(Flag.Explosions, Flag.Pistons)
         every { flagRepo.getByClaim(claim) } returns flags
         every { flagRepo.remove(claim, any()) } returns Unit
 
-        assertEquals(FlagChangeResult.SUCCESS, flagService.removeAll(claim))
+        // When
+        val result = flagService.removeAll(claim)
+
+        // Then
+        assertEquals(FlagChangeResult.SUCCESS, result)
         verify { flagRepo.remove(any(), any()) }
     }
 
     @Test
     fun `removeAll - when no flags are in repo - returns UNCHANGED`() {
+        // Given
         every { flagRepo.getByClaim(claim) } returns emptySet()
 
-        assertEquals(FlagChangeResult.UNCHANGED, flagService.removeAll(claim))
+        // When
+        val result = flagService.removeAll(claim)
+
+        // Then
+        assertEquals(FlagChangeResult.UNCHANGED, result)
         verify(exactly = 0) { flagRepo.remove(any(), any()) }
     }
 }
