181fef6c799ca6f08e0e3f30b4d9b0c87cabf215
Mizarc <mizarc@proton.me>
2023-09-11 23:34:36 +1000

    Change player state repo to get by id instead of player
    This makes it so that you don't have to rely on Bukkit services to
    access the repository. That logic should stay in the service layer.

--- src/main/kotlin/dev/mizarc/bellclaims/infrastructure/services/PlayerStateServiceImpl.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/infrastructure/services/PlayerStateServiceImpl.kt
@@ -1,20 +1,17 @@
 package dev.mizarc.bellclaims.infrastructure.services
 
 import dev.mizarc.bellclaims.api.PlayerStateService
-import dev.mizarc.bellclaims.domain.claims.ClaimRepository
-import dev.mizarc.bellclaims.domain.partitions.PartitionRepository
+import dev.mizarc.bellclaims.api.enums.PlayerRegisterResult
+import dev.mizarc.bellclaims.api.enums.PlayerUnregisterResult
 import dev.mizarc.bellclaims.domain.players.PlayerState
 import dev.mizarc.bellclaims.domain.players.PlayerStateRepository
-import dev.mizarc.bellclaims.infrastructure.persistence.Config
-import net.milkbowl.vault.chat.Chat
-import org.bukkit.Bukkit
 import org.bukkit.OfflinePlayer
 import org.bukkit.entity.Player
 import java.util.*
 
 class PlayerStateServiceImpl(private val playerStateRepo: PlayerStateRepository): PlayerStateService {
     override fun isPlayerRegistered(player: OfflinePlayer): Boolean {
-        return playerStateRepo.getAll().any { it.player == player }
+        return playerStateRepo.getAll().any { it.player.uniqueId == player.uniqueId }
     }
 
     override fun getAllOnline(): Set<PlayerState> {
@@ -22,20 +19,22 @@ class PlayerStateServiceImpl(private val playerStateRepo: PlayerStateRepository)
     }
 
     override fun getById(id: UUID): PlayerState? {
-        val player = Bukkit.getOfflinePlayer(id)
-        return playerStateRepo.get(player)
+        return playerStateRepo.get(id)
     }
 
     override fun getByPlayer(player: OfflinePlayer): PlayerState? {
-        return playerStateRepo.get(player)
+        return playerStateRepo.get(player.uniqueId)
     }
 
-    override fun registerPlayer(player: Player) {
+    override fun registerPlayer(player: Player): PlayerRegisterResult {
+        if (isPlayerRegistered(player)) return PlayerRegisterResult.UNCHANGED
         playerStateRepo.add(PlayerState(player))
+        return PlayerRegisterResult.SUCCESS
     }
 
-    override fun unregisterPlayer(player: OfflinePlayer) {
-        val playerState = getById(player.uniqueId) ?: return
+    override fun unregisterPlayer(player: OfflinePlayer): PlayerUnregisterResult {
+        val playerState = getById(player.uniqueId) ?: return PlayerUnregisterResult.UNCHANGED
         playerStateRepo.remove(playerState)
+        return PlayerUnregisterResult.SUCCESS
     }
 }
