83f3391c5dadea99a1b8b74c405e742441181091
Mizarc <mizarc@proton.me>
2023-09-12 12:49:08 +1000

    Add tests for VisualisationServiceImpl

---
+++ src/main/kotlin/dev/mizarc/bellclaims/utils/IntUtils.kt
@@ -0,0 +1,6 @@
+package dev.mizarc.bellclaims.utils
+
+infix fun Int.toward(to: Int): IntProgression {
+    val step = if (this > to) -1 else 1
+    return IntProgression.fromClosedRange(this, to, step)
+}
