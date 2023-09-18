0490ca8eeb63ad3fc2b20c5f5849fc387b01b905
Mizarc <mizarc@proton.me>
2023-09-19 02:13:55 +1000

    Add documentation for all result enum methods

--- src/main/kotlin/dev/mizarc/bellclaims/api/enums/PlayerPermissionChangeResult.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/api/enums/PlayerPermissionChangeResult.kt
@@ -1,5 +1,8 @@
 package dev.mizarc.bellclaims.api.enums
 
+/**
+ * Represents the result of changing a player's permissions for a claim.
+ */
 enum class PlayerPermissionChangeResult {
     CLAIM_DOES_NOT_EXIST,
     PLAYER_DOES_NOT_EXIST,
