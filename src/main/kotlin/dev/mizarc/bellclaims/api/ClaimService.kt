4f3b5ab9d53168aa30b4ccb0d4b448f60f304d88
Mizarc <mizarc@proton.me>
2023-09-19 02:09:02 +1000

    Add documentation for all service methods

--- src/main/kotlin/dev/mizarc/bellclaims/api/ClaimService.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/api/ClaimService.kt
@@ -1,9 +1,6 @@
 package dev.mizarc.bellclaims.api
 
-import dev.mizarc.bellclaims.api.enums.ClaimCreationResult
-import dev.mizarc.bellclaims.api.enums.ClaimMoveResult
 import dev.mizarc.bellclaims.domain.claims.Claim
-import org.bukkit.Location
 import org.bukkit.Material
 import org.bukkit.OfflinePlayer
 import java.util.UUID
@@ -20,11 +17,58 @@ interface ClaimService {
      */
     fun getById(id: UUID): Claim?
 
+    /**
+     * Gets all claims owned by a player.
+     *
+     * @param player The player to query.
+     * @return The set of claims that belong to the player.
+     */
     fun getByPlayer(player: OfflinePlayer): Set<Claim>
+
+    /**
+     * Gets the amount of blocks that the claim occupies.
+     *
+     * @param claim The claim to query.
+     * @return The number of blocks.
+     */
     fun getBlockCount(claim: Claim): Int
+
+    /**
+     * Gets the amount of partitions that belong to a claim.
+     *
+     * @param claim The claim to query.
+     * @return The number of partitions.
+     */
     fun getPartitionCount(claim: Claim): Int
+
+    /**
+     * Changes the name of the claim.
+     *
+     * @param claim The target claim.
+     * @param name The new name.
+     */
     fun changeName(claim: Claim, name: String)
+
+    /**
+     * Changes the description of the claim.
+     *
+     * @param claim The target claim.
+     * @param description The new description.
+     */
     fun changeDescription(claim: Claim, description: String)
+
+    /**
+     * Changes the icon of a claim using a material type.
+     *
+     * @param claim The target claim.
+     * @param material The material to use.
+     */
     fun changeIcon(claim: Claim, material: Material)
+
+    /**
+     * Deletes a claim and all its associated data.
+     *
+     * @param claim The claim to delete.
+     */
     fun destroy(claim: Claim)
 }
--- src/main/kotlin/dev/mizarc/bellclaims/api/DefaultPermissionService.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/api/DefaultPermissionService.kt
@@ -4,10 +4,49 @@ import dev.mizarc.bellclaims.api.enums.DefaultPermissionChangeResult
 import dev.mizarc.bellclaims.domain.claims.Claim
 import dev.mizarc.bellclaims.domain.permissions.ClaimPermission
 
+/**
+ * A service that handles the modification of default permissions for a claim, allowing access for all players.
+ */
 interface DefaultPermissionService {
+    /**
+     * Gets the set of permissions associated with a claim.
+     *
+     * @param claim The claim to retrieve permissions from.
+     * @return The Set of permissions attached to the claim.
+     */
     fun getByClaim(claim: Claim): Set<ClaimPermission>
+
+    /**
+     * Adds the specified permission to the given claim.
+     *
+     * @param claim The claim to add the permission to.
+     * @param permission The permission to be added to the claim.
+     * @return The result of adding the permission to the claim.
+     */
     fun add(claim: Claim, permission: ClaimPermission): DefaultPermissionChangeResult
+
+    /**
+     * Adds all available permissions to the given claim.
+     *
+     * @param claim The claim to add the permissions to.
+     * @return The result of adding all available permissions to the claim.
+     */
     fun addAll(claim: Claim): DefaultPermissionChangeResult
+
+    /**
+     * Removes the specified permission from the given claim.
+     *
+     * @param claim The claim to remove the permission from.
+     * @param permission The permission to be removed from the claim.
+     * @return The result of removing the permission from the claim.
+     */
     fun remove(claim: Claim, permission: ClaimPermission): DefaultPermissionChangeResult
+
+    /**
+     * Removes all permissions from the given claim.
+     *
+     * @param claim The claim to remove the permissions from.
+     * @return The result of removing all permissions from the claim.
+     */
     fun removeAll(claim: Claim): DefaultPermissionChangeResult
 }
