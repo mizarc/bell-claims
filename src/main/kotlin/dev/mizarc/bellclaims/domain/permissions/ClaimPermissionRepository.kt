f399200930360048db0cc655de42ac622fac373e
Mizarc <mizarc@proton.me>
2023-09-19 22:09:58 +1000

    Add documentation for the permission domain.

--- src/main/kotlin/dev/mizarc/bellclaims/domain/permissions/ClaimPermissionRepository.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/domain/permissions/ClaimPermissionRepository.kt
@@ -2,10 +2,47 @@ package dev.mizarc.bellclaims.domain.permissions
 
 import dev.mizarc.bellclaims.domain.claims.Claim
 
+/**
+ * A repository that handles the persistence of claim permissions.
+ */
 interface ClaimPermissionRepository {
+    /**
+     * Checks whether the given claim contains a permission.
+     *
+     * @param claim The claim to query.
+     * @param permission The permission to check for.
+     * @return True if the claim has the permission.
+     */
     fun doesClaimHavePermission(claim: Claim, permission: ClaimPermission): Boolean
+
+    /**
+     * Gets all default permissions linked to a given claim.
+     *
+     * @param claim The claim to query.
+     * @return The set of all default permissions.
+     */
     fun getByClaim(claim: Claim): Set<ClaimPermission>
+
+    /**
+     * Adds a new permission to a claim.
+     *
+     * @param claim The claim to add to.
+     * @param permission The permission to add.
+     */
     fun add(claim: Claim, permission: ClaimPermission)
+
+    /**
+     * Removes an existing permission from a claim.
+     *
+     * @param claim The claim to remove from.
+     * @param permission The permission to remove.
+     */
     fun remove(claim: Claim, permission: ClaimPermission)
+
+    /**
+     * Removes all permissions linked to a claim.
+     *
+     * @param claim The claim to remove from.
+     */
     fun removeByClaim(claim: Claim)
 }
