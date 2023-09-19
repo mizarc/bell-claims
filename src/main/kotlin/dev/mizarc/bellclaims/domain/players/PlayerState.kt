cf66303789157a7389e83c4ace5a3be46b349467
Mizarc <mizarc@proton.me>
2023-09-19 16:38:00 +1000

    Add documentation for the Player State domain.

--- src/main/kotlin/dev/mizarc/bellclaims/domain/players/PlayerState.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/domain/players/PlayerState.kt
@@ -6,6 +6,11 @@ import org.bukkit.entity.Player
 import dev.mizarc.bellclaims.domain.claims.Claim
 import dev.mizarc.bellclaims.domain.partitions.Position3D
 
+/**
+ * Holds temporary player state data mainly pertaining to claim editing.
+ *
+ * @property player The reference to the player.
+ */
 class PlayerState(val player: OfflinePlayer) {
     var claimOverride = false
     var claimToolMode = 0
@@ -13,6 +18,11 @@ class PlayerState(val player: OfflinePlayer) {
     var isVisualisingClaims = false
     var visualisedBlockPositions: MutableMap<Claim, Set<Position3D>> = mutableMapOf()
 
+    /**
+     * Gets the online version of the player instance.
+     *
+     * @return The online player instance.
+     */
     fun getOnlinePlayer(): Player? {
         return Bukkit.getPlayer(player.uniqueId)
     }
