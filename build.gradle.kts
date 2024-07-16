f668102e3a3a59b3f88da39b9008d2fdbb1adf04
DESKTOP-939K91A\Mizarc <mizarc@proton.me>
2024-07-16 23:43:34 +1000

    Update HikariCP dependency to 5.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21-R0.1-SNAPSHOT")
     shadow("org.jetbrains.kotlin:kotlin-stdlib")
     implementation ("org.slf4j:slf4j-nop:2.0.7")
-    implementation("com.zaxxer:HikariCP:5.0.1")
+    implementation("com.zaxxer:HikariCP:5.1.0")
     implementation("co.aikar:acf-paper:0.5.1-SNAPSHOT")
     implementation("co.aikar:idb-core:1.0.0-SNAPSHOT")
     implementation("com.github.stefvanschie.inventoryframework:IF:0.10.14")
