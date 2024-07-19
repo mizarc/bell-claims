fd970759c9731fee5e226c0e5831d5f465fe2a7e
DESKTOP-939K91A\Mizarc <mizarc@proton.me>
2024-07-20 02:45:44 +1000

    Update slf4j-nop dependency to 2.0.13

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
     compileOnly("io.papermc.paper:paper-api:1.21-R0.1-SNAPSHOT")
     shadow("org.jetbrains.kotlin:kotlin-stdlib")
-    implementation ("org.slf4j:slf4j-nop:2.0.7")
+    implementation ("org.slf4j:slf4j-nop:2.0.13")
     implementation("com.zaxxer:HikariCP:5.1.0")
     implementation("co.aikar:acf-paper:0.5.1-SNAPSHOT")
     implementation("co.aikar:idb-core:1.0.0-SNAPSHOT")
