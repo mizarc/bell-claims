3291d5428b2b10ebbbcc84a1f9c0ef710047d5c4
Mizarc <contact@mizarc.dev>
2025-04-05 14:59:22 +1000

    Add action for getting claim details
    This is currently being used in the add flag command

--- src/main/kotlin/dev/mizarc/bellclaims/interaction/commands/AddFlagCommand.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/interaction/commands/AddFlagCommand.kt
@@ -3,27 +3,40 @@ package dev.mizarc.bellclaims.interaction.commands
 import co.aikar.commands.annotation.CommandAlias
 import co.aikar.commands.annotation.CommandPermission
 import co.aikar.commands.annotation.Subcommand
-import dev.mizarc.bellclaims.application.enums.FlagChangeResult
+import dev.mizarc.bellclaims.application.actions.AddFlagToClaim
+import dev.mizarc.bellclaims.application.actions.GetClaimDetails
+import dev.mizarc.bellclaims.application.enums.AddFlagToClaimResult
 import org.bukkit.entity.Player
 import dev.mizarc.bellclaims.domain.values.Flag
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
 
 @CommandAlias("claim")
-class AddFlagCommand : ClaimCommand() {
+class AddFlagCommand : ClaimCommand(), KoinComponent {
+    private val addFlagToClaim: AddFlagToClaim by inject()
+    private val getClaimDetails: GetClaimDetails by inject()
+
     @Subcommand("addflag")
     @CommandPermission("bellclaims.command.claim.addflag")
-    fun onFlag(player: Player, rule: Flag) {
+    fun onFlag(player: Player, flag: Flag) {
+        // Gets the partition at the player's current location
         val partition = getPartitionAtPlayer(player) ?: return
-        if (!isPlayerHasClaimPermission(player, partition)) {
-            return
-        }
+        if (!isPlayerHasClaimPermission(player, partition)) return
 
-        val claim = claimService.getById(partition.claimId) ?: return
-        when (flagService.add(claim, rule)) {
-            FlagChangeResult.UNCHANGED ->
-                player.sendMessage("§6${claim.name} §calready has §6${rule}§c.")
-            FlagChangeResult.SUCCESS ->
-                player.sendMessage("§aAdded §6$rule §afor §6${claim.name}§a.")
-            else -> player.sendMessage("Unknown Error.")
+        // Add flag to the claim
+        when (addFlagToClaim.execute(flag, partition.claimId)) {
+            AddFlagToClaimResult.Success -> {
+                val claimName = getClaimDetails.execute(partition.claimId)?.name ?: "(Could not retrieve name)"
+                player.sendMessage("Flag $flag has been added to claim $claimName.")
+            }
+            AddFlagToClaimResult.AlreadyExists -> {
+                val claimName = getClaimDetails.execute(partition.claimId)?.name ?: "(Could not retrieve name)"
+                player.sendMessage("Claim $claimName already has $flag.")
+            }
+            AddFlagToClaimResult.ClaimNotFound ->
+                player.sendMessage("Claim was not found.")
+            AddFlagToClaimResult.StorageError ->
+                player.sendMessage("An internal error has occurred, contact your administrator for support.")
         }
     }
 }
