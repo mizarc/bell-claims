290742909a8940611f1a0475af80df468ebcd878
Mizarc <contact@mizarc.dev>
2025-04-06 22:10:07 +1000

    Add actions for getting flags and permissions

---
+++ src/main/kotlin/dev/mizarc/bellclaims/application/actions/GetClaimFlags.kt
@@ -0,0 +1,11 @@
+package dev.mizarc.bellclaims.application.actions
+
+import dev.mizarc.bellclaims.application.persistence.ClaimFlagRepository
+import dev.mizarc.bellclaims.domain.values.Flag
+import java.util.*
+
+class GetClaimFlags(private val flagRepository: ClaimFlagRepository) {
+    fun execute(claimId: UUID): List<Flag> {
+        return flagRepository.getByClaim(claimId).toList()
+    }
+}
