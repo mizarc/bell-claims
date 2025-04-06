f53cb463c62e89d2092fc91aa8acbae337125140
Mizarc <contact@mizarc.dev>
2025-04-06 13:52:03 +1000

    Add new update claim name action

---
+++ src/main/kotlin/dev/mizarc/bellclaims/application/actions/UpdateClaimName.kt
@@ -0,0 +1,23 @@
+package dev.mizarc.bellclaims.application.actions
+
+import dev.mizarc.bellclaims.application.enums.UpdateClaimAttributeResult
+import dev.mizarc.bellclaims.application.errors.DatabaseOperationException
+import dev.mizarc.bellclaims.application.persistence.ClaimRepository
+import java.util.UUID
+
+class UpdateClaimName(private val claimRepository: ClaimRepository) {
+    fun execute(claimId: UUID, name: String): UpdateClaimAttributeResult {
+        // Check if claim exists
+        val existingClaim = claimRepository.getById(claimId) ?: return UpdateClaimAttributeResult.ClaimNotFound
+
+        // Change Name and persist to storage
+        existingClaim.name = name
+        try {
+            claimRepository.update(existingClaim)
+            return UpdateClaimAttributeResult.Success
+        } catch (error: DatabaseOperationException) {
+            println("Error has occurred trying to save to the database")
+            return UpdateClaimAttributeResult.StorageError
+        }
+    }
+}
