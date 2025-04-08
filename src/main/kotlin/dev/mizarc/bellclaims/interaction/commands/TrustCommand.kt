251e48038b63bbb9b8d73ecd105f03591835c800
Mizarc <contact@mizarc.dev>
2025-04-08 19:38:54 +1000

    Rework trust command to use new action

--- src/main/kotlin/dev/mizarc/bellclaims/interaction/commands/TrustCommand.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/interaction/commands/TrustCommand.kt
@@ -2,31 +2,42 @@ package dev.mizarc.bellclaims.interaction.commands
 
 import co.aikar.commands.annotation.*
 import co.aikar.commands.bukkit.contexts.OnlinePlayer
-import dev.mizarc.bellclaims.application.results.PlayerPermissionChangeResult
-import org.bukkit.Bukkit
+import dev.mizarc.bellclaims.application.actions.AddClaimPlayerPermission
+import dev.mizarc.bellclaims.application.actions.GetClaimDetails
+import dev.mizarc.bellclaims.application.results.AddClaimPlayerPermissionResult
 import org.bukkit.entity.Player
 import dev.mizarc.bellclaims.domain.values.ClaimPermission
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
 
 @CommandAlias("claim")
-class TrustCommand : ClaimCommand() {
+class TrustCommand : ClaimCommand(), KoinComponent {
+    private val addClaimPlayerPermission: AddClaimPlayerPermission by inject()
+    private val getClaimDetails: GetClaimDetails by inject()
+
     @Subcommand("trust")
     @CommandPermission("bellclaims.command.claim.trust")
     fun onTrust(player: Player, otherPlayer: OnlinePlayer, permission: ClaimPermission) {
+        // Gets the partition at the player's current location
         val partition = getPartitionAtPlayer(player) ?: return
-        val claim = claimService.getById(partition.claimId) ?: return
-        if (!isPlayerHasClaimPermission(player, partition)) {
-            return
-        }
+        if (!isPlayerHasClaimPermission(player, partition)) return
 
         // Add permission for player and output result
-        when (playerPermissionService.addForPlayer(claim, otherPlayer.player, permission)) {
-            PlayerPermissionChangeResult.UNCHANGED ->
-                player.sendMessage("§6${Bukkit.getPlayer(otherPlayer.player.uniqueId)?.name} §calready has " +
-                        "permission §6${permission.name}§c.")
-            PlayerPermissionChangeResult.SUCCESS ->
-                player.sendMessage("§6${Bukkit.getPlayer(otherPlayer.player.uniqueId)?.name} §ahas been given " +
-                        "permission §6${permission.name} §afor this claim.")
-            else -> player.sendMessage("Unknown Error.")
+        when (addClaimPlayerPermission.execute(partition.claimId, otherPlayer.player.uniqueId, permission)) {
+            AddClaimPlayerPermissionResult.Success -> {
+                val claimName = getClaimDetails.execute(partition.claimId)?.name ?: "(Could not retrieve name)"
+                player.sendMessage("Permission $permission has been assigned to player " +
+                        "${otherPlayer.player.displayName()} in claim $claimName.")
+            }
+            AddClaimPlayerPermissionResult.AlreadyExists -> {
+                val claimName = getClaimDetails.execute(partition.claimId)?.name ?: "(Could not retrieve name)"
+                player.sendMessage("${otherPlayer.player.displayName()} already has $permission " +
+                        "permissions in claim $claimName.")
+            }
+            AddClaimPlayerPermissionResult.ClaimNotFound ->
+                player.sendMessage("Claim was not found.")
+            AddClaimPlayerPermissionResult.StorageError ->
+                player.sendMessage("An internal error has occurred, contact your administrator for support.")
         }
     }
 }
