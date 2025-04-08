dcdbfc52ba057c6f14d857cce77868aba0c5179d
Mizarc <contact@mizarc.dev>
2025-04-09 01:49:17 +1000

    Rework player claim permission actions

--- src/main/kotlin/dev/mizarc/bellclaims/application/results/AssignClaimPlayerPermissionResult.kt
+++
@@ -1,8 +0,0 @@
-package dev.mizarc.bellclaims.application.results
-
-sealed class AssignClaimPlayerPermissionResult {
-    object Success : AssignClaimPlayerPermissionResult()
-    object ClaimNotFound : AssignClaimPlayerPermissionResult()
-    object AlreadyExists : AssignClaimPlayerPermissionResult()
-    object StorageError: AssignClaimPlayerPermissionResult()
-}
