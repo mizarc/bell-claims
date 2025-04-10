9f7c8fb0735445374384df81cafd6dd586a6fa59
Mizarc <contact@mizarc.dev>
2025-04-10 23:45:45 +1000

    Rework untrust command to use new actions

--- src/main/kotlin/dev/mizarc/bellclaims/interaction/commands/UntrustCommand.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/interaction/commands/UntrustCommand.kt
@@ -2,31 +2,43 @@ package dev.mizarc.bellclaims.interaction.commands
 
 import co.aikar.commands.annotation.*
 import co.aikar.commands.bukkit.contexts.OnlinePlayer
-import dev.mizarc.bellclaims.application.results.PlayerPermissionChangeResult
+import dev.mizarc.bellclaims.application.actions.GetClaimDetails
+import dev.mizarc.bellclaims.application.actions.RevokePlayerClaimPermission
+import dev.mizarc.bellclaims.application.results.RevokePlayerClaimPermissionResult
 import org.bukkit.entity.Player
 import dev.mizarc.bellclaims.domain.values.ClaimPermission
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+import kotlin.getValue
 
 @CommandAlias("claim")
-class UntrustCommand : ClaimCommand() {
+class UntrustCommand : ClaimCommand(), KoinComponent {
+    private val revokePlayerClaimPermission: RevokePlayerClaimPermission by inject()
+    private val getClaimDetails: GetClaimDetails by inject()
+
     @Subcommand("untrust")
     @CommandPermission("bellclaims.command.claim.untrust")
-    fun onUntrust(player: Player, targetPlayer: OnlinePlayer, claimPermission: ClaimPermission) {
+    fun onTrust(player: Player, targetPlayer: OnlinePlayer, permission: ClaimPermission) {
+        // Gets the partition at the player's current location
         val partition = getPartitionAtPlayer(player) ?: return
-        val claim = claimService.getById(partition.claimId) ?: return
-        if (!isPlayerHasClaimPermission(player, partition)) {
-            return
-        }
+        if (!isPlayerHasClaimPermission(player, partition)) return
 
-        // Remove permission for player and output result
-        when (playerPermissionService.removeForPlayer(claim, targetPlayer.player, claimPermission)) {
-            PlayerPermissionChangeResult.UNCHANGED ->
-                player.sendMessage("§6${targetPlayer.player.name} §cdoesn't have permission " +
-                        "§6${claimPermission.name}§c.")
-            PlayerPermissionChangeResult.SUCCESS ->
-                player.sendMessage("§6${targetPlayer.player.name} §ahas been revoked permission " +
-                        "§6${claimPermission.name} §afor this claim.")
-            else ->
-                player.sendMessage("Unknown Error.")
+        // Add permission for player and output result
+        when (revokePlayerClaimPermission.execute(partition.claimId, targetPlayer.player.uniqueId, permission)) {
+            RevokePlayerClaimPermissionResult.Success -> {
+                val claimName = getClaimDetails.execute(partition.claimId)?.name ?: "(Could not retrieve name)"
+                player.sendMessage("Permission $permission has been revoked from player " +
+                        "${targetPlayer.player.displayName()} in claim $claimName.")
+            }
+            RevokePlayerClaimPermissionResult.DoesNotExist -> {
+                val claimName = getClaimDetails.execute(partition.claimId)?.name ?: "(Could not retrieve name)"
+                player.sendMessage("${targetPlayer.player.displayName()} does not have $permission " +
+                        "permissions in claim $claimName.")
+            }
+            RevokePlayerClaimPermissionResult.ClaimNotFound ->
+                player.sendMessage("Claim was not found.")
+            RevokePlayerClaimPermissionResult.StorageError ->
+                player.sendMessage("An internal error has occurred, contact your administrator for support.")
         }
     }
 }
