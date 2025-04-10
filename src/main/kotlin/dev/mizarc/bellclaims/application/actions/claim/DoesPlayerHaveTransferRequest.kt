9b5eb82f33ce8be5791cbc581e8e83b15d24e6fb
Mizarc <contact@mizarc.dev>
2025-04-11 02:39:03 +1000

    Add new action to check for transfer requests

---
+++ src/main/kotlin/dev/mizarc/bellclaims/application/actions/claim/DoesPlayerHaveTransferRequest.kt
@@ -0,0 +1,13 @@
+package dev.mizarc.bellclaims.application.actions.claim
+
+import dev.mizarc.bellclaims.application.persistence.ClaimRepository
+import dev.mizarc.bellclaims.application.results.DoesPlayerHaveTransferRequestResult
+import java.util.UUID
+
+class DoesPlayerHaveTransferRequest(private val claimRepository: ClaimRepository) {
+    fun execute(claimId: UUID, playerId: UUID): DoesPlayerHaveTransferRequestResult {
+        val claim = claimRepository.getById(claimId) ?: return DoesPlayerHaveTransferRequestResult.ClaimNotFound
+        if (playerId in claim.transferRequests.keys) return DoesPlayerHaveTransferRequestResult.Success(true)
+        return DoesPlayerHaveTransferRequestResult.Success(false)
+    }
+}
