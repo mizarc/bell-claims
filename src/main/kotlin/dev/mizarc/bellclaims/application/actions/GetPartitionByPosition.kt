adbd87696af9c1d83e227bc566ef330ece4639e6
Mizarc <contact@mizarc.dev>
2025-04-11 00:38:43 +1000

    Add new action to get partition by position

---
+++ src/main/kotlin/dev/mizarc/bellclaims/application/actions/GetPartitionByPosition.kt
@@ -0,0 +1,20 @@
+package dev.mizarc.bellclaims.application.actions
+
+import dev.mizarc.bellclaims.application.persistence.ClaimRepository
+import dev.mizarc.bellclaims.application.persistence.PartitionRepository
+import dev.mizarc.bellclaims.domain.entities.Partition
+import dev.mizarc.bellclaims.domain.values.Position
+import java.util.UUID
+
+class GetPartitionByPosition(private val partitionRepository: PartitionRepository,
+                             private val claimRepository: ClaimRepository) {
+    fun execute(position: Position, worldId: UUID): Partition? {
+        val partitions = partitionRepository.getByPosition(position)
+
+        for (partition in partitions) {
+            val claim = claimRepository.getById(partition.claimId) ?: continue
+            if (claim.worldId == worldId) return partition
+        }
+        return null
+    }
+}
