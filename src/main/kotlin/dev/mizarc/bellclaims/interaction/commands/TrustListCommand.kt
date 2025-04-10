3fc3ca8004db92ae6a9d1be5cf22afb65faa83db
Mizarc <contact@mizarc.dev>
2025-04-10 23:42:07 +1000

    Rework trust list command to use new actions

--- src/main/kotlin/dev/mizarc/bellclaims/interaction/commands/TrustListCommand.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/interaction/commands/TrustListCommand.kt
@@ -4,22 +4,33 @@ import co.aikar.commands.annotation.CommandAlias
 import co.aikar.commands.annotation.CommandPermission
 import co.aikar.commands.annotation.Default
 import co.aikar.commands.annotation.Subcommand
+import dev.mizarc.bellclaims.application.actions.GetClaimDetails
+import dev.mizarc.bellclaims.application.actions.GetClaimPlayerPermissions
+import dev.mizarc.bellclaims.application.actions.GetPlayersWithPermissionInClaim
 import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 import dev.mizarc.bellclaims.infrastructure.ChatInfoBuilder
-import dev.mizarc.bellclaims.utils.getDisplayName
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
 import kotlin.math.ceil
 
 @CommandAlias("claim")
-class TrustListCommand : ClaimCommand() {
+class TrustListCommand : ClaimCommand(), KoinComponent {
+    private val getPlayersWithPermissionInClaim: GetPlayersWithPermissionInClaim by inject()
+    private val getClaimDetails: GetClaimDetails by inject()
+    private val getClaimPlayerPermissions: GetClaimPlayerPermissions by inject()
+
     @Subcommand("trustlist")
     @CommandPermission("bellclaims.command.claim.trustlist")
     fun onTrustList(player: Player, @Default("1") page: Int) {
+        // Gets the partition at the player's current location
         val partition = getPartitionAtPlayer(player) ?: return
-        val claim = claimService.getById(partition.claimId) ?: return
-        val trustedPlayers = playerPermissionService.getByClaim(claim).toSortedMap(compareBy { it.uniqueId })
+        if (!isPlayerHasClaimPermission(player, partition)) return
 
-        // Check if claim has no trusted players
+        // Get players who have at least one permission in the claim
+        val trustedPlayers = getPlayersWithPermissionInClaim.execute(partition.claimId)
+
+        // Notify if claim has no trusted players
         if (trustedPlayers.isEmpty()) {
             player.sendMessage("§cThis claim has no trusted players.")
             return
@@ -31,13 +42,22 @@ class TrustListCommand : ClaimCommand() {
             return
         }
 
-        // Output 5 player permissions at a time
-        val chatInfo = ChatInfoBuilder("${claim.name} Trusted Players")
-        val entries = trustedPlayers.entries.withIndex().toList().subList(0 + ((page - 1) * 5),
+        // Get names and sort alphabetically
+        val trustedPlayerInfo = trustedPlayers.map { playerId ->
+            val offlinePlayer = Bukkit.getOfflinePlayer(playerId)
+            offlinePlayer.let { playerId to it.name }
+        }.sortedBy { it.second }
+
+        // Generate chat output header
+        val claimName = getClaimDetails.execute(partition.claimId)
+        val chatInfo = ChatInfoBuilder("$claimName Trusted Players")
+
+        // Output 5 players at a time per page
+        val entries = trustedPlayerInfo.withIndex().toList().subList(0 + ((page - 1) * 5),
             (4 + ((page - 1) * 5)).coerceAtMost(trustedPlayers.size))
         entries.forEach { (_, entry) ->
-            chatInfo.addLinked(Bukkit.getOfflinePlayer(entry.key.uniqueId).name ?: "N/A",
-                entry.value.map { it.getDisplayName() }.toString())
+            val permissions = getClaimPlayerPermissions.execute(partition.claimId, entry.first)
+            chatInfo.addLinked(entry.second ?: "N/A", permissions.joinToString(", "))
         }
         player.sendMessage(chatInfo.createPaged(page, ceil(trustedPlayers.count() / 5.0).toInt()))
     }
