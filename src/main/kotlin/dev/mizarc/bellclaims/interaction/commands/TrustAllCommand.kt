c4c59f76f904470a1f330754c192fdaa63f9a23e
Mizarc <contact@mizarc.dev>
2025-04-11 00:11:22 +1000

    Rework trustall command to use new actions

--- src/main/kotlin/dev/mizarc/bellclaims/interaction/commands/TrustAllCommand.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/interaction/commands/TrustAllCommand.kt
@@ -1,28 +1,41 @@
 package dev.mizarc.bellclaims.interaction.commands
 
 import co.aikar.commands.annotation.*
-import dev.mizarc.bellclaims.application.results.DefaultPermissionChangeResult
+import dev.mizarc.bellclaims.application.actions.GetClaimDetails
+import dev.mizarc.bellclaims.application.actions.GrantClaimWidePermission
+import dev.mizarc.bellclaims.application.results.GrantClaimWidePermissionResult
 import org.bukkit.entity.Player
 import dev.mizarc.bellclaims.domain.values.ClaimPermission
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+import kotlin.getValue
 
 @CommandAlias("claim")
-class TrustAllCommand : ClaimCommand() {
+class TrustAllCommand : ClaimCommand(), KoinComponent {
+    private val grantClaimWidePermission: GrantClaimWidePermission by inject()
+    private val getClaimDetails: GetClaimDetails by inject()
+
     @Subcommand("trustall")
     @CommandPermission("bellclaims.command.claim.trustall")
     fun onTrustAll(player: Player, permission: ClaimPermission) {
+        // Gets the partition at the player's current location
         val partition = getPartitionAtPlayer(player) ?: return
-        val claim = claimService.getById(partition.claimId) ?: return
-        if (!isPlayerHasClaimPermission(player, partition)) {
-            return
-        }
+        if (!isPlayerHasClaimPermission(player, partition)) return
 
         // Add permission for player and output result
-        when (defaultPermissionService.add(claim, permission)) {
-            DefaultPermissionChangeResult.UNCHANGED ->
-                player.sendMessage("§cClaim already has §6${permission.name} §cset as a default permission.")
-            DefaultPermissionChangeResult.SUCCESS ->
-                player.sendMessage("§aAll players now have §6${permission.name} permissions §afor this claim.")
-            else -> player.sendMessage("Unknown Error.")
+        when (grantClaimWidePermission.execute(partition.claimId, permission)) {
+            GrantClaimWidePermissionResult.Success -> {
+                val claimName = getClaimDetails.execute(partition.claimId)?.name ?: "(Could not retrieve name)"
+                player.sendMessage("Permission $permission has been granted claim wide in claim $claimName.")
+            }
+            GrantClaimWidePermissionResult.AlreadyExists -> {
+                val claimName = getClaimDetails.execute(partition.claimId)?.name ?: "(Could not retrieve name)"
+                player.sendMessage("Claim $claimName already has $permission granted claim wide.")
+            }
+            GrantClaimWidePermissionResult.ClaimNotFound ->
+                player.sendMessage("Claim was not found.")
+            GrantClaimWidePermissionResult.StorageError ->
+                player.sendMessage("An internal error has occurred, contact your administrator for support.")
         }
     }
 }
