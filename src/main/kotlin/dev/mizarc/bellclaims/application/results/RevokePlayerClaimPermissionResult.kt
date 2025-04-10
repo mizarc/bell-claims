5ef2526977e829c74e47e6d4ff1b51a75969d1b0
Mizarc <contact@mizarc.dev>
2025-04-10 23:45:45 +1000

    Rework untrust command to use new actions

--- src/main/kotlin/dev/mizarc/bellclaims/application/results/RevokePlayerClaimPermissionResult.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/application/results/RevokePlayerClaimPermissionResult.kt
@@ -3,6 +3,6 @@ package dev.mizarc.bellclaims.application.results
 sealed class RevokePlayerClaimPermissionResult {
     object Success : RevokePlayerClaimPermissionResult()
     object ClaimNotFound : RevokePlayerClaimPermissionResult()
-    object AlreadyExists : RevokePlayerClaimPermissionResult()
+    object DoesNotExist : RevokePlayerClaimPermissionResult()
     object StorageError: RevokePlayerClaimPermissionResult()
 }
