59c09ab0f85fb04c1913dc4453cbb0049f702389
Mizarc <contact@mizarc.dev>
2025-04-12 14:55:50 +1000

    Change metadata output type from Int? to Int

--- src/main/kotlin/dev/mizarc/bellclaims/application/services/PlayerMetadataService.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/application/services/PlayerMetadataService.kt
@@ -3,6 +3,6 @@ package dev.mizarc.bellclaims.application.services
 import java.util.UUID
 
 interface PlayerMetadataService {
-    fun getPlayerClaimLimit(playerId: UUID): Int?
-    fun getPlayerClaimBlockLimit(playerId: UUID): Int?
+    fun getPlayerClaimLimit(playerId: UUID): Int
+    fun getPlayerClaimBlockLimit(playerId: UUID): Int
 }
