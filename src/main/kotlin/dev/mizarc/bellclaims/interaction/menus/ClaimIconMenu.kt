28d32fe08f6c0e0bb712245df486502440e8c591
Mizarc <contact@mizarc.dev>
2025-04-12 20:02:15 +1000

    Rework claim icon menu with new actions

---
+++ src/main/kotlin/dev/mizarc/bellclaims/interaction/menus/ClaimIconMenu.kt
@@ -0,0 +1,81 @@
+package dev.mizarc.bellclaims.interaction.menus
+
+import com.github.stefvanschie.inventoryframework.gui.GuiItem
+import com.github.stefvanschie.inventoryframework.gui.type.FurnaceGui
+import com.github.stefvanschie.inventoryframework.pane.StaticPane
+import dev.mizarc.bellclaims.application.actions.claim.metadata.UpdateClaimIcon
+import dev.mizarc.bellclaims.domain.entities.Claim
+import dev.mizarc.bellclaims.utils.getLangText
+import dev.mizarc.bellclaims.utils.lore
+import dev.mizarc.bellclaims.utils.name
+import org.bukkit.Material
+import org.bukkit.entity.Player
+import org.bukkit.event.inventory.ClickType
+import org.bukkit.inventory.ItemStack
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+import kotlin.concurrent.thread
+
+class ClaimIconMenu(private val player: Player, private val menuNavigator: MenuNavigator,
+                    private val claim: Claim): Menu, KoinComponent {
+    private val updateClaimIcon: UpdateClaimIcon by inject()
+
+    override fun open() {
+        val gui = FurnaceGui(getLangText("SetWarpIcon"))
+        gui.setOnTopClick { guiEvent -> guiEvent.isCancelled = true }
+        gui.setOnBottomClick { guiEvent -> if (guiEvent.click == ClickType.SHIFT_LEFT ||
+            guiEvent.click == ClickType.SHIFT_RIGHT) guiEvent.isCancelled = true }
+
+        val fuelPane = StaticPane(0, 0, 1, 1)
+
+        // Add info paper menu item
+        val paperItem = ItemStack(Material.PAPER)
+            .name(getLangText("PlaceItemTopSlot"))
+            .lore(getLangText("KeepItemMessage"))
+        val guiIconEditorItem = GuiItem(paperItem) { guiEvent -> guiEvent.isCancelled = true }
+        fuelPane.addItem(guiIconEditorItem, 0, 0)
+        gui.fuelComponent.addPane(fuelPane)
+
+        // Allow item to be placed in slot
+        val inputPane = StaticPane(0, 0, 1, 1)
+        inputPane.setOnClick { guiEvent ->
+            guiEvent.isCancelled = true
+            val temp = guiEvent.cursor
+            val cursor = guiEvent.cursor?.type ?: Material.AIR
+
+            if (cursor == Material.AIR) {
+                inputPane.removeItem(0, 0)
+                gui.update()
+                return@setOnClick
+            }
+
+            inputPane.addItem(GuiItem(ItemStack(cursor)), 0, 0)
+            gui.update()
+            thread(start = true) {
+                Thread.sleep(1)
+                player.setItemOnCursor(temp)
+            }
+        }
+        gui.ingredientComponent.addPane(inputPane)
+
+        // Add confirm menu item
+        val outputPane = StaticPane(0, 0, 1, 1)
+        val confirmItem = ItemStack(Material.NETHER_STAR).name(getLangText("Confirm2"))
+        val confirmGuiItem = GuiItem(confirmItem) { guiEvent ->
+            guiEvent.isCancelled = true
+            val newIcon = gui.ingredientComponent.getItem(0, 0)
+
+            // Set icon if item in slot
+            if (newIcon != null) {
+                updateClaimIcon.execute(claim.id, newIcon.type.name)
+                openClaimEditMenu(claim)
+            }
+
+            // Go back to edit menu if no item in slot
+            openClaimEditMenu(claim)
+        }
+        outputPane.addItem(confirmGuiItem, 0, 0)
+        gui.outputComponent.addPane(outputPane)
+        gui.show(player)
+    }
+}
