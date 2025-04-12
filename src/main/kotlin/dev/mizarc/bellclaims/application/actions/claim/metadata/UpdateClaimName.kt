7ccf99b93becb0e24ad216177acef82ce15b45f5
Mizarc <contact@mizarc.dev>
2025-04-12 20:31:16 +1000

    Change update claim name action to check for already existing names

--- src/main/kotlin/dev/mizarc/bellclaims/application/actions/claim/metadata/UpdateClaimName.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/application/actions/claim/metadata/UpdateClaimName.kt
@@ -1,23 +1,27 @@
 package dev.mizarc.bellclaims.application.actions.claim.metadata
 
-import dev.mizarc.bellclaims.application.results.UpdateClaimAttributeResult
 import dev.mizarc.bellclaims.application.errors.DatabaseOperationException
 import dev.mizarc.bellclaims.application.persistence.ClaimRepository
+import dev.mizarc.bellclaims.application.results.UpdateClaimNameResult
 import java.util.UUID
 
 class UpdateClaimName(private val claimRepository: ClaimRepository) {
-    fun execute(claimId: UUID, name: String): UpdateClaimAttributeResult {
+    fun execute(claimId: UUID, name: String): UpdateClaimNameResult {
         // Check if claim exists
-        val existingClaim = claimRepository.getById(claimId) ?: return UpdateClaimAttributeResult.ClaimNotFound
+        val existingClaim = claimRepository.getById(claimId) ?: return UpdateClaimNameResult.ClaimNotFound
+
+        // Check if name already exists in player's list of claims
+        if (claimRepository.getByName(existingClaim.playerId, name) != null)
+            return UpdateClaimNameResult.NameAlreadyExists
 
         // Change Name and persist to storage
         existingClaim.name = name
         try {
             claimRepository.update(existingClaim)
-            return UpdateClaimAttributeResult.Success
+            return UpdateClaimNameResult.Success
         } catch (error: DatabaseOperationException) {
             println("Error has occurred trying to save to the database")
-            return UpdateClaimAttributeResult.StorageError
+            return UpdateClaimNameResult.StorageError
         }
     }
 }
