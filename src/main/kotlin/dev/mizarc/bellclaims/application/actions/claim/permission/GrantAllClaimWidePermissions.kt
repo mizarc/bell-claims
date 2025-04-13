64005bfa32da1752aa5b380fbc17298427ffe98f
Mizarc <contact@mizarc.dev>
2025-04-13 13:28:14 +1000

    Rename incorrectly named GrantAllPlayerClaimPermissions
    This implementation was claim wide, not player specific.

--- src/main/kotlin/dev/mizarc/bellclaims/application/actions/claim/permission/GrantAllClaimWidePermissions.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/application/actions/claim/permission/GrantAllClaimWidePermissions.kt
@@ -3,21 +3,21 @@ package dev.mizarc.bellclaims.application.actions.claim.permission
 import dev.mizarc.bellclaims.application.errors.DatabaseOperationException
 import dev.mizarc.bellclaims.application.persistence.ClaimPermissionRepository
 import dev.mizarc.bellclaims.application.persistence.ClaimRepository
-import dev.mizarc.bellclaims.application.results.claim.permission.GrantAllClaimPermissionsResult
+import dev.mizarc.bellclaims.application.results.claim.permission.GrantAllClaimWidePermissionsResult
 import dev.mizarc.bellclaims.domain.values.ClaimPermission
 import java.util.UUID
 
-class GrantAllPlayerClaimPermissions(private val claimPermissionRepository: ClaimPermissionRepository,
-                                     private val claimRepository: ClaimRepository) {
+class GrantAllClaimWidePermissions(private val claimPermissionRepository: ClaimPermissionRepository,
+                                   private val claimRepository: ClaimRepository) {
     /**
      * Adds all available flags to the claim with the given [claimId].
      *
      * @param claimId The [UUID] of the claim to which the flag should be added.
-     * @return An [GrantAllClaimPermissionsResult] indicating the outcome of the flag addition operation.
+     * @return An [GrantAllClaimWidePermissionsResult] indicating the outcome of the flag addition operation.
      */
-    fun execute(claimId: UUID): GrantAllClaimPermissionsResult {
+    fun execute(claimId: UUID): GrantAllClaimWidePermissionsResult {
         // Check if claim exists
-        claimRepository.getById(claimId) ?: return GrantAllClaimPermissionsResult.ClaimNotFound
+        claimRepository.getById(claimId) ?: return GrantAllClaimWidePermissionsResult.ClaimWideNotFound
 
         // Add all flags to the claim
         var anyPermissionEnabled = false
@@ -29,13 +29,13 @@ class GrantAllPlayerClaimPermissions(private val claimPermissionRepository: Clai
 
             // Return success if at least one permission was granted
             return if (anyPermissionEnabled) {
-                GrantAllClaimPermissionsResult.Success
+                GrantAllClaimWidePermissionsResult.Success
             } else {
-                GrantAllClaimPermissionsResult.AllAlreadyGranted
+                GrantAllClaimWidePermissionsResult.AllAlreadyGrantedWide
             }
         } catch (error: DatabaseOperationException) {
             println("Error has occurred trying to save to the database: ${error.message}")
-            return GrantAllClaimPermissionsResult.StorageError
+            return GrantAllClaimWidePermissionsResult.StorageError
         }
     }
 }
