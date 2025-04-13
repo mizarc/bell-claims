bf34a57dfcc4dd7afde0aa2040248def0571834e
Mizarc <contact@mizarc.dev>
2025-04-13 20:10:07 +1000

    Rename GetClaimAtPosition to GetClaimAnchorAtPosition
    This will reduce confusion when considering the claim as the area itself
    and trying to get the claim based on the area rather than the block that
    anchors the claim itself.

--- src/main/kotlin/dev/mizarc/bellclaims/application/actions/claim/GetClaimAnchorAtPosition.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/application/actions/claim/GetClaimAnchorAtPosition.kt
@@ -5,7 +5,7 @@ import dev.mizarc.bellclaims.application.results.claim.GetClaimAtPositionResult
 import dev.mizarc.bellclaims.domain.values.Position3D
 import java.util.UUID
 
-class GetClaimAtPosition(private val claimRepository: ClaimRepository) {
+class GetClaimAnchorAtPosition(private val claimRepository: ClaimRepository) {
     fun execute(position3D: Position3D, worldId: UUID): GetClaimAtPositionResult {
         val claim = claimRepository.getByPosition(position3D, worldId) ?: return GetClaimAtPositionResult.NoClaimFound
         return GetClaimAtPositionResult.Success(claim)
