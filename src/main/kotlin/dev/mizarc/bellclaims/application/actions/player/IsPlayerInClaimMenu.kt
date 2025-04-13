646a92a5cc1e03598808dc8a2ae5aed0a6d4e292
Mizarc <contact@mizarc.dev>
2025-04-13 15:16:36 +1000

    Add new action to check if player is in the claim menu

---
+++ src/main/kotlin/dev/mizarc/bellclaims/application/actions/player/IsPlayerInClaimMenu.kt
@@ -0,0 +1,23 @@
+package dev.mizarc.bellclaims.application.actions.player
+
+import dev.mizarc.bellclaims.application.persistence.PlayerStateRepository
+import dev.mizarc.bellclaims.application.results.player.IsPlayerInClaimMenuResult
+import dev.mizarc.bellclaims.domain.entities.PlayerState
+import java.util.UUID
+
+class IsPlayerInClaimMenu(private val playerStateRepository: PlayerStateRepository) {
+    fun execute(playerId: UUID, claimId: UUID): IsPlayerInClaimMenuResult {
+        // Get or create player state if it doesn't exist
+        var playerState = playerStateRepository.get(playerId)
+        if (playerState == null) {
+            playerState = PlayerState(playerId)
+            playerStateRepository.add(playerState)
+        }
+
+        val claim = playerState.isInClaimMenu
+        if (claim != null && claim.id == claimId) {
+            return IsPlayerInClaimMenuResult.Success(true)
+        }
+        return IsPlayerInClaimMenuResult.Success(false)
+    }
+}
