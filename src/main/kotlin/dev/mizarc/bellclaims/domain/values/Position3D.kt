f3f217dbc5e4bfee216f0b0a7097c61fff059d5f
Mizarc <contact@mizarc.dev>
2025-04-14 00:39:50 +1000

    Remove location mappers from domain

--- src/main/kotlin/dev/mizarc/bellclaims/domain/values/Position3D.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/domain/values/Position3D.kt
@@ -10,21 +10,4 @@ import org.bukkit.World
  * @property y The Y-Axis position.
  * @property z The Z-Axis position.
  */
-data class Position3D(override val x: Int, override val y: Int, override val z: Int): Position(x, y, z) {
-    /**
-     * Creates a position from a location.
-     *
-     * @param location The location instance to use.
-     */
-    constructor(location: Location): this(location.blockX, location.blockY, location.blockZ)
-
-    /**
-     * Converts the position to a location.
-     *
-     * @param world The world to use.
-     * @return The combined location instance.
-     */
-    fun toLocation(world: World): Location {
-        return Location(world, x.toDouble(), y.toDouble(), z.toDouble())
-    }
-}
+data class Position3D(override val x: Int, override val y: Int, override val z: Int): Position(x, y, z)
