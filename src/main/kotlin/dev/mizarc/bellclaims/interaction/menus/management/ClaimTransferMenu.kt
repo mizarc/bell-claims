26af27793f246fc98086b628b766180d00724cac
Mizarc <contact@mizarc.dev>
2025-04-13 18:49:32 +1000

    Add menu links to transfer menu

--- src/main/kotlin/dev/mizarc/bellclaims/interaction/menus/management/ClaimTransferMenu.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/interaction/menus/management/ClaimTransferMenu.kt
@@ -2,19 +2,21 @@ package dev.mizarc.bellclaims.interaction.menus.management
 
 import dev.mizarc.bellclaims.domain.entities.Claim
 import dev.mizarc.bellclaims.interaction.menus.Menu
+import dev.mizarc.bellclaims.interaction.menus.MenuNavigator
 import dev.mizarc.bellclaims.interaction.menus.common.ConfirmationMenu
 import dev.mizarc.bellclaims.interaction.menus.common.ConfirmationMenu.Companion.openConfirmationMenu
 import dev.mizarc.bellclaims.utils.getLangText
 import org.bukkit.entity.Player
 
-class ClaimTransferMenu(private val claim: Claim, private val player: Player): Menu {
+class ClaimTransferMenu(private val menuNavigator: MenuNavigator, private val claim: Claim,
+                        private val player: Player): Menu {
     override fun open() {
         val cancelAction: () -> Unit = {
             player.closeInventory()
         }
 
         val confirmAction: () -> Unit = {
-            openTransferNamingMenu(claim)
+            menuNavigator.openMenu(ClaimTransferNamingMenu(menuNavigator, claim, player))
         }
 
         val parameters = ConfirmationMenu.Companion.ConfirmationMenuParameters(
