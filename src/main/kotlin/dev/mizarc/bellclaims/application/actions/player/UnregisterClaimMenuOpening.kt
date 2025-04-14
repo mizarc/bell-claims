897055ac7df07fb4e9275eddfb189e88836f745e
Mizarc <contact@mizarc.dev>
2025-04-15 00:06:40 +1000

    Remove unused claimrepository arg from unregister

--- src/main/kotlin/dev/mizarc/bellclaims/application/actions/player/UnregisterClaimMenuOpening.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/application/actions/player/UnregisterClaimMenuOpening.kt
@@ -1,13 +1,11 @@
 package dev.mizarc.bellclaims.application.actions.player
 
-import dev.mizarc.bellclaims.application.persistence.ClaimRepository
 import dev.mizarc.bellclaims.application.persistence.PlayerStateRepository
 import dev.mizarc.bellclaims.application.results.player.UnregisterClaimMenuOpeningResult
 import dev.mizarc.bellclaims.domain.entities.PlayerState
 import java.util.UUID
 
-class UnregisterClaimMenuOpening(private val playerStateRepository: PlayerStateRepository,
-                                 private val claimRepository: ClaimRepository) {
+class UnregisterClaimMenuOpening(private val playerStateRepository: PlayerStateRepository) {
     fun execute(playerId: UUID): UnregisterClaimMenuOpeningResult {
         // Get or create player state if it doesn't exist
         var playerState = playerStateRepository.get(playerId)
