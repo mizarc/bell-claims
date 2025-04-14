4a324d7eaf3d4800902275933fc753bdfb9c094d
Mizarc <contact@mizarc.dev>
2025-04-14 22:54:27 +1000

    Update break claim anchor to return break count

--- src/main/kotlin/dev/mizarc/bellclaims/application/actions/claim/BreakClaimAnchor.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/application/actions/claim/BreakClaimAnchor.kt
@@ -21,7 +21,7 @@ class BreakClaimAnchor(private val claimRepository: ClaimRepository,
         // Trigger the break reset countdown and decrement break count by 1
         claim.resetBreakCount()
         if (claim.breakCount > 1) {
-            return BreakClaimAnchorResult.ClaimBreaking
+            return BreakClaimAnchorResult.ClaimBreaking(claim.breakCount)
         }
 
         // If break counter met, destroy claim and all associated
