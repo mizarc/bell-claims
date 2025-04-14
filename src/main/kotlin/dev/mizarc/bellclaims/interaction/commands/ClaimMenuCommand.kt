38c471c2c44d25274cd74dd7d73dc29d4f858df1
Mizarc <contact@mizarc.dev>
2025-04-14 23:13:45 +1000

    Update claim menu command with new menu connection

--- src/main/kotlin/dev/mizarc/bellclaims/interaction/commands/ClaimMenuCommand.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/interaction/commands/ClaimMenuCommand.kt
@@ -4,19 +4,17 @@ import co.aikar.commands.BaseCommand
 import co.aikar.commands.annotation.CommandAlias
 import co.aikar.commands.annotation.CommandPermission
 import co.aikar.commands.annotation.Default
-import co.aikar.commands.annotation.Dependency
-import dev.mizarc.bellclaims.application.services.old.ClaimService
+import dev.mizarc.bellclaims.interaction.menus.MenuNavigator
 import org.bukkit.entity.Player
 import dev.mizarc.bellclaims.interaction.menus.misc.ClaimListMenu
 
 @CommandAlias("claimmenu")
 class ClaimMenuCommand: BaseCommand() {
-    @Dependency
-    lateinit var claimService: ClaimService
 
     @Default
     @CommandPermission("bellclaims.command.claimmenu")
-    fun onClaimMenu(player: Player, backCommand: String? = null) {
-        ClaimListMenu(claimService, player).openClaimListMenu(backCommand)
+    fun onClaimMenu(player: Player) {
+        val menuNavigator = MenuNavigator(player)
+        menuNavigator.openMenu(ClaimListMenu(menuNavigator, player))
     }
 }
