e27f8cf906a8090884aec6bd958ff8cddf01ed68
Mizarc <contact@mizarc.dev>
2025-04-14 22:51:28 +1000

    Add new action to break claim anchor

---
+++ src/main/kotlin/dev/mizarc/bellclaims/application/results/claim/BreakClaimAnchorResult.kt
@@ -0,0 +1,10 @@
+package dev.mizarc.bellclaims.application.results.claim
+
+import dev.mizarc.bellclaims.domain.entities.Claim
+
+sealed class BreakClaimAnchorResult {
+    object Success: BreakClaimAnchorResult()
+    object ClaimBreaking: BreakClaimAnchorResult()
+    object ClaimNotFound: BreakClaimAnchorResult()
+    object StorageError: BreakClaimAnchorResult()
+}
