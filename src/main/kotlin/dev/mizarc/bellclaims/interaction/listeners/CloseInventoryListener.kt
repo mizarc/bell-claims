7200f7e46cbae44687c75d299f24040e468645b4
Mizarc <contact@mizarc.dev>
2025-04-15 00:07:26 +1000

    Rework listener on inventory close

--- src/main/kotlin/dev/mizarc/bellclaims/interaction/listeners/CloseInventoryListener.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/interaction/listeners/CloseInventoryListener.kt
@@ -1,22 +1,17 @@
 package dev.mizarc.bellclaims.interaction.listeners
 
-import dev.mizarc.bellclaims.application.services.old.PlayerStateService
+import dev.mizarc.bellclaims.application.actions.player.UnregisterClaimMenuOpening
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.inventory.InventoryCloseEvent
 
-class PlayerStateListener(private val playerStateService: PlayerStateService) : Listener {
+class CloseInventoryListener(private val unregisterClaimMenuOpening: UnregisterClaimMenuOpening) : Listener {
     @EventHandler
     fun onInventoryClose(event: InventoryCloseEvent) {
         if (event.player !is Player) return
         if (event.reason == InventoryCloseEvent.Reason.OPEN_NEW) return
         if (event.reason == InventoryCloseEvent.Reason.UNKNOWN) return
-
-        val playerState = playerStateService.getByPlayer((event.player as Player))
-
-        if (playerState?.isInClaimMenu != null) {
-            playerState.isInClaimMenu = null
-        }
+        unregisterClaimMenuOpening.execute(event.player.uniqueId)
     }
 }
