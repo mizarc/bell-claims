7b82b7d821b180320f5bf0e24a69e7a7b718be07
Mizarc <contact@mizarc.dev>
2025-04-14 23:49:01 +1000

    Add new action to unregister menu opening

---
+++ src/main/kotlin/dev/mizarc/bellclaims/application/actions/player/UnregisterClaimMenuOpening.kt
@@ -0,0 +1,30 @@
+package dev.mizarc.bellclaims.application.actions.player
+
+import dev.mizarc.bellclaims.application.persistence.ClaimRepository
+import dev.mizarc.bellclaims.application.persistence.PlayerStateRepository
+import dev.mizarc.bellclaims.application.results.player.UnregisterClaimMenuOpeningResult
+import dev.mizarc.bellclaims.domain.entities.PlayerState
+import java.util.UUID
+
+class UnregisterClaimMenuOpening(private val playerStateRepository: PlayerStateRepository,
+                                 private val claimRepository: ClaimRepository) {
+    fun execute(playerId: UUID, claimId: UUID): UnregisterClaimMenuOpeningResult {
+        // Check if claim exists
+        claimRepository.getById(claimId) ?: return UnregisterClaimMenuOpeningResult.ClaimNotFound
+
+        // Get or create player state if it doesn't exist
+        var playerState = playerStateRepository.get(playerId)
+        if (playerState == null) {
+            playerState = PlayerState(playerId)
+            playerStateRepository.add(playerState)
+        }
+
+        // No claim is registered to unregister
+        if (playerState.isInClaimMenu == null) return UnregisterClaimMenuOpeningResult.NotRegistered
+
+        // Unset currently open claim menu
+        playerState.isInClaimMenu = null
+        playerStateRepository.update(playerState)
+        return UnregisterClaimMenuOpeningResult.Success
+    }
+}
