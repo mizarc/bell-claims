00ff4025c7d5e9573e8ad039429a134df4c9f082
Mizarc <contact@mizarc.dev>
2025-04-15 19:51:02 +1000

    Rework partition update listener with new actions

--- src/main/kotlin/dev/mizarc/bellclaims/interaction/listeners/PartitionUpdateListener.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/interaction/listeners/PartitionUpdateListener.kt
@@ -1,9 +1,13 @@
 package dev.mizarc.bellclaims.interaction.listeners
 
-import dev.mizarc.bellclaims.application.services.old.ClaimService
-import dev.mizarc.bellclaims.application.services.old.PartitionService
-import dev.mizarc.bellclaims.application.services.old.PlayerStateService
+import dev.mizarc.bellclaims.application.actions.claim.metadata.GetClaimDetails
+import dev.mizarc.bellclaims.application.actions.claim.partition.GetClaimPartitions
+import dev.mizarc.bellclaims.application.actions.player.visualisation.GetVisualisedClaimBlocks
+import dev.mizarc.bellclaims.application.actions.player.visualisation.IsPlayerVisualising
+import dev.mizarc.bellclaims.application.actions.player.visualisation.UnregisterVisualisation
 import dev.mizarc.bellclaims.application.events.PartitionModificationEvent
+import dev.mizarc.bellclaims.application.results.player.visualisation.GetVisualisedClaimBlocksResult
+import dev.mizarc.bellclaims.application.results.player.visualisation.IsPlayerVisualisingResult
 import dev.mizarc.bellclaims.domain.entities.Claim
 import dev.mizarc.bellclaims.interaction.visualisation.Visualiser
 import org.bukkit.Bukkit
@@ -11,22 +15,30 @@ import org.bukkit.Chunk
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
 
-class PartitionUpdateListener(private val claimService: ClaimService,
-                              private val partitionService: PartitionService,
-                              private val playerStateService: PlayerStateService,
-                              private val visualiser: Visualiser): Listener {
+class PartitionUpdateListener(private val visualiser: Visualiser): Listener, KoinComponent {
+    private val getClaimDetails: GetClaimDetails by inject()
+    private val getClaimPartitions: GetClaimPartitions by inject()
+    private val isPlayerVisualising: IsPlayerVisualising by inject()
+    private val unregisterVisualisation: UnregisterVisualisation by inject()
+    private val getVisualisedClaimBlocks: GetVisualisedClaimBlocks by inject()
+
     @EventHandler
     fun onPartitionUpdate(event: PartitionModificationEvent) {
-        val claim = claimService.getById(event.partition.claimId) ?: return
+        val claim = getClaimDetails.execute(event.partition.claimId) ?: return
         val nearbyPlayers = getNearbyPlayers(claim)
         for (player in nearbyPlayers) {
-            val playerState = playerStateService.getByPlayer(player) ?: continue
-            val visualisedClaim = playerState.visualisedBlockPositions[claim] ?: continue
 
             // Clear and redo visualisation for selected claim
-            visualiser.revertVisualisedBlocks(player, visualisedClaim)
-            playerState.visualisedBlockPositions.remove(claim)
+            when (val result = getVisualisedClaimBlocks.execute(player.uniqueId, claim.id)) {
+                is GetVisualisedClaimBlocksResult.Success -> {
+                    visualiser.revertVisualisedBlocks(player, result.blockPositions)
+                }
+                else -> {}
+            }
+            unregisterVisualisation.execute(player.uniqueId, claim.id)
             visualiser.show(player, claim)
         }
     }
@@ -39,8 +51,8 @@ class PartitionUpdateListener(private val claimService: ClaimService,
 
         // Get list of chunks that the claim occupies
         val startingChunks: MutableSet<Chunk> = mutableSetOf()
-        val partitions = partitionService.getByClaim(claim)
-        val world = claim.getWorld() ?: return arrayOf()
+        val partitions = getClaimPartitions.execute(claim.id)
+        val world = Bukkit.getWorld(claim.worldId) ?: return arrayOf()
         for (partition in partitions) {
             startingChunks.addAll(partition.getChunks().map { world.getChunkAt(it.x, it.z) })
         }
@@ -54,8 +66,12 @@ class PartitionUpdateListener(private val claimService: ClaimService,
         // Get players in chunks
         for (player in Bukkit.getOnlinePlayers()) {
             if (player.location.world != world) continue
-            val playerState = playerStateService.getByPlayer(player) ?: continue
-            if (finalChunks.contains(player.chunk) && playerState.isVisualisingClaims) {
+            if (finalChunks.contains(player.chunk)) {
+                val result = isPlayerVisualising.execute(player.uniqueId)
+                when (result) {
+                    is IsPlayerVisualisingResult.Success -> players.add(player)
+                    else -> {}
+                }
                 players.add(player)
             }
         }
