7aa64c3848ba3f715a2c3ba9675289215857cd1f
Mizarc <contact@mizarc.dev>
2025-04-15 18:26:13 +1000

    Add visualiser register and unregister
    This isn't the most ideal way to do visualisation, but it'll be a
    temporary measure until all the errors are cleared.

---
+++ src/main/kotlin/dev/mizarc/bellclaims/application/actions/player/RegisterVisualisation.kt
@@ -0,0 +1,29 @@
+package dev.mizarc.bellclaims.application.actions.player
+
+import dev.mizarc.bellclaims.application.persistence.ClaimRepository
+import dev.mizarc.bellclaims.application.persistence.PlayerStateRepository
+import dev.mizarc.bellclaims.application.results.player.RegisterClaimMenuOpeningResult
+import dev.mizarc.bellclaims.application.results.player.RegisterVisualisationResult
+import dev.mizarc.bellclaims.domain.entities.PlayerState
+import dev.mizarc.bellclaims.domain.values.Position3D
+import java.util.UUID
+
+class RegisterVisualisation(private val claimRepository: ClaimRepository,
+                            private val playerStateRepository: PlayerStateRepository) {
+    fun execute(playerId: UUID, claimId: UUID, blockPositions: Set<Position3D>): RegisterVisualisationResult {
+        // Check if claim exists
+        claimRepository.getById(claimId) ?: return RegisterVisualisationResult.ClaimNotFound
+
+        // Get or create player state if it doesn't exist
+        var playerState = playerStateRepository.get(playerId)
+        if (playerState == null) {
+            playerState = PlayerState(playerId)
+            playerStateRepository.add(playerState)
+        }
+
+        // Set visualised block positions
+        playerState.visualisedBlockPositions[claimId] = blockPositions
+        playerStateRepository.update(playerState)
+        return RegisterVisualisationResult.Success
+    }
+}
