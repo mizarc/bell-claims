fce52e56924ac6e81754a0267d2a305cc31ac583
Mizarc <contact@mizarc.dev>
2025-04-15 17:59:52 +1000

    Add action to check if player is visualising

---
+++ src/main/kotlin/dev/mizarc/bellclaims/application/actions/player/IsPlayerVisualising.kt
@@ -0,0 +1,22 @@
+package dev.mizarc.bellclaims.application.actions.player
+
+import dev.mizarc.bellclaims.application.persistence.PlayerStateRepository
+import dev.mizarc.bellclaims.application.results.player.IsPlayerVisualisingResult
+import dev.mizarc.bellclaims.domain.entities.PlayerState
+import java.util.UUID
+
+class IsPlayerVisualising(private val playerStateRepository: PlayerStateRepository) {
+    fun execute(playerId: UUID): IsPlayerVisualisingResult {
+        // Get or create player state if it doesn't exist
+        var playerState = playerStateRepository.get(playerId)
+        if (playerState == null) {
+            playerState = PlayerState(playerId)
+            playerStateRepository.add(playerState)
+        }
+
+        if (playerState.isVisualisingClaims) {
+            return IsPlayerVisualisingResult.Success(true)
+        }
+        return IsPlayerVisualisingResult.Success(false)
+    }
+}
