ddd343eadac68b75e71250c6b31d5e0ff51564bb
Mizarc <contact@mizarc.dev>
2025-04-16 21:07:04 +1000

    Change visualiser service by merging function uses
    Rather than having individual functions to display own and other's
    claims as the algorithm and display methods are the same, the function
    now takes in a block type to be specified by the action instead.

--- src/main/kotlin/dev/mizarc/bellclaims/application/services/VisualisationService.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/application/services/VisualisationService.kt
@@ -6,9 +6,8 @@ import dev.mizarc.bellclaims.domain.values.Position3D
 import java.util.UUID
 
 interface VisualisationService {
-    fun displayComplete(playerId: UUID, areas: Set<Area>): Set<Position3D>
-    fun displayPartitioned(playerId: UUID, areas: Set<Area>): Set<Position3D>
-    fun displayOthers(playerId: UUID, areas: Set<Area>): Set<Position3D>
+    fun displayComplete(playerId: UUID, areas: Set<Area>, edgeBlock: String): Set<Position3D>
+    fun displayPartitioned(playerId: UUID, areas: Set<Area>, edgeBlock: String, cornerBlock: String): Set<Position3D>
     fun clear(playerId: UUID, areas: Set<Area>): Set<Position3D>
     fun clearAll(playerId: UUID): Set<Position2D>
 }
