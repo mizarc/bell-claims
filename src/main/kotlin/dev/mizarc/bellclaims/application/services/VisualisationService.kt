4360c3d9b54589198e7efb2202ea45f92d0075f5
Mizarc <contact@mizarc.dev>
2025-04-16 21:33:52 +1000

    Add surface block to argument
    Surface block is used to replace things like snow, carpet, moss.

--- src/main/kotlin/dev/mizarc/bellclaims/application/services/VisualisationService.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/application/services/VisualisationService.kt
@@ -6,8 +6,9 @@ import dev.mizarc.bellclaims.domain.values.Position3D
 import java.util.UUID
 
 interface VisualisationService {
-    fun displayComplete(playerId: UUID, areas: Set<Area>, edgeBlock: String): Set<Position3D>
-    fun displayPartitioned(playerId: UUID, areas: Set<Area>, edgeBlock: String, cornerBlock: String): Set<Position3D>
+    fun displayComplete(playerId: UUID, areas: Set<Area>, edgeBlock: String, edgeSurfaceBlock: String): Set<Position3D>
+    fun displayPartitioned(playerId: UUID, areas: Set<Area>, edgeBlock: String, edgeSurfaceBlock: String,
+                           cornerBlock: String, cornerSurfaceBlock: String): Set<Position3D>
     fun clear(playerId: UUID, areas: Set<Area>): Set<Position3D>
     fun clearAll(playerId: UUID): Set<Position2D>
 }
