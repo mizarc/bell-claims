79cc28c665e0a2b19c21b03078eae22dcf002493
Mizarc <contact@mizarc.dev>
2025-04-16 20:08:30 +1000

    Change visualisation service again to use area arguments
    Positions alone did not provide enough data to fully map out the
    visualisations. Using area should cover it.

--- src/main/kotlin/dev/mizarc/bellclaims/application/services/VisualisationService.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/application/services/VisualisationService.kt
@@ -1,14 +1,14 @@
 package dev.mizarc.bellclaims.application.services
 
+import dev.mizarc.bellclaims.domain.values.Area
 import dev.mizarc.bellclaims.domain.values.Position2D
 import dev.mizarc.bellclaims.domain.values.Position3D
 import java.util.UUID
 
 interface VisualisationService {
-    fun displayComplete(playerId: UUID, edgePositions: Set<Position2D>): Set<Position3D>
-    fun displayPartitioned(playerId: UUID, edgePositions: Set<Position2D>,
-                           cornerPositions: Set<Position2D>): Set<Position3D>
-    fun displayOthers(playerId: UUID, edgePositions: Set<Position2D>): Set<Position3D>
-    fun clear(playerId: UUID, edgePositions: Set<Position2D>): Set<Position3D>
+    fun displayComplete(playerId: UUID, areas: Set<Area>): Set<Position3D>
+    fun displayPartitioned(playerId: UUID, areas: Set<Area>): Set<Position3D>
+    fun displayOthers(playerId: UUID, areas: Set<Area>): Set<Position3D>
+    fun clear(playerId: UUID, areas: Set<Area>): Set<Position3D>
     fun clearAll(playerId: UUID): Set<Position2D>
 }
