f732889fbeef93955b1490c1fd3c800487c109fd
Mizarc <contact@mizarc.dev>
2025-04-16 14:54:49 +1000

    Change argument names and add one for corner positions

--- src/main/kotlin/dev/mizarc/bellclaims/application/services/VisualisationService.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/application/services/VisualisationService.kt
@@ -5,9 +5,10 @@ import dev.mizarc.bellclaims.domain.values.Position3D
 import java.util.UUID
 
 interface VisualisationService {
-    fun displayComplete(playerId: UUID, partitions: Set<Position2D>): Set<Position3D>
-    fun displayPartitioned(playerId: UUID, partitions: Set<Position2D>): Set<Position3D>
-    fun displayOthers(playerId: UUID, partitions: Set<Position2D>): Set<Position3D>
-    fun clear(playerId: UUID, partitions: Set<Position2D>): Set<Position3D>
+    fun displayComplete(playerId: UUID, edgePositions: Set<Position2D>): Set<Position3D>
+    fun displayPartitioned(playerId: UUID, edgePositions: Set<Position2D>,
+                           cornerPositions: Set<Position2D>): Set<Position3D>
+    fun displayOthers(playerId: UUID, edgePositions: Set<Position2D>): Set<Position3D>
+    fun clear(playerId: UUID, edgePositions: Set<Position2D>): Set<Position3D>
     fun clearAll(playerId: UUID): Set<Position2D>
 }
