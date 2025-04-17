6d806b960392e2945debbf237824b6df98f59e2d
Mizarc <contact@mizarc.dev>
2025-04-17 20:02:17 +1000

    Add new visualisation service functions for refresh
    This handles the refreshing of either complete or partitioned. This
    should be preferred to just clearing and revisualisation and it prevents
    the flashing that occurs when doing so with packet sent blocks.

--- src/main/kotlin/dev/mizarc/bellclaims/application/services/VisualisationService.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/application/services/VisualisationService.kt
@@ -1,7 +1,6 @@
 package dev.mizarc.bellclaims.application.services
 
 import dev.mizarc.bellclaims.domain.values.Area
-import dev.mizarc.bellclaims.domain.values.Position2D
 import dev.mizarc.bellclaims.domain.values.Position3D
 import java.util.UUID
 
@@ -9,6 +8,9 @@ interface VisualisationService {
     fun displayComplete(playerId: UUID, areas: Set<Area>, edgeBlock: String, edgeSurfaceBlock: String): Set<Position3D>
     fun displayPartitioned(playerId: UUID, areas: Set<Area>, edgeBlock: String, edgeSurfaceBlock: String,
                            cornerBlock: String, cornerSurfaceBlock: String): Set<Position3D>
-    fun clear(playerId: UUID, areas: Set<Area>): Set<Position3D>
-    fun clearAll(playerId: UUID): Set<Position2D>
+    fun refreshComplete(playerId: UUID, existingPositions: Set<Position3D>, areas: Set<Area>,
+                        edgeBlock: String, edgeSurfaceBlock: String)
+    fun refreshPartitioned(playerId: UUID, existingPositions: Set<Position3D>, areas: Set<Area>, edgeBlock: String,
+                           edgeSurfaceBlock: String, cornerBlock: String, cornerSurfaceBlock: String)
+    fun clear(playerId: UUID, areas: Set<Position3D>)
 }
