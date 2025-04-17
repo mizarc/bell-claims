891972043566e13b1dc7166b3ea32e70409aab7e
Mizarc <contact@mizarc.dev>
2025-04-17 15:44:52 +1000

    Add new action to clear visualisation

---
+++ src/main/kotlin/dev/mizarc/bellclaims/application/actions/player/visualisation/ClearVisualisation.kt
@@ -0,0 +1,26 @@
+package dev.mizarc.bellclaims.application.actions.player.visualisation
+
+import dev.mizarc.bellclaims.application.persistence.PlayerStateRepository
+import dev.mizarc.bellclaims.application.services.VisualisationService
+import dev.mizarc.bellclaims.domain.entities.PlayerState
+import java.util.UUID
+
+class ClearVisualisation(private val playerStateRepository: PlayerStateRepository,
+                         private val visualisationService: VisualisationService) {
+    /**
+     * Clears the claim visualisation for target player
+     */
+    fun execute(playerId: UUID) {
+        var playerState = playerStateRepository.get(playerId)
+        if (playerState == null) {
+            playerState = PlayerState(playerId)
+            playerStateRepository.add(playerState)
+        }
+
+        visualisationService.clear(playerId, playerState.visualisedBlockPositions.values.flatten().toSet())
+        playerState.visualisedBlockPositions.clear()
+        playerState.isVisualisingClaims = false
+        playerStateRepository.update(playerState)
+        return
+    }
+}
