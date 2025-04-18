7ce2502f24e6e3958658fdb33595d142a9baa840
Mizarc <contact@mizarc.dev>
2025-04-18 17:17:53 +1000

    Change remove partition to incorporate new check

--- src/main/kotlin/dev/mizarc/bellclaims/application/results/claim/partition/RemovePartitionResult.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/application/results/claim/partition/RemovePartitionResult.kt
@@ -2,6 +2,7 @@ package dev.mizarc.bellclaims.application.results.claim.partition
 
 sealed class RemovePartitionResult {
     object Success : RemovePartitionResult()
+    object WillBeDisconnected: RemovePartitionResult()
     object DoesNotExist : RemovePartitionResult()
     object StorageError: RemovePartitionResult()
 }
