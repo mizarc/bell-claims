ac1639f980ad6948b7b6d403959583b5f18c79a0
Mizarc <contact@mizarc.dev>
2025-04-20 03:18:03 +1000

    Add message when no permission to interact in claim
    Result output now includes claim that is being interacted with.

--- src/main/kotlin/dev/mizarc/bellclaims/application/results/claim/IsPlayerActionAllowedResult.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/application/results/claim/IsPlayerActionAllowedResult.kt
@@ -1,8 +1,10 @@
 package dev.mizarc.bellclaims.application.results.claim
 
+import dev.mizarc.bellclaims.domain.entities.Claim
+
 sealed class IsPlayerActionAllowedResult {
-    object Allowed: IsPlayerActionAllowedResult()
-    object Denied: IsPlayerActionAllowedResult()
+    data class Allowed(val claim: Claim): IsPlayerActionAllowedResult()
+    data class Denied(val claim: Claim): IsPlayerActionAllowedResult()
     object NoClaimFound: IsPlayerActionAllowedResult()
     object NoAssociatedPermission: IsPlayerActionAllowedResult()
     object StorageError: IsPlayerActionAllowedResult()
