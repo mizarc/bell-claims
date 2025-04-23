7d8572858e60d4387f75a89122363017db648172
Mizarc <contact@mizarc.dev>
2025-04-23 20:45:07 +1000

    Remove unused register visualisation results

--- src/main/kotlin/dev/mizarc/bellclaims/application/results/player/visualisation/UnregisterVisualisationResult.kt
+++
@@ -1,6 +0,0 @@
-package dev.mizarc.bellclaims.application.results.player.visualisation
-
-sealed class UnregisterVisualisationResult {
-    object Success: UnregisterVisualisationResult()
-    object ClaimNotFound: UnregisterVisualisationResult()
-}
