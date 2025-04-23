87fb58e25eaa4e631ebe4e79920b22627e99449a
Mizarc <contact@mizarc.dev>
2025-04-23 20:50:04 +1000

    Integrate config value into clear visualisation delay period

--- src/main/kotlin/dev/mizarc/bellclaims/application/actions/player/visualisation/ScheduleClearVisualisation.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/application/actions/player/visualisation/ScheduleClearVisualisation.kt
@@ -3,12 +3,14 @@ package dev.mizarc.bellclaims.application.actions.player.visualisation
 import dev.mizarc.bellclaims.application.persistence.PlayerStateRepository
 import dev.mizarc.bellclaims.application.results.player.visualisation.ScheduleClearVisualisationResult
 import dev.mizarc.bellclaims.application.services.scheduling.SchedulerService
+import dev.mizarc.bellclaims.config.MainConfig
 import dev.mizarc.bellclaims.domain.entities.PlayerState
 import java.util.UUID
 
 class ScheduleClearVisualisation(private val playerStateRepository: PlayerStateRepository,
                                  private val schedulerService: SchedulerService,
-                                 private val clearVisualisation: ClearVisualisation) {
+                                 private val clearVisualisation: ClearVisualisation,
+                                 private val config: MainConfig) {
     fun execute(playerId: UUID): ScheduleClearVisualisationResult {
         // Get or create player state if it doesn't exist
         var playerState = playerStateRepository.get(playerId)
@@ -24,7 +26,7 @@ class ScheduleClearVisualisation(private val playerStateRepository: PlayerStateR
         // Only schedule a new timer if the player currently has a visualization active
         if (playerState.isVisualisingClaims) {
             playerState.isVisualisingClaims = false
-            val delayTicks = 20L * 3 // 3 seconds delay
+            val delayTicks = (20L * config.visualiserHideDelayPeriod).toLong()
             val task = schedulerService.schedule(delayTicks) { clearVisualisation.execute(playerId) }
             playerState.scheduledVisualiserHide = task
             return ScheduleClearVisualisationResult.Success
