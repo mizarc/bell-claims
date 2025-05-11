f0853c8bf3e5e2824797c21535029648d4249d4b
Mizarc <contact@mizarc.dev>
2025-05-11 11:16:32 +1000

    Add localization provider interface
    This will be used to grab the locale text.

---
+++ src/main/kotlin/dev/mizarc/bellclaims/application/utilities/LocalizationProvider.kt
@@ -0,0 +1,12 @@
+package dev.mizarc.bellclaims.application.utilities
+
+interface LocalizationProvider {
+    /**
+     * Gets a localized message for the config defined locale.
+     *
+     * @param key The message key.
+     * @param args Optional arguments for formatting the message.
+     * @return The localized and formatted message. Handles missing keys internally.
+     */
+    fun get(key: String, vararg args: Any?)
+}
