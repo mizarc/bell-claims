188be6d70a64d5feeaff91eaa060ad10ed494dac
Mizarc <contact@mizarc.dev>
2025-05-11 11:10:44 +1000

    Add template for localization keys
    This contains some examples of keys in the format that will be used in
    the future for all localization keys.

---
+++ src/main/kotlin/dev/mizarc/bellclaims/domain/values/LocalizationKeys.kt
@@ -0,0 +1,30 @@
+package dev.mizarc.bellclaims.domain.values
+
+object LocalizationKeys {
+    // Action Feedback
+    const val FEEDBACK_CLAIM_DENIED = "feedback.claim.denied"
+
+    // Menu Titles
+    const val MENU_CLAIM_CREATION_TITLE = "menu.claim_creation.title"
+    const val MENU_CLAIM_MANAGEMENT_TITLE = "menu.claim_management.title"
+
+    // Common Menu Items
+    const val MENU_COMMON_CLOSE_NAME = "menu.common.close.name"
+    const val MENU_COMMON_BACK_NAME = "menu.common.back.name"
+
+    // Confirmation Menu Items
+    const val MENU_CONFIRMATION_ITEM_YES_NAME = "menu.confirmation.yes.name"
+    const val MENU_CONFIRMATION_ITEM_YES_LORE = "menu.confirmation.yes.lore"
+    const val MENU_CONFIRMATION_ITEM_NO_NAME = "menu.confirmation.no.name"
+    const val MENU_CONFIRMATION_ITEM_NO_LORE = "menu.confirmation.no.lore"
+
+    // Claim Creation Menu Items
+    const val MENU_CLAIM_CREATION_ITEM_INSUFFICIENT_CLAIMS_NAME = "menu.claim_creation.item.insufficient_claims.name"
+    const val MENU_CLAIM_CREATION_ITEM_INSUFFICIENT_CLAIMS_LORE = "menu.claim_creation.item.insufficient_claims.lore"
+    const val MENU_CLAIM_CREATION_ITEM_INSUFFICIENT_BLOCKS_NAME = "menu.claim_creation.item.insufficient_blocks.name"
+    const val MENU_CLAIM_CREATION_ITEM_INSUFFICIENT_BLOCKS_LORE = "menu.claim_creation.item.insufficient_blocks.lore"
+    const val MENU_CLAIM_CREATION_ITEM_OVERLAP_NAME = "menu.claim_creation.item.overlap.name"
+    const val MENU_CLAIM_CREATION_ITEM_OVERLAP_LORE = "menu.claim_creation.item.overlap.lore"
+    const val MENU_CLAIM_CREATION_ITEM_CREATE_NAME = "menu.claim_creation.item.create.name"
+    const val MENU_CLAIM_CREATION_ITEM_CREATE_LORE = "menu.claim_creation.item.create.lore"
+}
