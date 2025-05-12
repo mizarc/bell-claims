a87524f89b1ed6ddd0de9304acff28ce392ae4ef
Mizarc <contact@mizarc.dev>
2025-05-12 18:44:26 +1000

    Add get overload for specific locale

--- src/main/kotlin/dev/mizarc/bellclaims/application/utilities/LocalizationProvider.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/application/utilities/LocalizationProvider.kt
@@ -1,5 +1,7 @@
 package dev.mizarc.bellclaims.application.utilities
 
+import java.util.Locale
+
 interface LocalizationProvider {
     /**
      * Gets a localized message for the config defined locale.
@@ -9,4 +11,14 @@ interface LocalizationProvider {
      * @return The localized and formatted message. Handles missing keys internally.
      */
     fun get(key: String, vararg args: Any?): String
+
+    /**
+     * Gets a localized message for a specified locale.
+     *
+     * @param locale The specific locale to use.
+     * @param key The message key.
+     * @param args Optional arguments for formatting the message.
+     * @return The localized and formatted message. Handles missing keys internally.
+     */
+    fun get(locale: Locale, key: String, vararg args: Any?): String
 }
