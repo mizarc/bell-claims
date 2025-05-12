8b95465f64c4f9be76cf3da200484ec94a8e0614
Mizarc <contact@mizarc.dev>
2025-05-12 21:07:16 +1000

    Fix references to new confirmation menu system

--- src/main/kotlin/dev/mizarc/bellclaims/interaction/menus/management/ClaimTransferMenu.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/interaction/menus/management/ClaimTransferMenu.kt
@@ -4,28 +4,16 @@ import dev.mizarc.bellclaims.domain.entities.Claim
 import dev.mizarc.bellclaims.interaction.menus.Menu
 import dev.mizarc.bellclaims.interaction.menus.MenuNavigator
 import dev.mizarc.bellclaims.interaction.menus.common.ConfirmationMenu
-import dev.mizarc.bellclaims.interaction.menus.common.ConfirmationMenu.Companion.openConfirmationMenu
 import dev.mizarc.bellclaims.utils.getLangText
 import org.bukkit.entity.Player
 
 class ClaimTransferMenu(private val menuNavigator: MenuNavigator, private val claim: Claim,
                         private val player: Player): Menu {
     override fun open() {
-        val cancelAction: () -> Unit = {
-            player.closeInventory()
-        }
-
         val confirmAction: () -> Unit = {
             menuNavigator.openMenu(ClaimTransferNamingMenu(menuNavigator, claim, player))
         }
-
-        val parameters = ConfirmationMenu.Companion.ConfirmationMenuParameters(
-            menuTitle = getLangText("AcceptTransferClaim"),
-            cancelAction = cancelAction,
-            confirmAction = confirmAction,
-            confirmActionDescription = getLangText("AcceptTransferClaimConfirmDescription")
-        )
-
-        openConfirmationMenu(player, parameters)
+        menuNavigator.openMenu(ConfirmationMenu(menuNavigator, player,
+            getLangText("AcceptTransferClaim"), confirmAction))
     }
 }
