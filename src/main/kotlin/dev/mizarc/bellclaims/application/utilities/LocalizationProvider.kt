38f86049fd3e0c0aed79fe9eab2016fcd9e63fa1
Mizarc <contact@mizarc.dev>
2025-05-20 00:06:05 +1000

    Change localization provider to separate console locale
    As per player locale will be a feature added later down the line, the
    localization provider will always require the player's locale to move
    the decision making process from the presentation to the infrastructure
    layer. This also solves the current issue where it assumes the first
    string is a locale when it isn't in language keys that require
    arguments to be set.

--- src/main/kotlin/dev/mizarc/bellclaims/application/utilities/LocalizationProvider.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/application/utilities/LocalizationProvider.kt
@@ -4,21 +4,21 @@ import java.util.Locale
 
 interface LocalizationProvider {
     /**
-     * Gets a localized message for the config defined locale.
-     *
-     * @param key The message key.
-     * @param args Optional arguments for formatting the message.
-     * @return The localized and formatted message. Handles missing keys internally.
-     */
-    fun get(key: String, vararg args: Any?): String
-
-    /**
      * Gets a localized message for a specified locale.
      *
      * @param locale The specific locale to use.
      * @param key The message key.
      * @param args Optional arguments for formatting the message.
-     * @return The localized and formatted message. Handles missing keys internally.
+     * @return The localized and formatted message.
      */
     fun get(locale: String, key: String, vararg args: Any?): String
+
+    /**
+     * Gets a localized message using the server locale, used for console logs.
+     *
+     * @param key The message key.
+     * @param args Optional arguments for formatting the message.
+     * @return The localized and formatted message.
+     */
+    fun getConsole(key: String, vararg args: Any?): String
 }
