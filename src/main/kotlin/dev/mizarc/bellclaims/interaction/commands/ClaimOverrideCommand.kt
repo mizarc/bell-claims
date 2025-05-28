592d6963c251a7e7167b047c36ca563c198d218d
Mizarc <contact@mizarc.dev>
2025-05-29 00:55:48 +1000

    Localize claim override command

--- src/main/kotlin/dev/mizarc/bellclaims/interaction/commands/ClaimOverrideCommand.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/interaction/commands/ClaimOverrideCommand.kt
@@ -6,6 +6,8 @@ import co.aikar.commands.annotation.CommandPermission
 import co.aikar.commands.annotation.Default
 import dev.mizarc.bellclaims.application.actions.player.ToggleClaimOverride
 import dev.mizarc.bellclaims.application.results.player.ToggleClaimOverrideResult
+import dev.mizarc.bellclaims.application.utilities.LocalizationProvider
+import dev.mizarc.bellclaims.domain.values.LocalizationKeys
 import org.bukkit.entity.Player
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
@@ -13,21 +15,23 @@ import kotlin.getValue
 
 @CommandAlias("claimoverride")
 class ClaimOverrideCommand: BaseCommand(), KoinComponent {
+    private val localizationProvider: LocalizationProvider by inject()
     private val toggleClaimOverride: ToggleClaimOverride by inject()
 
     @Default
     @CommandPermission("bellclaims.command.claimoverride")
     fun onClaimOverride(player: Player) {
         val result = toggleClaimOverride.execute(player.uniqueId)
-        when (result) {
-            is ToggleClaimOverrideResult.Success ->  {
-                val newState = if (result.isOverrideEnabled) "enabled" else "disabled"
-                player.sendMessage("Claim override has been $newState.")
+
+        // Execute claim override action and output result to player
+        val messageKey = when (result) {
+            is ToggleClaimOverrideResult.Success -> {
+                if (result.isOverrideEnabled) LocalizationKeys.COMMAND_CLAIM_OVERRIDE_ENABLED
+                else LocalizationKeys.COMMAND_CLAIM_OVERRIDE_DISABLED
             }
-            is ToggleClaimOverrideResult.PlayerNotFound ->
-                player.sendMessage("Your player data was not found, contact your administrator for support.")
-            is ToggleClaimOverrideResult.StorageError ->
-                player.sendMessage("An internal error has occurred, contact your administrator for support.")
+            is ToggleClaimOverrideResult.PlayerNotFound,
+            is ToggleClaimOverrideResult.StorageError -> LocalizationKeys.GENERAL_ERROR
         }
+        player.sendMessage(localizationProvider.get(player.uniqueId, messageKey))
     }
 }
