61c7d53181b6eb331e76a4288536fbec6a8a57eb
Mizarc <contact@mizarc.dev>
2025-05-31 18:43:21 +1000

    Localize claim partitions command

--- src/main/kotlin/dev/mizarc/bellclaims/interaction/commands/PartitionsCommand.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/interaction/commands/PartitionsCommand.kt
@@ -6,14 +6,18 @@ import co.aikar.commands.annotation.Default
 import co.aikar.commands.annotation.Subcommand
 import dev.mizarc.bellclaims.application.actions.claim.metadata.GetClaimDetails
 import dev.mizarc.bellclaims.application.actions.claim.partition.GetClaimPartitions
+import dev.mizarc.bellclaims.application.utilities.LocalizationProvider
+import dev.mizarc.bellclaims.domain.values.LocalizationKeys
 import org.bukkit.entity.Player
 import dev.mizarc.bellclaims.infrastructure.ChatInfoBuilder
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
+import java.util.UUID
 import kotlin.math.ceil
 
 @CommandAlias("claim")
 class PartitionsCommand : ClaimCommand(), KoinComponent {
+    private val localizationProvider: LocalizationProvider by inject()
     private val getClaimPartitions: GetClaimPartitions by inject()
     private val getClaimDetails: GetClaimDetails by inject()
 
@@ -33,18 +37,27 @@ class PartitionsCommand : ClaimCommand(), KoinComponent {
         }
 
         // Output list of partitions
-        val claimName = getClaimDetails.execute(partition.claimId)?.name ?: "(Could not retrieve name)"
-        val chatInfo = ChatInfoBuilder("$claimName Partitions")
+        val claimName = getClaimName(player.uniqueId, partition.claimId)
+        val header = localizationProvider.get(player.uniqueId, LocalizationKeys.COMMAND_PARTITIONS_HEADER, claimName)
+        val chatInfo = ChatInfoBuilder(localizationProvider, player.uniqueId, header)
         for (i in 0..9 + page) {
             if (i > partitions.count() - 1) {
                 break
             }
-            chatInfo.addLinked((i + 1).toString(),
-                "Lower (${partitions[i].area.lowerPosition2D.x}), " +
-                        "${partitions[i].area.lowerPosition2D.z} | " +
-                        "Upper (${partitions[i].area.upperPosition2D.x}), " +
-                        "${partitions[i].area.upperPosition2D.z}")
+
+            chatInfo.addRow(localizationProvider.get(player.uniqueId, LocalizationKeys.COMMAND_CLAIM_LIST_ROW,
+                partitions[i].area.lowerPosition2D.x, partitions[i].area.lowerPosition2D.z,
+                partitions[i].area.upperPosition2D.x, partitions[i].area.upperPosition2D.z))
         }
         player.sendMessage(chatInfo.createPaged(page, ceil((partitions.count() / 10.0)).toInt()))
+    }
+
+    /**
+     * Helper function to retrieve the claim name or a default error message if not found.
+     */
+    private fun getClaimName(playerId: UUID, claimId: UUID): String {
+        return getClaimDetails.execute(claimId)?.name ?: localizationProvider.get(
+            playerId, LocalizationKeys.GENERAL_NAME_ERROR
+        )
     }
 }
