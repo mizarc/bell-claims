6e6f0451d9407478d4ca07915ab6e71ef7ac740c
Mizarc <contact@mizarc.dev>
2025-06-07 00:35:34 +1000

    Split give and check inventory methods

--- src/main/kotlin/dev/mizarc/bellclaims/application/services/ToolItemService.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/application/services/ToolItemService.kt
@@ -6,6 +6,8 @@ import java.util.UUID
 interface ToolItemService {
     fun giveClaimTool(playerId: UUID): Boolean
     fun giveMoveTool(playerId: UUID, claim: Claim): Boolean
+    fun doesPlayerHaveClaimTool(playerId: UUID): Boolean
+    fun doesPlayerHaveMoveTool(playerId: UUID, claim: Claim): Boolean
     fun isClaimTool(itemData: Map<String, String>): Boolean
     fun isMoveTool(itemData: Map<String, String>): Boolean
     fun getClaimIdFromPlayerMoveTool(itemData: Map<String, String>): String?
