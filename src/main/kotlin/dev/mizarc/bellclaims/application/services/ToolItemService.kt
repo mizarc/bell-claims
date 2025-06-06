76f7de42b7fbdfe2a8a7022951941771bc763efb
Mizarc <contact@mizarc.dev>
2025-06-06 22:56:26 +1000

    Add functional implementation of all tool item service functions

--- src/main/kotlin/dev/mizarc/bellclaims/application/services/ToolItemService.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/application/services/ToolItemService.kt
@@ -5,10 +5,8 @@ import java.util.UUID
 
 interface ToolItemService {
     fun giveClaimTool(playerId: UUID): Boolean
-    fun giveClaimMoveTool(playerId: UUID, claim: Claim): Boolean
-    fun isPlayerHoldingClaimTool(playerId: UUID): Boolean
-    fun isPlayerHoldingClaimMoveTool(playerId: UUID): Boolean
-    fun getClaimIdFromPlayerMoveTool(playerId: UUID): String?
-    fun removeClaimToolCommand(playerId: UUID): Boolean
-    fun removeClaimMoveToolCommand(playerId: UUID, claim: Claim): Boolean
+    fun giveMoveTool(playerId: UUID, claim: Claim): Boolean
+    fun isClaimTool(itemData: Map<String, String>): Boolean
+    fun isMoveTool(itemData: Map<String, String>): Boolean
+    fun getClaimIdFromPlayerMoveTool(itemData: Map<String, String>): String?
 }
