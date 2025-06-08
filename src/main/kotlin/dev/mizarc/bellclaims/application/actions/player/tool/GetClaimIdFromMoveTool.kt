2cc358b0dbb62343553cb172353399ca4c684911
Mizarc <contact@mizarc.dev>
2025-06-08 14:25:43 +1000

    Add new action to get claim id from move tool

---
+++ src/main/kotlin/dev/mizarc/bellclaims/application/actions/player/tool/GetClaimIdFromMoveTool.kt
@@ -0,0 +1,16 @@
+package dev.mizarc.bellclaims.application.actions.player.tool
+
+import dev.mizarc.bellclaims.application.results.player.tool.GetClaimIdFromMoveToolResult
+import dev.mizarc.bellclaims.application.services.ToolItemService
+import java.util.UUID
+
+class GetClaimIdFromMoveTool(private val toolItemService: ToolItemService) {
+    fun execute(itemData: Map<String, String>): GetClaimIdFromMoveToolResult {
+        val claimIdString = toolItemService.getClaimIdFromPlayerMoveTool(itemData)
+        if (claimIdString != null) {
+            val claimId = UUID.fromString(claimIdString)
+            return GetClaimIdFromMoveToolResult.Success(claimId)
+        }
+        return GetClaimIdFromMoveToolResult.NotMoveTool
+    }
+}
