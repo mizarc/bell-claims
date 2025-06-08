08e8dded019e19bf5417fdb4749702edbf480dfa
Mizarc <contact@mizarc.dev>
2025-06-08 14:02:57 +1000

    Add new action to automatically handle tool visualisation

--- src/main/kotlin/dev/mizarc/bellclaims/interaction/listeners/EditToolVisualisingListener.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/interaction/listeners/EditToolVisualisingListener.kt
@@ -1,13 +1,8 @@
 package dev.mizarc.bellclaims.interaction.listeners
 
-import dev.mizarc.bellclaims.application.actions.player.visualisation.ClearVisualisation
-import dev.mizarc.bellclaims.application.actions.player.visualisation.DisplayVisualisation
-import dev.mizarc.bellclaims.application.actions.player.visualisation.IsPlayerVisualising
-import dev.mizarc.bellclaims.application.actions.player.visualisation.ScheduleClearVisualisation
-import dev.mizarc.bellclaims.application.results.player.visualisation.IsPlayerVisualisingResult
+import dev.mizarc.bellclaims.application.actions.player.tool.SyncToolVisualization
+import dev.mizarc.bellclaims.infrastructure.adapters.bukkit.toCustomItemData
 import dev.mizarc.bellclaims.infrastructure.adapters.bukkit.toPosition3D
-import dev.mizarc.bellclaims.infrastructure.getClaimTool
-import dev.mizarc.bellclaims.infrastructure.isClaimTool
 import org.bukkit.entity.EntityType
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
@@ -23,17 +18,14 @@ import org.koin.core.component.inject
 import kotlin.concurrent.thread
 
 class EditToolVisualisingListener(private val plugin: JavaPlugin): Listener, KoinComponent {
-    private val displayVisualisation: DisplayVisualisation by inject()
-    private val clearVisualisation: ClearVisualisation by inject()
-    private val scheduleClearVisualisation: ScheduleClearVisualisation by inject()
-    private val isPlayerVisualising: IsPlayerVisualising by inject()
+    private val syncToolVisualization: SyncToolVisualization by inject()
 
     /**
      * Triggers when the player swaps to the claim tool in their inventory.
      */
     @EventHandler
     fun onHoldClaimTool(event: PlayerItemHeldEvent) {
-        plugin.server.scheduler.runTask(plugin, Runnable { autoClaimToolVisualisation(event.player) })
+        plugin.server.scheduler.runTask(plugin, Runnable { handleAutoVisualisation(event.player) })
     }
 
     /**
@@ -42,7 +34,7 @@ class EditToolVisualisingListener(private val plugin: JavaPlugin): Listener, Koi
     @EventHandler
     fun onPickupClaimTool(event: EntityPickupItemEvent) {
         if (event.entityType != EntityType.PLAYER) return
-        plugin.server.scheduler.runTask(plugin, Runnable { autoClaimToolVisualisation(event.entity as Player) })
+        plugin.server.scheduler.runTask(plugin, Runnable { handleAutoVisualisation(event.entity as Player) })
     }
 
     /**
@@ -50,7 +42,7 @@ class EditToolVisualisingListener(private val plugin: JavaPlugin): Listener, Koi
      */
     @EventHandler
     fun onDropClaimTool(event: PlayerDropItemEvent) {
-        plugin.server.scheduler.runTask(plugin, Runnable { autoClaimToolVisualisation(event.player) })
+        plugin.server.scheduler.runTask(plugin, Runnable { handleAutoVisualisation(event.player) })
     }
 
     /**
@@ -59,7 +51,7 @@ class EditToolVisualisingListener(private val plugin: JavaPlugin): Listener, Koi
     @EventHandler
     fun onClaimToolInventoryInteract(event: InventoryClickEvent) {
         val player = event.whoClicked as Player
-        plugin.server.scheduler.runTask(plugin, Runnable { autoClaimToolVisualisation(player) })
+        plugin.server.scheduler.runTask(plugin, Runnable { handleAutoVisualisation(player) })
     }
 
     /**
@@ -69,27 +61,18 @@ class EditToolVisualisingListener(private val plugin: JavaPlugin): Listener, Koi
     fun onClaimToolClick(event: PlayerInteractEvent) {
         thread(start = true) {
             Thread.sleep(1)
-            autoClaimToolVisualisation(event.player)
+            handleAutoVisualisation(event.player)
         }
     }
 
     /**
      * Visualise if player isn't already holding the claim tool (e.g. swapping hands)
      */
-    private fun autoClaimToolVisualisation(player: Player) {
-        val holdingClaimTool = (isClaimTool(player.inventory.itemInMainHand)
-                || (isClaimTool(player.inventory.itemInOffHand)))
-        if (holdingClaimTool) {
-            displayVisualisation.execute(player.uniqueId, player.location.toPosition3D())
-        } else {
-            when (val result = isPlayerVisualising.execute(player.uniqueId)) {
-                is IsPlayerVisualisingResult.Success -> {
-                    if (result.isVisualising) {
-                        scheduleClearVisualisation.execute(player.uniqueId)
-                    }
-                }
-                else -> {}
-            }
-        }
+    private fun handleAutoVisualisation(player: Player) {
+        val playerId = player.uniqueId
+        val position = player.location.toPosition3D()
+        val mainHand = player.inventory.itemInMainHand
+        val offHand = player.inventory.itemInOffHand
+        syncToolVisualization.execute(playerId, position, mainHand.toCustomItemData(), offHand.toCustomItemData())
     }
 }
