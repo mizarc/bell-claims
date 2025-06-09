a973a9151e1a07342199ca456135dad1400a06c4
Mizarc <contact@mizarc.dev>
2025-06-10 02:13:00 +1000

    Add config custom model ids for tools

--- src/main/kotlin/dev/mizarc/bellclaims/infrastructure/services/ToolItemServiceBukkit.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/infrastructure/services/ToolItemServiceBukkit.kt
@@ -1,8 +1,10 @@
 package dev.mizarc.bellclaims.infrastructure.services
 
 import dev.mizarc.bellclaims.application.errors.PlayerNotFoundException
+import dev.mizarc.bellclaims.application.services.ConfigService
 import dev.mizarc.bellclaims.application.services.ToolItemService
 import dev.mizarc.bellclaims.application.utilities.LocalizationProvider
+import dev.mizarc.bellclaims.config.MainConfig
 import dev.mizarc.bellclaims.domain.entities.Claim
 import dev.mizarc.bellclaims.domain.values.LocalizationKeys
 import dev.mizarc.bellclaims.infrastructure.adapters.bukkit.toCustomItemData
@@ -15,7 +17,8 @@ import org.bukkit.inventory.ItemStack
 import org.bukkit.persistence.PersistentDataType
 import java.util.UUID
 
-class ToolItemServiceBukkit(private val localizationProvider: LocalizationProvider): ToolItemService {
+class ToolItemServiceBukkit(private val localizationProvider: LocalizationProvider,
+                            private val config: MainConfig): ToolItemService {
     override fun giveClaimTool(playerId: UUID): Boolean {
         // Create the claim tool with special metadata
         val tool = ItemStack(Material.STICK)
@@ -23,7 +26,7 @@ class ToolItemServiceBukkit(private val localizationProvider: LocalizationProvid
             .lore(localizationProvider.get(playerId, LocalizationKeys.ITEM_CLAIM_TOOL_LORE_MAIN_HAND))
             .lore(localizationProvider.get(playerId, LocalizationKeys.ITEM_CLAIM_TOOL_LORE_OFF_HAND))
         val itemMeta = tool.itemMeta
-        itemMeta?.setCustomModelData(1)
+        itemMeta?.setCustomModelData(config.customClaimToolModelId)
         itemMeta.persistentDataContainer.set(ItemKeys.CLAIM_TOOL_KEY, PersistentDataType.BOOLEAN, true)
         tool.itemMeta = itemMeta
 
@@ -40,7 +43,7 @@ class ToolItemServiceBukkit(private val localizationProvider: LocalizationProvid
             .name(localizationProvider.get(playerId, LocalizationKeys.ITEM_MOVE_TOOL_NAME, claim.name))
             .lore(localizationProvider.get(playerId, LocalizationKeys.ITEM_MOVE_TOOL_LORE))
         val itemMeta = tool.itemMeta
-        itemMeta?.setCustomModelData(1)
+        itemMeta?.setCustomModelData(config.customMoveToolModelId)
         itemMeta.persistentDataContainer.set(ItemKeys.MOVE_TOOL_KEY, PersistentDataType.STRING, claim.id.toString())
         tool.itemMeta = itemMeta
 
