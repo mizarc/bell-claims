5be3ea4b16f6fff89061bff41edb699ca0fc4527
Mizarc <contact@mizarc.dev>
2025-12-24 19:04:54 +1000

    Rename the claim description result action

--- src/main/kotlin/dev/mizarc/bellclaims/application/actions/claim/metadata/UpdateClaimDescription.kt
+++ src/main/kotlin/dev/mizarc/bellclaims/application/actions/claim/metadata/UpdateClaimDescription.kt
@@ -1,23 +1,23 @@
 package dev.mizarc.bellclaims.application.actions.claim.metadata
 
-import dev.mizarc.bellclaims.application.results.claim.metadata.UpdateClaimAttributeResult
 import dev.mizarc.bellclaims.application.errors.DatabaseOperationException
 import dev.mizarc.bellclaims.application.persistence.ClaimRepository
+import dev.mizarc.bellclaims.application.results.claim.metadata.UpdateClaimDescriptionResult
 import java.util.UUID
 
 class UpdateClaimDescription(private val claimRepository: ClaimRepository) {
-    fun execute(claimId: UUID, description: String): UpdateClaimAttributeResult {
-        // Check if claim exists
-        val existingClaim = claimRepository.getById(claimId) ?: return UpdateClaimAttributeResult.ClaimNotFound
+    fun execute(claimId: UUID, description: String): UpdateClaimDescriptionResult {
+        // Check if the claim exists
+        val existingClaim = claimRepository.getById(claimId) ?: return UpdateClaimDescriptionResult.ClaimNotFound
 
-        // Change description and persist to storage
+        // Change description and persist in storage
         val newClaim = existingClaim.copy(description = description)
         try {
             claimRepository.update(newClaim)
-            return UpdateClaimAttributeResult.Success(newClaim)
+            return UpdateClaimDescriptionResult.Success(newClaim)
         } catch (error: DatabaseOperationException) {
             println("Error has occurred trying to save to the database")
-            return UpdateClaimAttributeResult.StorageError
+            return UpdateClaimDescriptionResult.StorageError
         }
     }
 }
